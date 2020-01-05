5f30cee99fdc603c0e984b8d2e23a5019ad122b0
omarjaber <omar.ja93@gmail.com>
2020-01-05 21:34:00 +0000

    edit transfer

--- one_fm/templates/pages/domain_transfer.js
+++ one_fm/templates/pages/domain_transfer.js
@@ -1,9 +1,11 @@
 frappe.ready(function() {
 
     var curr_url = window.location.href
-    if (curr_url=='http://one-fm.com') {
+    if (curr_url.startsWith('http://one-fm') && location.pathname!='/homepage') {
         window.location.replace('/homepage');
-    } else {
+    }
+
+    if (!curr_url.startsWith('http://one-fm')) {
         window.location.replace('/login');
     }
 
