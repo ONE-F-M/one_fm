9f93bea2df44bc8b062ec2f7f6a1ca5afb2f2f7f
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-05-07 17:34:40 +0530

    fix: Agency - Updated - Validated

--- one_fm/one_fm/doctype/agency/agency.js
+++ one_fm/one_fm/doctype/agency/agency.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Agency', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+    set_address_and_contact_html(frm);
+    frm.set_df_property('attachments_required_section', 'hidden', frm.doc.__islocal);
+	}
 });
+
+var set_address_and_contact_html = function(frm) {
+  frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Agency'}
+  frm.toggle_display(['address_html','contact_html'], !frm.doc.__islocal);
+
+  if(!frm.doc.__islocal) {
+    frappe.contacts.render_address_and_contact(frm);
+  } else {
+    frappe.contacts.clear_address_and_contact(frm);
+  }
+};
