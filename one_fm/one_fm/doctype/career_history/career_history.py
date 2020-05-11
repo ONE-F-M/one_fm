5cfd6e7aeb6eaff4fd722615369b5292923cfd99
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-05-11 16:20:27 +0530

    Career History - Validate - fix

--- one_fm/one_fm/doctype/career_history/career_history.py
+++ one_fm/one_fm/doctype/career_history/career_history.py
@@ -21,12 +21,14 @@ def set_totals_in_career_history_company(doc):
 				for promotion in doc.promotions:
 					if company.company_name == promotion.company_name:
 						total_promotion += 1
-						recruiter_validation_score_promotion += promotion.recruiter_validation_score
+						if promotion.recruiter_validation_score:
+							recruiter_validation_score_promotion += promotion.recruiter_validation_score
 			if doc.salary_hikes:
 				for salary_hike in doc.salary_hikes:
 					if company.company_name == salary_hike.company_name:
 						total_salary_hike += 1
-						recruiter_validation_score_salary_change += salary_hike.recruiter_validation_score
+						if salary_hike.recruiter_validation_score:
+							recruiter_validation_score_salary_change += salary_hike.recruiter_validation_score
 			company.total_promotions = total_promotion
 			company.recruiter_validation_score_promotion = recruiter_validation_score_promotion
 			company.total_salary_changes = total_salary_hike
