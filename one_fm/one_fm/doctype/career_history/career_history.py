282c5f2d16fe61b95ca1c7f0549e464e068067d1
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-05-11 17:21:48 +0530

    fix: Career History - Validate Date Overlap - Promotions and Salary Hikes

--- one_fm/one_fm/doctype/career_history/career_history.py
+++ one_fm/one_fm/doctype/career_history/career_history.py
@@ -3,12 +3,57 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
+class OverlapError(frappe.ValidationError): pass
 class CareerHistory(Document):
 	def validate(self):
 		set_totals_in_career_history_company(self)
+		self.validate_date_in_promotions_salary_hikes()
+
+	def validate_date_in_promotions_salary_hikes(self):
+		if self.promotions:
+			for promotion in self.promotions:
+				validate_overlap(self, promotion, 'Promotions')
+
+		if self.salary_hikes:
+			for salary_hike in self.salary_hikes:
+				validate_overlap(self, salary_hike, 'Salary Hikes')
+
+def validate_overlap(doc, child_doc, table_label):
+	query = """
+		select
+			name
+		from
+			`tab{0}`
+		where
+			name != %(name)s
+		"""
+	query += get_doc_condition()
+
+	if not child_doc.name:
+		# hack! if name is null, it could cause problems with !=
+		child_doc.name = "New "+child_doc.doctype
+
+	overlap_doc = frappe.db.sql(query.format(child_doc.doctype),{
+			"start_date": child_doc.start_date,
+			"end_date": child_doc.end_date,
+			"name": child_doc.name,
+			"company_name": child_doc.company_name,
+			"parent": doc.name
+		}, as_dict = 1)
+	if overlap_doc:
+		frappe.throw(_("Row {0}: Start Date and End Date of Career History ({1}) is overlapping with {2}")
+			.format(child_doc.idx, table_label, overlap_doc[0].name), OverlapError)
+
+def get_doc_condition():
+	return """
+		and company_name = %(company_name)s and (start_date between %(start_date)s and %(end_date)s
+		or end_date between %(start_date)s and %(end_date)s
+		or (start_date < %(start_date)s and end_date > %(end_date)s))
+	"""
 
 def set_totals_in_career_history_company(doc):
 	if doc.career_history_company:
