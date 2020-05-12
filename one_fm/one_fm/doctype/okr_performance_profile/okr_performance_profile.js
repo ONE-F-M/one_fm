4ffee3939aa09c11ca1c1334613ce343ccb1a5ed
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-05-12 17:47:16 +0530

    fix: OKR Performance Profile - Help text and Realign fields

--- one_fm/one_fm/doctype/okr_performance_profile/okr_performance_profile.js
+++ one_fm/one_fm/doctype/okr_performance_profile/okr_performance_profile.js
@@ -5,11 +5,38 @@ frappe.ui.form.on('OKR Performance Profile', {
 	refresh: function(frm) {
     set_objective_option_to_kr(frm);
 		set_objectoves_description(frm);
+		frm.add_custom_button(__('Help'), function() {
+			show_help_text(frm);
+		});
 	},
 	description: function(frm) {
 		set_objectoves_description(frm);
 	}
 });
+
+var show_help_text = function(frm) {
+	var dialog = new frappe.ui.Dialog({
+		title: 'Help',
+		width: 400,
+		fields: [
+			{fieldname:'help_text', fieldtype:'HTML'}
+		]
+	});
+	var help_text_html = `\
+			<h3>You can Re evaluate the OKRs based on the below questions</h3>\
+			<p>Objective:</p>\
+			<ol>\
+			<li>On this job what types of projects and tasks would this person be assigned?</li>\
+			<li>Why would a top person who's not looking, would see this as a better career opportunity than the person's current role or some competing opportunity for other than a big monetary increase?</li>\
+			</ol>\
+			<p>Key Results:</p>\
+			<ol>\
+			<li>What the candidate would need to accomplish in doing this work that indicates this person is a great performer?</li>\
+			</ol>
+	`;
+	dialog.fields_dict.help_text.html(help_text_html);
+	dialog.show();
+};
 
 var set_objectoves_description = function(frm) {
 	let description = frm.doc.description?frm.doc.description:'';
