db739e8b7606606f3b7155165c76a926df30f31c
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-05-13 10:47:53 +0530

    fix: Agency - validate website address

--- one_fm/one_fm/doctype/agency/agency.js
+++ one_fm/one_fm/doctype/agency/agency.js
@@ -5,8 +5,25 @@ frappe.ui.form.on('Agency', {
 	refresh: function(frm) {
     set_address_and_contact_html(frm);
     frm.set_df_property('attachments_required_section', 'hidden', frm.doc.__islocal);
+	},
+	agency_website: function(frm) {
+		validate_website_adress(frm);
 	}
 });
+
+var validate_website_adress = function(frm) {
+	if(frm.doc.agency_website){
+		frappe.call({
+			method: 'one_fm.one_fm.doctype.agency.agency.validate_website_adress',
+			args: {website: frm.doc.agency_website},
+			callback: function(r) {
+				if(r && !r.message){
+					frappe.throw(__("The Given Website Address is not Valid."));
+				}
+			}
+		});
+	}
+};
 
 var set_address_and_contact_html = function(frm) {
   frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Agency'}
