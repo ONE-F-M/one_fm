4dbffc3a904c21b407d8d3ce0e6870259ea045ff
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-05-26 12:21:59 +0530

    feat: Initial - Web Form - Agency and Career History

---
+++ one_fm/one_fm/web_form/career_history/career_history.js
@@ -0,0 +1,10 @@
+frappe.ready(function() {
+	// bind events here
+  // frappe.web_form.fields_dict["tablename"].grid.get_field("fieldname").get_query = function(){
+	// 		return {
+	// 			query: "method_path",
+	// 		};
+	// 	}
+
+  // frappe.web_form.fields_dict['career_history_company'].grid.get_data('company_name')
+})
