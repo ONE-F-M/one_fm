feaaa5665d7b9059b0b3cd2ddb4233c8fb2e6230
Kartik Sharma <kartiksharma9319@gmail.com>
2020-06-08 20:51:54 +0000

    fix: Clearing markers and radius when a new is created, Default location set to Head office.

--- one_fm/public/js/doctype_js/location.js
+++ one_fm/public/js/doctype_js/location.js
@@ -1,23 +1,26 @@
 frappe.ui.form.on('Location',{
     onload_post_render: function(frm){
         $('[data-fieldname="map_html"]').append(`<div style='width:100%; height:500px' id='map'></div>`);
-
+        let {latitude, longitude}= frm.doc;
         window.markers = [];
         window.circles = [];
         // JS API is loaded and available
+        console.log("Called")
         window.map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 29.338392, lng: 48.005867},
+            center: {lat: 29.338394, lng: 48.005958},
             zoom: 17
         });
+        loadGoogleMap(frm);
 
         // Configure the click listener.
         map.addListener('click', function(mapsMouseEvent) {
           clearMarkers();
+          clearCircles();
+          console.log(mapsMouseEvent.latLng);
           frappe.model.set_value(frm.doc.doctype, frm.doc.name, 'latitude', mapsMouseEvent.latLng.lat());
           frappe.model.set_value(frm.doc.doctype, frm.doc.name, 'longitude',mapsMouseEvent.latLng.lng());
         });
 
-        loadGoogleMap(frm);
     },
     latitude: function(frm){
         loadGoogleMap(frm);
@@ -32,9 +35,10 @@ frappe.ui.form.on('Location',{
 })
 
 function loadGoogleMap(frm){
-    let lat = frm.doc.latitude;
-    let lng = frm.doc.longitude;
+    let lat = frm.doc.latitude === 19.0800 ? 29.338394 : frm.doc.latitude;
+    let lng = frm.doc.longitude === 72.8961 ? 48.005958 : frm.doc.longitude;
     let radius = frm.doc.geofence_radius;
+    console.log(lat, lng, radius);
     if(lat !== undefined && lng !== undefined){
         let marker = new google.maps.Marker({
             position: {lat, lng},
