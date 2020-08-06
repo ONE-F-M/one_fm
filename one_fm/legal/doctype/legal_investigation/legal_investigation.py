ff820e9f72726639902805807a37cc03e18dbd6a
Kartik Sharma <kartiksharma9319@gmail.com>
2020-08-06 07:30:40 +0000

    fix: Add remaining penalty code

--- one_fm/legal/doctype/legal_investigation/legal_investigation.py
+++ one_fm/legal/doctype/legal_investigation/legal_investigation.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe import _
-from frappe.utils import get_link_to_form
+from frappe.utils import get_link_to_form, nowdate, getdate, now_datetime
 
 class LegalInvestigation(Document):
 	def after_insert(self):
@@ -35,7 +35,51 @@ class LegalInvestigation(Document):
 		frappe.sendmail([supervisor_user], subject=subject, message=message, reference_doctype=self.doctype, reference_name=self.name)
 
 
+	def on_update(self):
+		if self.workflow_state == "Investigation Completed":
+			self.validate_results_and_sessions()
+			self.end_date = nowdate()
+			self.validate_penalty_details()
+			self.issue_penalty()
 
+	def validate_penalty_details(self):
+		for penalty in self.legal_investigation_penalty:
+			date = getdate(penalty.penalty_occurence_time)
+			if not frappe.db.exists("Shift Assignment", {"date": date, "employee": penalty.employee_id, "shift": penalty.shift}):
+				frappe.throw(_("Please check the penalty occurence details. Shift assignment for {name}:{employee_id} does not match for selected date in Row {idx}"
+							.format(name=penalty.employee_name, employee_id=penalty.employee_id, idx=penalty.idx)))
+
+	def validate_results_and_sessions(self):
+		if self.investigation_results is None:
+			frappe.throw(_("Investigations results field cannot be empty. Please update investigation results."))
+		if len(self.session_summary) == 0 or len(frappe.get_all("Legal Investigation Session", {"legal_investigation_code": self.name})) == 0:
+			frappe.throw(_("Cannot complete Legal investigation. No Legal Investigation session conducted."))
+			
+	def issue_penalty(self):
+		for penalty in self.legal_investigation_penalty:
+			penalty_issuance = frappe.new_doc("Penalty Issuance")
+			penalty_issuance.issuing_time = now_datetime()
+			penalty_issuance.penalty_location = penalty.penalty_location
+			penalty_issuance.penalty_occurence_time = penalty.penalty_occurence_time
+			penalty_issuance.shift = penalty.shift
+			penalty_issuance.site = penalty.site
+			penalty_issuance.project = penalty.project
+			penalty_issuance.site_location = penalty.site_location
+			penalty_issuance.append("employees", {
+				"employee_id": penalty.employee_id,
+				"employee_name": penalty.employee_name,
+				"designation": penalty.designation,
+			})
+			penalty_issuance.append("penalty_issuance_details",{
+				"penalty_type": penalty.penalty_type,
+				"exact_notes": penalty.investigation_results
+			})
+			penalty_issuance.issuing_employee = self.investigation_lead
+			# penalty_issuance.employee_name = self.lead_name
+			penalty_issuance.flags.ignore_permissions = True
+			penalty_issuance.insert()
+			penalty_issuance.submit()
+			
 @frappe.whitelist()
 def filter_employees(doctype, txt, searchfield, start, page_len, filters):
 	doctype = filters.get('doctype')
