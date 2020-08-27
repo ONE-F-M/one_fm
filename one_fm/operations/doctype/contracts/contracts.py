80c645f808a19134d606f29d214bb1e83203857c
abrarpv97 <abrarpv97@gmail.com>
2020-08-27 16:47:10 +0530

    Updated Contracts doctype in operations module

--- one_fm/operations/doctype/contracts/contracts.py
+++ one_fm/operations/doctype/contracts/contracts.py
@@ -3,8 +3,28 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Contracts(Document):
 	pass
+
+@frappe.whitelist()
+def insert_login_credential(url,user_name,password,client):
+	password_management_name = client+'-'+user_name
+	password_management = frappe.new_doc('Password Management')
+	password_management.flags.ignore_permissions  = True
+	password_management.update({
+		'password_management':password_management_name,
+		'password_category': 'Customer Portal',
+		'url': url,
+		'username':user_name,
+		'password':password
+	}).insert()
+
+	frappe.msgprint(msg = 'Online portal credentials are saved into password management',
+       title = 'Notification',
+       indicator = 'green'
+    )
+
+	return 	password_management
