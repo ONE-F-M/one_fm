a6bff1a7113fa9ce3572aaad457d179c22a70e3a
almarzouq10 <62424501+almarzouq10@users.noreply.github.com>
2020-08-31 20:13:20 +0300

    MOI Residency Doctype + Print Format for PAM Authorized Signatory

---
+++ one_fm/grd/doctype/moi_residency_jawazat/moi_residency_jawazat.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, omar jaber and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('MOI Residency Jawazat', {
+	// refresh: function(frm) {
+
+	// }
+});
