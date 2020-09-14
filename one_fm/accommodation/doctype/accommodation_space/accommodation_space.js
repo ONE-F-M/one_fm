a6f4980b85f98dade875932b90c7f310e50cbb58
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-09-14 23:10:40 +0530

    fix: QR Code for Accommodation, Unit, Space and Bed

--- one_fm/accommodation/doctype/accommodation_space/accommodation_space.js
+++ one_fm/accommodation/doctype/accommodation_space/accommodation_space.js
@@ -15,12 +15,26 @@ frappe.ui.form.on('Accommodation Space', {
 		calculate_area_and_volume(frm);
 	},
 	refresh: function(frm){
+		set_qr_code(frm);
 		set_filters(frm);
 	},
 	accommodation_unit: function(frm) {
 		set_space_type_filter(frm);
 	}
 });
+
+var set_qr_code = function(frm) {
+	let qr_code_html = `{%if doc.name%}
+	<div style="display: inline-block;padding: 5%;">
+	<div class="qr_code_print" id="qr_code_print">
+	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{doc.name}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
+	</div>
+	</div>
+	{%endif%}`
+	var qr_code = frappe.render_template(qr_code_html,{"doc":frm.doc});
+	$(frm.fields_dict["space_qr"].wrapper).html(qr_code);
+	refresh_field("space_qr")
+};
 
 var set_space_type_filter = function(frm) {
 	if(frm.doc.accommodation_unit){
--- one_fm/accommodation/doctype/bed/bed.js
+++ one_fm/accommodation/doctype/bed/bed.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on('Bed', {
 	refresh: function(frm) {
+		set_qr_code(frm);
 		frm.set_query('accommodation_space', function () {
 			return {
 				filters: {
@@ -22,4 +23,17 @@ frappe.ui.form.on('Bed', {
 var book_bed = function(frm) {
   frappe.route_options = {"bed": frm.doc.name};
 	frappe.new_doc("Book Bed");
+};
+
+var set_qr_code = function(frm) {
+	let qr_code_html = `{%if doc.name%}
+	<div style="display: inline-block;padding: 5%;">
+	<div class="qr_code_print" id="qr_code_print">
+	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{doc.name}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
+	</div>
+	</div>
+	{%endif%}`
+	var qr_code = frappe.render_template(qr_code_html,{"doc":frm.doc});
+	$(frm.fields_dict["bed_qr"].wrapper).html(qr_code);
+	refresh_field("bed_qr")
 };
