02ae57c85495a7fa2c65e4d75780b27261ef1c17
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-09-24 18:07:11 +0530

    fix: Accommodation - Module - Docs QRCode print and Details

--- one_fm/accommodation/doctype/accommodation/accommodation.js
+++ one_fm/accommodation/doctype/accommodation/accommodation.js
@@ -34,10 +34,25 @@ var set_qr_code = function(frm) {
 	<div style="display: inline-block;padding: 5%;">
 	<div class="qr_code_print" id="qr_code_print">
 	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{doc.name}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
+	<p>
+		{{qr_details}}
+	</p>
 	</div>
+	<br>
+	<input name="qr_b_print" type="button" class="qr_ipt" id="qr_ipt" value=" Print ">
 	</div>
-	{%endif%}`
-	var qr_code = frappe.render_template(qr_code_html,{"doc":frm.doc});
+	{%endif%}
+	<script type="text/javascript">
+	$("#qr_ipt").click(function() {
+			var divToPrint = document.getElementById("qr_code_print");
+			newWin = window.open("");
+			newWin.document.write(divToPrint.outerHTML);
+			newWin.print();
+	});
+	</script>`
+	var doc = frm.doc;
+	var qr_details = __('{0}<br/>Code: {1}', [doc.accommodation, doc.name])
+	var qr_code = frappe.render_template(qr_code_html,{"doc":doc, "qr_details": qr_details});
 	$(frm.fields_dict["accommodation_qr"].wrapper).html(qr_code);
 	refresh_field("accommodation_qr")
 };
--- one_fm/accommodation/doctype/accommodation_space/accommodation_space.js
+++ one_fm/accommodation/doctype/accommodation_space/accommodation_space.js
@@ -28,10 +28,26 @@ var set_qr_code = function(frm) {
 	<div style="display: inline-block;padding: 5%;">
 	<div class="qr_code_print" id="qr_code_print">
 	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{doc.name}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
+	<p>
+		{{qr_details}}
+	</p>
 	</div>
+	<br>
+	<input name="qr_b_print" type="button" class="qr_ipt" id="qr_ipt" value=" Print ">
 	</div>
-	{%endif%}`
-	var qr_code = frappe.render_template(qr_code_html,{"doc":frm.doc});
+	{%endif%}
+	<script type="text/javascript">
+	$("#qr_ipt").click(function() {
+			var divToPrint = document.getElementById("qr_code_print");
+			newWin = window.open("");
+			newWin.document.write(divToPrint.outerHTML);
+			newWin.print();
+	});
+	</script>`
+	var doc = frm.doc;
+	var qr_details = __('{0}, {1}<br/>{2} Floor, Unit: {3}<br/>{4}<br/>Code: {5}', [doc.accommodation_name,
+		doc.type, doc.floor_name, doc.accommodation_unit, doc.accommodation_space_type, doc.name])
+	var qr_code = frappe.render_template(qr_code_html,{"doc":doc, "qr_details": qr_details});
 	$(frm.fields_dict["space_qr"].wrapper).html(qr_code);
 	refresh_field("space_qr")
 };
