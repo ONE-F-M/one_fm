471eedcff9c5680819e4001b29493d6b9bb05650
abrarpv97 <frappe@ubuntu.vm>
2020-10-03 08:45:05 +0000

    set pricelist and contracts

--- one_fm/public/js/doctype_js/delivery_note.js
+++ one_fm/public/js/doctype_js/delivery_note.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on('Delivery Note', {
 	refresh(frm) {
+        frm.set_df_property('contracts', 'read_only', 1);
         if(frm.doc.customer){
             frm.set_query("project", function() {
                 return {
@@ -9,6 +10,88 @@ frappe.ui.form.on('Delivery Note', {
                 };
             });
             frm.refresh_field("project");
+        }
+    },
+    customer: function(frm){
+        if(frm.doc.project){
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args:{
+                    'doctype':'Price List',
+                    'filters':{
+                        'project': frm.doc.project,
+                        'enabled':1,
+                        'selling':1
+                    },
+                    'fieldname':[
+                        'name'
+                    ]
+                },
+                callback:function(s){
+                    if (!s.exc) {
+                        var selling_price_list = s.message.name;
+                        frm.set_value("selling_price_list",selling_price_list);
+                        frm.refresh_field("selling_price_list");
+                    }
+                }
+            });
+        }
+        else{
+            frm.set_value("selling_price_list",null);
+            frm.refresh_field("selling_price_list");
+        }
+    },
+    project: function(frm){
+        if(frm.doc.project){
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args:{
+                    'doctype':'Price List',
+                    'filters':{
+                        'project': frm.doc.project,
+                        'enabled':1,
+                        'selling':1
+                    },
+                    'fieldname':[
+                        'name'
+                    ]
+                },
+                callback:function(s){
+                    if (!s.exc) {
+                        //console.log(s.message);
+                        if(s.message){
+                            var selling_price_list = s.message.name;
+                            frm.set_value("selling_price_list",selling_price_list);
+                            frm.refresh_field("selling_price_list");
+                        }
+                        else{
+                            frm.set_value("selling_price_list",null);
+                            frm.refresh_field("selling_price_list");
+                        }
+                    }
+                }
+            });
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args:{
+                    'doctype':'Contracts',
+                    'filters':{
+                        'project': frm.doc.project,
+                    },
+                    'fieldname':[
+                        'name'
+                    ]
+                },
+                callback:function(s){
+                    if (!s.exc) {
+                        if(s.message){
+                            var contracts = s.message.name;
+                            frm.set_value("contracts",contracts);
+                            frm.refresh_field("contracts");
+                        }
+                    }
+                }
+            });
         }
     }
 });
