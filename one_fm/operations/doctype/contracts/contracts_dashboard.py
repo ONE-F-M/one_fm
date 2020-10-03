da932e732072330f0af493662279b1d774f227c1
abrarpv97 <frappe@ubuntu.vm>
2020-10-03 08:53:51 +0000

    changes in contacts doctype

---
+++ one_fm/operations/doctype/contracts/contracts_dashboard.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'contracts',
+		'non_standard_fieldnames': {
+			'Sales Invoice': 'contracts',
+			'Delivery Note': 'contracts',
+		},
+		'transactions': [
+			{
+				'items': ['Sales Invoice', 'Delivery Note']
+			},
+		]
+	}
