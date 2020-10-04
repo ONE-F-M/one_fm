b58f40390585216bf24b4954af0f5d8164f81033
Hassan marzooq <marzooq703@gmail.com>
2020-10-04 03:46:40 +0000

    first version of job application portal

---
+++ one_fm/templates/pages/job_application.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+
+def get_context(context):
+    context.show_search = True
+
+
+@frappe.whitelist(allow_guest=True)
+def get_dummy():
+    doc = frappe.get_doc('ERF')
+
+    doc.title = 'Test'
+    doc.save()
+    return doc
+# @frappe.whitelist(allow_guest=True)
+# def get_jobs_info():
+#     job_name = []
+#     job_title = []
+#     job_description = []
+
+#     jobs = frappe.db.sql(
+#         """ select name,job_title,description from `tabJob Opening` where status='Open' """)
+#     if jobs:
+#         for job in jobs:
+#             job_name.append(job[0])
+#             job_title.append(job[1])
+#             job_description.append(job[2])
+#         return job_name, job_title, job_description
+#     else:
+#         return None
