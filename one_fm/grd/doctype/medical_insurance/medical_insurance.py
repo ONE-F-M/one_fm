fb5e9c0632f7881b26b52df491afcac562100c0c
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-10-04 20:53:35 +0530

    fix: Medical Insurance - Group and Individual

--- one_fm/grd/doctype/medical_insurance/medical_insurance.py
+++ one_fm/grd/doctype/medical_insurance/medical_insurance.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import add_years
 
 class MedicalInsurance(Document):
 	def validate(self):
@@ -12,19 +13,19 @@ class MedicalInsurance(Document):
 		self.update_end_date()
 
 	def update_end_date(self):
-		if self.no_of_years and self.no_of_years > 0 and self.start_date:
-			self.end_date = frappe.utils.add_years(self.start_date, self.no_of_years)
+		if self.category == 'Individual' and self.no_of_years and self.no_of_years > 0 and self.start_date:
+			self.end_date = add_years(self.start_date, self.no_of_years)
+		elif self.category == 'Group':
+			for item in self.items:
+				if item.no_of_years and item.no_of_years > 0 and item.start_date:
+					item.end_date = add_years(item.start_date, item.no_of_years)
 
 	def valid_work_permit_exists(self):
 		# TODO: Check valid work permit exists for the employee
 		pass
 
-	def get_employee_data_from_civil_id(self):
-		if self.civil_id:
-			employee_id = frappe.db.exists('Employee', {'one_fm_civil_id': self.civil_id})
-			if employee_id:
-				employee = frappe.get_doc('Employee', employee_id)
-				self.employee_name = employee.employee_name
-				self.gender = employee.gender
-				self.nationality = employee.one_fm_nationality
-				self.passport_expiry_date = employee.valid_upto
+@frappe.whitelist()
+def get_employee_data_from_civil_id(civil_id):
+	employee_id = frappe.db.exists('Employee', {'one_fm_civil_id': civil_id})
+	if employee_id:
+		return frappe.get_doc('Employee', employee_id)
