15d90fc66a896390a7e27fab6d9920249f421ba9
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-10-10 11:56:14 +0530

    fix: Uniform Designation Profile - filter update

--- one_fm/uniform_management/doctype/designation_uniform_profile/designation_uniform_profile.js
+++ one_fm/uniform_management/doctype/designation_uniform_profile/designation_uniform_profile.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Designation Uniform Profile', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.set_query("designation", function() {
+			return {
+				filters: {
+					"one_fm_is_uniform_needed_for_this_job": true
+				}
+			}
+		});
+	}
 });
