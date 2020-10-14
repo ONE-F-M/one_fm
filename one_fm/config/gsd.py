147549e9ea9e7fe392b2593a136d51e6618bba91
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-10-14 13:38:34 +0530

    fix: Purchase Menu in GSD Module

--- one_fm/config/gsd.py
+++ one_fm/config/gsd.py
@@ -116,4 +116,112 @@ def get_data():
 				}
 			]
 		},
+		{
+			"label": _("Purchasing"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Request for Material",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Request for Purchase",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Quotation Comparison Sheet",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Quotation From Supplier",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Purchase Order",
+					"onboard": 1,
+					"dependencies": ["Item", "Supplier"],
+					"description": _("Purchase Orders given to Suppliers."),
+				},
+				{
+					"type": "doctype",
+					"name": "Purchase Invoice",
+					"onboard": 1,
+					"dependencies": ["Item", "Supplier"]
+				},
+				{
+					"type": "doctype",
+					"name": "Purchase Settings",
+					"onboard": 1,
+					"label": _("One FM Purchase Settings")
+				}
+			]
+		},
+		{
+			"label": _("Items"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Item",
+					"onboard": 1,
+					"description": _("All Products or Services."),
+				},
+				{
+					"type": "doctype",
+					"name": "Item Group",
+					"icon": "fa fa-sitemap",
+					"label": _("Item Group"),
+					"link": "Tree/Item Group",
+					"description": _("Tree of Item Groups."),
+				}
+			]
+		},
+		{
+			"label": _("Supplier"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Supplier",
+					"onboard": 1,
+					"description": _("Supplier database."),
+				},
+				{
+					"type": "doctype",
+					"name": "Supplier Group",
+					"description": _("Supplier Group master.")
+				},
+				{
+					"type": "doctype",
+					"name": "Contact",
+					"description": _("All Contacts."),
+				},
+				{
+					"type": "doctype",
+					"name": "Address",
+					"description": _("All Addresses."),
+				},
+
+			]
+		},
+		{
+			"label": _("Stock Transactions"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Stock Entry",
+					"onboard": 1,
+					"dependencies": ["Item"],
+				},
+				{
+					"type": "doctype",
+					"name": "Purchase Receipt",
+					"onboard": 1,
+					"dependencies": ["Item", "Supplier"],
+				}
+			]
+		},
 	]
