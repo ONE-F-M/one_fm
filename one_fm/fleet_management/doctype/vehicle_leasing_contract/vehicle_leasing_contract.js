c5049db3fb4123f07a596433c9e3e9a423c79cbb
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-10-27 03:36:31 +0530

    fix: Create Vehicle from Contract

--- one_fm/fleet_management/doctype/vehicle_leasing_contract/vehicle_leasing_contract.js
+++ one_fm/fleet_management/doctype/vehicle_leasing_contract/vehicle_leasing_contract.js
@@ -22,18 +22,33 @@ var create_vehicle_dialoge = function(frm) {
 			{fieldtype: "Column Break"},
 			{fieldtype: "Float", label: "Fuel Capacity", fieldname: "fuel_capcity"},
 			{fieldtype: "Link", label: "Fuel UOM", fieldname: "uom", options: "UOM", reqd: 1},
-			{fieldtype: "Float", label: "Milage", fieldname: "milage"}
+			{fieldtype: "Float", label: "Milage", fieldname: "milage"},
+			{fieldtype: "Section Break"},
+			{fieldtype: "Data", label: "Chassis No", fieldname: "chassis_no"},
+			{fieldtype: "Date", label: 'Acquisition Date', fieldname: 'acquisition_date'},
+			{fieldtype: "Date", label: 'Registry Expiration Date', fieldname: 'one_fm_registry_expiration_date'},
+			{fieldtype: "Column Break"},
+			{fieldtype: "Data", label: "Color", fieldname: "color"},
+			{fieldtype: "Int", label: "Wheels", fieldname: "wheels"},
+			{fieldtype: "Int", label: "Doors", fieldname: "doors"}
 		],
 		primary_action_label: __("Create"),
 		primary_action : function(){
 			var args = {
+				vd: dialog.get_value('vehicle_detail'),
 				one_fm_purpose_of_use: dialog.get_value('purpose_of_use'),
 				license_plate: dialog.get_value('license_plate'),
 				one_fm_fuel_capcity: dialog.get_value('fuel_capcity') || '',
 				one_fm_milage: dialog.get_value('milage') || '',
 				last_odometer: dialog.get_value('last_odometer'),
 				fuel_type: dialog.get_value('fuel_type'),
-				uom: dialog.get_value('uom')
+				uom: dialog.get_value('uom'),
+				chassis_no: dialog.get_value('chassis_no'),
+				acquisition_date: dialog.get_value('acquisition_date'),
+				one_fm_registry_expiration_date: dialog.get_value('one_fm_registry_expiration_date'),
+				color: dialog.get_value('color'),
+				wheels: dialog.get_value('wheels'),
+				doors: dialog.get_value('doors')
 			}
 			frappe.call({
 				doc: frm.doc,
