74d2c7a7c55dd3568d74423220f8cd57037b9eb4
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-10-28 21:42:52 +0530

    fix: Accommodation Doctypes QRCode live data

--- one_fm/accommodation/doctype/accommodation/accommodation.js
+++ one_fm/accommodation/doctype/accommodation/accommodation.js
@@ -33,7 +33,7 @@ var set_qr_code = function(frm) {
 	let qr_code_html = `{%if doc.name%}
 	<div style="display: inline-block;padding: 5%;">
 	<div class="qr_code_print" id="qr_code_print">
-	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{doc.name}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
+	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{url}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
 	<p>
 		{{qr_details}}
 	</p>
@@ -51,8 +51,10 @@ var set_qr_code = function(frm) {
 	});
 	</script>`
 	var doc = frm.doc;
+	var url = frappe.urllib.get_full_url("/api/method/one_fm.accommodation.utils.accommodation_qr_code_live_details?"
+		+ "docname=" + encodeURIComponent(doc.name))
 	var qr_details = __('{0}<br/>Code: {1}', [doc.accommodation, doc.name])
-	var qr_code = frappe.render_template(qr_code_html,{"doc":doc, "qr_details": qr_details});
+	var qr_code = frappe.render_template(qr_code_html,{"doc":doc, "qr_details": qr_details, "url": url});
 	$(frm.fields_dict["accommodation_qr"].wrapper).html(qr_code);
 	refresh_field("accommodation_qr")
 };
--- one_fm/accommodation/doctype/accommodation_unit/accommodation_unit.js
+++ one_fm/accommodation/doctype/accommodation_unit/accommodation_unit.js
@@ -11,7 +11,7 @@ var set_qr_code = function(frm) {
 	let qr_code_html = `{%if doc.name%}
 	<div style="display: inline-block;padding: 5%;">
 	<div class="qr_code_print" id="qr_code_print">
-	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{doc.name}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
+	<img src="https://barcode.tec-it.com/barcode.ashx?code=MobileQRCode&multiplebarcodes=false&translate-esc=false&data={{url}}&unit=Fit&dpi=150&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=2.5&eclevel=H" alt="">
 	<p>
 		{{qr_details}}
 	</p>
@@ -29,9 +29,11 @@ var set_qr_code = function(frm) {
 	});
 	</script>`
 	var doc = frm.doc;
+	var url = frappe.urllib.get_full_url("/api/method/one_fm.accommodation.utils.accommodation_qr_code_live_details?"
+		+ "docname=" + encodeURIComponent(doc.name))
 	var qr_details = __('{0}<br/>{1}, {2} Floor<br/>Owner Reference: {3}<br/>Code: {4}', [doc.accommodation_name,
 		doc.type, doc.floor_name, doc.owner_reference_number, doc.name])
-	var qr_code = frappe.render_template(qr_code_html,{"doc":doc, "qr_details": qr_details});
+	var qr_code = frappe.render_template(qr_code_html,{"doc":doc, "qr_details": qr_details, "url": url});
 	$(frm.fields_dict["unit_qr"].wrapper).html(qr_code);
 	refresh_field("unit_qr")
 };
