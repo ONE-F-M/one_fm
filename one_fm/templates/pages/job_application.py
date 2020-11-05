055d9126819bda05b344847ab043b97500509ed4
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-11-05 12:19:39 +0530

    fix: Job Application - Portal server method

--- one_fm/templates/pages/job_application.py
+++ one_fm/templates/pages/job_application.py
@@ -31,3 +31,63 @@ def get_dummy():
 #         return job_name, job_title, job_description
 #     else:
 #         return None
+
+@frappe.whitelist(allow_guest=True)
+def easy_apply(applicant_name, applicant_email, applicant_mobile, cover_letter, designation):
+    sender = frappe.get_value("Email Account", filters = {"default_outgoing": 1}, fieldname = "email_id") or None
+
+    subject = """<b>Subject:</b> Application for {0}<br>""".format(designation)
+    message_details = """
+        <b>Name:</b> {0}<br>
+        <b>Email:</b> {1}<br>
+        <b>Mobile:</b> {2}<br>
+        <hr stle='width:50%;'>
+        <b>Message:</b><br><br>
+        {3}
+    """.format(applicant_name, applicant_email, applicant_mobile, cover_letter)
+
+    try:
+        # Notify the HR User
+        frappe.sendmail(sender=sender, recipients= 'j.poil@armor-services.com',
+            content=message_details, subject=contact_subject)
+
+        # Email back to the Applicant
+        applied_subject =  "Thanks for applying for {0}".format(designation)
+        applied_msg =  "<b>We have received your email and our HR team will be responding to you soon.</b>"
+        frappe.sendmail(sender=sender, recipients= applicant_email,
+            content=applied_msg, subject=applied_subject)
+        return 1
+    except:
+        return 0
+
+@frappe.whitelist(allow_guest=True)
+def create_job_applicant(job_opening, first_name, last_name, email_id, languages=None, skills=None, files=None):
+    job_applicant = frappe.db.exists("Job Applicant", {"job_title": job_opening, "email_id": email_id})
+    if job_applicant:
+        return job_applicant
+    else:
+        job_applicant = frappe.new_doc('Job Applicant')
+        job_applicant.job_title = job_opening
+        job_applicant.one_fm_first_name = first_name
+        job_applicant.one_fm_last_name = last_name
+        if languages:
+            set_languages(job_applicant, languages)
+        if skills:
+            set_skills(job_applicant, skills)
+        job_applicant.insert(ignore_permissions=True)
+        return 1, job_applicant.name
+
+def set_skills(doc, skills):
+    for designation_skill in skills:
+        skill = doc.append('one_fm_designation_skill')
+        skill.skill = designation_skill.skill
+        skill.proficiency = designation_skill.proficiency
+
+def set_languages(doc, languages):
+    for language in languages:
+        lang = doc.append('one_fm_languages')
+        lang.language = language.language
+        lang.language_name = language.language_name
+        lang.speak = language.speak
+        lang.read = language.read
+        lang.write = language.write
