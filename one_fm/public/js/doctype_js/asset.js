1276900c53ff23991b8de5d9b75987e3125f97f0
abrarpv97 <frappe@ubuntu.vm>
2020-11-19 06:39:32 +0000

    calculate last day of month

--- one_fm/public/js/doctype_js/asset.js
+++ one_fm/public/js/doctype_js/asset.js
@@ -2,6 +2,7 @@ frappe.ui.form.on('Asset', {
     available_for_use_date: function(frm) {
         var available_for_use_date = new Date(frm.doc.available_for_use_date);
         var depreciation_start_date = new Date(available_for_use_date.getFullYear(), available_for_use_date.getMonth()+1, 0);
+        depreciation_start_date = moment(depreciation_start_date).format("YYYY-MM-DD");
         $.each(frm.doc.finance_books || [], function(i, d) {
             d.depreciation_start_date = depreciation_start_date;
 		});
