c7a364ee7c7a3740a86d946748b983df3211c071
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-12-15 10:05:23 +0530

    fix: Unifrom Issued - Report - updated

--- one_fm/uniform_management/report/uniform_issued_report/uniform_issued_report.py
+++ one_fm/uniform_management/report/uniform_issued_report/uniform_issued_report.py
@@ -42,16 +42,18 @@ def get_data(filters):
 
 	uniform_list=frappe.db.sql(query,as_dict=1)
 	for uniform in uniform_list:
-		pay_back = calculate_amount_pay_back(uniform)
+		pay_back = calculate_amount_pay_back(uniform, filters.returned_on)
 		row = [uniform.employee, uniform.employee_id, uniform.employee_name, uniform.item, uniform.issued_on,
 			uniform.quantity, uniform.returned, uniform.expire_on, pay_back]
 		data.append(row)
 
 	return data
 
-def calculate_amount_pay_back(uniform):
+def calculate_amount_pay_back(uniform, returned_on=None):
 	pay_back = 0
-	returned_on = getdate(today())
+	if not returned_on:
+		returned_on = getdate(today())
+
 	qty_return = uniform.quantity - (uniform.returned or 0)
 	if qty_return > 0 and getdate(uniform.expire_on) > returned_on:
 		per_month_rate = (qty_return * uniform.rate) / month_diff(uniform.expire_on, uniform.issued_on)
