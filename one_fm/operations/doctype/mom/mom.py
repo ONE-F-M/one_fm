b26ecb885667eb2e8ebd8da47904949a31e08460
Kartik Sharma <kartiksharma9319@gmail.com>
2021-01-27 07:22:10 +0000

    fix: Bugfix for attendees error MOM

--- one_fm/operations/doctype/mom/mom.py
+++ one_fm/operations/doctype/mom/mom.py
@@ -10,11 +10,12 @@ from frappe import _
 class MOM(Document):
 	def validate(self):
 		attendees_count = 0
-		for attendee in self.attendees:
+		for attendee in self.attendees[:]:
 			if attendee.attended_meeting:
 				attendees_count = attendees_count + 1
 			else:
 				self.remove(attendee)
+				self.reload()
 
 		if(attendees_count < 1):
 			frappe.throw(_("Please check the attendees present."))
