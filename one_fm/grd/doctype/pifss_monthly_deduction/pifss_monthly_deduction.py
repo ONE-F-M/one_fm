1bca2c6e814203b30a6acfc89e7fcaa7c2e93609
Kartik Sharma <kartiksharma9319@gmail.com>
2021-03-23 15:52:11 +0000

    fix: fixes to PIFSS monthly deduction

--- one_fm/grd/doctype/pifss_monthly_deduction/pifss_monthly_deduction.py
+++ one_fm/grd/doctype/pifss_monthly_deduction/pifss_monthly_deduction.py
@@ -3,10 +3,12 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, erpnext
 from frappe.model.document import Document
-from frappe.utils import flt
+from frappe.utils import flt, getdate
 import pandas as pd
+from one_fm.api.notification import create_notification_log
+from frappe import _
 
 class PIFSSMonthlyDeduction(Document):
 	def after_insert(self):
@@ -16,7 +18,41 @@ class PIFSSMonthlyDeduction(Document):
 		file_doc = frappe.get_value("File", {"file_url": self.attach_report})
 		frappe.set_value("File", file_doc, "attached_to_name", self.name)
 
+	def on_submit(self):
+		missing_list = []
+		for row in self.deductions:
+			if frappe.db.exists("Employee", {"pifss_id_no": row.pifss_id_no}):
+				employee = frappe.db.get_value("Employee", {"pifss_id_no": row.pifss_id_no})
+				employee_contribution_percentage = flt(frappe.get_value("PIFSS Settings", "PIFSS Settings", "employee_contribution"))
+				amount = flt(row.total_subscription * (employee_contribution_percentage / 100), precision=3)
+				print(employee, amount)
+				create_additional_salary(employee, amount)
+			else:
+				missing_list.append(row.pifss_id_no)
+
+		self.notify_payroll(missing_list)
+
+	def notify_payroll(self, employee_list):
+		email = frappe.get_value("HR Settings", "HR Settings", "payroll_notifications_email")
+		subject = _("Urgent Attention Needed")
+		missing_list =  '<br> '.join(['{}'.format(value) for value in employee_list])
+		message = _("Employees linked to the list of PIFSS ID numbers below could not be found. <br> {}".format(missing_list))
+		create_notification_log(subject,message,[email], self)
 		
+		
+def create_additional_salary(employee, amount):
+	additional_salary = frappe.new_doc("Additional Salary")
+	additional_salary.employee = employee
+	additional_salary.salary_component = "Social Security"
+	additional_salary.amount = amount
+	additional_salary.payroll_date = getdate()
+	additional_salary.company = erpnext.get_default_company()
+	additional_salary.overwrite_salary_structure_amount = 1
+	additional_salary.notes = "Social Security Deduction"
+	additional_salary.insert()
+	additional_salary.submit()
+
+
 @frappe.whitelist()
 def import_deduction_data(file_url):
 	url = frappe.get_site_path() + file_url
