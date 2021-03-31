f8a5740a0b27cd020edb409388380314521228e7
Kartik Sharma <kartiksharma9319@gmail.com>
2021-03-31 12:57:22 +0000

    fix: Don't mark employee absent on unscheduled days

--- one_fm/__init__.py
+++ one_fm/__init__.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 from erpnext.hr.doctype.shift_request.shift_request import ShiftRequest 
 from erpnext.hr.doctype.payroll_entry.payroll_entry import PayrollEntry
 from erpnext.hr.doctype.salary_slip.salary_slip import SalarySlip
+from erpnext.hr.doctype.shift_type.shift_type import ShiftType
 from one_fm.api.doc_methods.shift_request import shift_request_submit
 from one_fm.api.doc_methods.payroll_entry import validate_employee_attendance, get_count_holidays_of_employee, get_count_employee_attendance
 from one_fm.api.doc_methods.salary_slip import get_holidays_for_employee,get_leave_details
+from one_fm.api.doc_methods.shift_type import process_auto_attendance
 __version__ = '0.0.1'
 
 
@@ -15,3 +17,4 @@ PayrollEntry.get_count_holidays_of_employee = get_count_holidays_of_employee
 PayrollEntry.get_count_employee_attendance = get_count_employee_attendance
 SalarySlip.get_holidays_for_employee = get_holidays_for_employee
 SalarySlip.get_leave_details = get_leave_details
+ShiftType.process_auto_attendance = process_auto_attendance
