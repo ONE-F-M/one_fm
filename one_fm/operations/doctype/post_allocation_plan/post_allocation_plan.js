13f324afb2a0b6236668392b1176cdefdc6d1cdd
Kartik Sharma <kartiksharma9319@gmail.com>
2021-04-20 12:39:57 +0000

    fix: Show match percentage, calculate skills score on manual employee assignment

--- one_fm/operations/doctype/post_allocation_plan/post_allocation_plan.js
+++ one_fm/operations/doctype/post_allocation_plan/post_allocation_plan.js
@@ -79,6 +79,7 @@ function assign_post(frm, posts, employees){
 			employee: match.employee ? match.employee : undefined,
 			employee_name: match.employee ? match.employee_name : undefined,
 			post: posts[v] ? posts[v].post : undefined, 
+			match_percentage: match.employee ? match.match_percentage : "0%" ,
 			skill_score: match.employee ? match.score_details : undefined
 		}); 
 
@@ -100,29 +101,32 @@ function get_best_employee_match(post, employees){
 		// Check if all the employee skills are matching the post skills.
 		let post_skill_list = skills.map(a => a.skill);
 		let employee_skill_list = employee.skills.map(a => a.skill);
-		let skill_match = employee_skill_list.every(val => post_skill_list.includes(val)) ? 1 : 0;
+		let skill_match = post_skill_list.every(val => employee_skill_list.includes(val)) ? 1 : 0;
 
 		// If all skills match, calculate the skill score
 		if(skill_match){
 			let score_detail = ``;
+			let skill_length = 0;
 			skills.forEach(function(skill, i){
 				employee.skills.forEach(function(e_skill, i){
 					if(skill.skill == e_skill.skill){
+						skill_length++;
 						skill_score += roundNumber(e_skill.proficiency/ skill.proficiency, 2)
 						score_detail += `${skill.skill} : ${e_skill.proficiency}/${skill.proficiency}<br>`;
 					}
 				})
 			})
-			console.log(score_detail);
+			//Match percentage
+			let match_percent = roundNumber((skill_score * 100) / skill_length);
 			//Push the score to list if gender matches
 			if(gender == "Both"){
-				scores.push({'employee': employee.employee, 'employee_name': employee.employee_name, 'final_score': skill_score, 'score_details': score_detail});
+				scores.push({'employee': employee.employee, 'employee_name': employee.employee_name, 'final_score': skill_score, 'score_details': score_detail, 'match_percentage': `${match_percent}%`});
 			}
 			else if(gender == "Male" && employee.gender == "Male"){
-				scores.push({'employee': employee.employee, 'employee_name': employee.employee_name, 'final_score': skill_score, 'score_details': score_detail});
+				scores.push({'employee': employee.employee, 'employee_name': employee.employee_name, 'final_score': skill_score, 'score_details': score_detail, 'match_percentage': `${match_percent}%`});
 			}
 			else if(gender == "Female" && employee.gender == "Female"){
-				scores.push({'employee': employee.employee, 'employee_name': employee.employee_name, 'final_score': skill_score, 'score_details': score_detail});
+				scores.push({'employee': employee.employee, 'employee_name': employee.employee_name, 'final_score': skill_score, 'score_details': score_detail, 'match_percentage': `${match_percent}%`});
 			}
 		}
 
@@ -131,6 +135,30 @@ function get_best_employee_match(post, employees){
 	scores.sort((a, b) => (a.final_score < b.final_score) ? 1 : -1)
 
 	let match = scores.length > 0 ? scores[0] : {'employee': undefined, 'score': 0};
-	console.log(match)
 	return match;
 }
+
+
+frappe.ui.form.on('Post Allocation Employee Assignment', {
+	employee: function(frm, cdt, cdn){
+		let doc = locals[cdt][cdn];
+		if(doc.employee == undefined){
+			frappe.model.set_value("Post Allocation Employee Assignment", doc.name, "employee_name", undefined);
+			frappe.model.set_value("Post Allocation Employee Assignment", doc.name, "skill_score", undefined);
+			frappe.model.set_value("Post Allocation Employee Assignment", doc.name, "match_percentage", undefined);
+		}
+		else{
+			let {employee, post} = doc;
+			frappe.xcall('one_fm.operations.doctype.post_allocation_plan.post_allocation_plan.get_post_employee_data', {employee, post}).then(res => {
+				let {employee, post} = res;
+				let match = get_best_employee_match(post, [employee]);
+				if(match){
+					frappe.model.set_value("Post Allocation Employee Assignment", doc.name, "match_percentage", match.match_percentage);
+					frappe.model.set_value("Post Allocation Employee Assignment", doc.name, "skill_score", match.score_details);
+				} else {
+					frappe.throw(__("Employee doesn't match the Post criteria."))
+				}
+			})
+		}
+	}
+});
