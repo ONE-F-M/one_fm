f04045fa81081c3165e2b6cb2c090ce72742bd44
osamaasim <frappe@ubuntu.vm>
2021-05-26 00:35:11 +0000

    hotFix:Supplier argument removed in RFP

--- one_fm/purchase/doctype/request_for_purchase/request_for_purchase.py
+++ one_fm/purchase/doctype/request_for_purchase/request_for_purchase.py
@@ -180,7 +180,7 @@ def create_purchase_order(**args):
 		po.set_warehouse = args.warehouse
 		# po.schedule_date = add_days(nowdate(), 1)
 		# po.company = args.company
-		po.supplier = args.supplier or None
+		#po.supplier = args.supplier or None
 		po.is_subcontracted = args.is_subcontracted or "No"
 		# po.currency = args.currency or frappe.get_cached_value('Company',  po.company,  "default_currency")
 		po.conversion_factor = args.conversion_factor or 1
