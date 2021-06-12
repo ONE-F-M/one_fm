9bb48d55dfdf1f7808fcf908e0cfae0a5e48f11c
Salwa Shaikh <slwshkh@gmail.com>
2021-06-12 18:27:34 +0300

    Delete twilio_setting.js

--- one_fm/one_fm/doctype/twilio_setting/twilio_setting.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, omar jaber and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Twilio Setting', {
-	// refresh: function(frm) {
-
-	// }
-});
