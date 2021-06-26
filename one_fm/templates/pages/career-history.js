542d060bc41c82e2900ef2ff7c45b09b8b285b58
marzooq703 <marzooq703@gmail.com>
2021-06-26 02:47:04 +0000

    number of company validated

--- one_fm/templates/pages/career-history.js
+++ one_fm/templates/pages/career-history.js
@@ -1,3 +1,14 @@
 // career-history JS here
 
 document.getElementById('erfNumber').value = "987654321";
+document.getElementById('applicantId').value = "12345";
+document.getElementById('historyScore').value = "10";
+document.getElementById('Promotions').value = "100$";
+document.getElementById('Experience').value = "";
+let numberofCompany= document.getElementById('numberofCompany');
+
+function show(){
+    var value = numberofCompany.options[numberofCompany.selectedIndex].value;
+    console.log( value);
+}
+numberofCompany.onchange = show
