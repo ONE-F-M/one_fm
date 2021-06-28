d4aaa8d19cd4017d367ec883d34cc02a0fa88723
marzooq703 <marzooq703@gmail.com>
2021-06-28 19:01:38 +0000

    date calculation validation

--- one_fm/templates/pages/career-history.js
+++ one_fm/templates/pages/career-history.js
@@ -8,7 +8,27 @@ document.getElementById('Experience').value = "";
 let numberofCompany= document.getElementById('numberofCompany');
 
 function show(){
-    var value = numberofCompany.options[numberofCompany.selectedIndex].value;
-    console.log( value);
+    var valuee = numberofCompany.options[numberofCompany.selectedIndex].value;
+    console.log(valuee);
 }
-numberofCompany.onchange = show
+numberofCompany.onchange = show();
+
+// var df= document.getElementById("startDate").value;
+// var dt = .value;  
+
+
+function totelvalue(){
+    var df= Number(document.getElementById("startDate"));
+var dt = Number(document.getElementById('endDate'));   
+var allMonths= dt.getMonth() - df.getMonth() + (12 * (dt.getFullYear() -     df.getFullYear()));
+var allYears= dt.getFullYear() - df.getFullYear();
+var partialMonths = dt.getMonth() - df.getMonth();
+if (partialMonths < 0) {
+allYears--;
+partialMonths = partialMonths + 12;
+}
+var total = allYears + " years " + partialMonths + " months";
+console.log(total); 
+console.log({jaren: allYears, maanden: partialMonths});
+}
+console.log(totelvalue());
