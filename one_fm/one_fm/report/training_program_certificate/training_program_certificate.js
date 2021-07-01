e980129aa220599f6d53024fb73898b1a08c68d4
Ehtesham599 <ehtesham599@gmail.com>
2021-07-01 12:34:30 +0000

    Created Training Program Cetificate report

---
+++ one_fm/one_fm/report/training_program_certificate/training_program_certificate.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, omar jaber and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Training Program Certificate"] = {
+	"filters": [
+		{
+			"fieldname": "training_program_name",
+			"label": _("Training Program Name"),
+			"fieldtype": "Data"
+		}
+	]
+};
