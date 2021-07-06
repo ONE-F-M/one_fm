22c6ab5dba4866c56528a251aeb3b488ffea58d1
Ehtesham599 <ehtesham599@gmail.com>
2021-07-06 07:30:52 +0000

    Added controller to update employee license data

--- one_fm/one_fm/doctype/employee_license_issuance/employee_license_issuance.py
+++ one_fm/one_fm/doctype/employee_license_issuance/employee_license_issuance.py
@@ -16,3 +16,23 @@ class EmployeeLicenseIssuance(Document):
 
 		elif not self.issue_date and self.expiry_date and getdate(self.expiry_date) <= getdate():
 			frappe.throw(_("Expiry date cannot be on or before today"))
+
+	def on_submit(self):
+		for employee in self.employees:
+			if frappe.db.exists("Employee License", {'license_name': self.license_name, 'employee': employee.employee}):
+				doc_el = frappe.get_doc("Employee License", {'license_name': self.license_name, 'employee': employee.employee})
+				doc_el.issue_date = self.issue_date
+				doc_el.expiry_date = self.expiry_date
+				doc_el.save()
+			else:
+				doc_el = frappe.new_doc("Employee License")
+				doc_el.license_name = self.license_name
+				doc_el.employee = employee.employee
+				doc_el.employee_name = get_employee_name(employee.employee)
+				doc_el.issue_date = self.issue_date
+				doc_el.expiry_date = self.expiry_date
+				doc_el.save()
+
+@frappe.whitelist()
+def get_employee_name(employee_code):
+	return frappe.db.get_value("Employee", employee_code, "employee_name")
