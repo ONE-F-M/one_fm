00a6524fc6c967e64e2ed44ad97072540d076452
Amna-onefm <frappe@ubuntu.vm>
2021-07-11 17:47:06 +0000

    Update: transfer paper adding complete functionality

--- one_fm/hiring/doctype/transfer_paper/transfer_paper.py
+++ one_fm/hiring/doctype/transfer_paper/transfer_paper.py
@@ -12,43 +12,66 @@ from frappe.utils import today, add_days, get_url
 from one_fm.grd.doctype.work_permit import work_permit
 from one_fm.api.notification import create_notification_log
 
-
 class TransferPaper(Document):
-
     def validate(self):
-        self.set_salary_from_job_offer()
+        self.set_today_date()
+        self.set_new_salary_from_job_offer()
+        self.set_electronic_signature()
+        self.set_pas_values()
         self.set_grd_values()
-        self.set_electronic_signatory()
+        self.check_signed_workContract_employee_completed()
+        self.get_wp_status()
+        
+    def set_today_date(self):
+        self.db_set('date_of_application',today())
 
-    def set_salary_from_job_offer(self):
+    def set_new_salary_from_job_offer(self):
         salary = frappe.db.get_value('Job Offer',{'job_applicant':self.applicant},['one_fm_job_offer_total_salary'])
         self.db_set('salary', salary) 
-
+        
     def set_grd_values(self):
         if not self.grd_operator:
             self.grd_operator = frappe.db.get_single_value("GRD Settings", "default_grd_operator_transfer")
 
-    def set_electronic_signatory(self):
+    def set_electronic_signature(self):
         doc = frappe.get_doc('Job Applicant',self.applicant)
         if doc.one_fm_signatory_name and doc.one_fm_authorized_signatory:
-            # print(doc.one_fm_signatory_name)
             authorized_list = frappe.get_doc('PAM Authorized Signatory List',doc.one_fm_authorized_signatory)
             for authorized in authorized_list.authorized_signatory:
                 if doc.one_fm_signatory_name == authorized.authorized_signatory_name_arabic:
                     self.db_set('authorized_signature', authorized.signature)
 
-   
+    def set_pas_values(self):
+        doc = frappe.get_doc('Job Applicant',self.applicant)
+        if doc.one_fm_authorized_signatory:
+            authorized_list = frappe.get_doc('PAM Authorized Signatory List',doc.one_fm_authorized_signatory)
+            self.db_set('company_trade_name_arabic', authorized_list.company_name_arabic)
+            self.db_set('issuer_number', authorized_list.pam_issuer_number)
+            self.db_set('pam_file_number', authorized_list.pam_file_number)
 
-    def on_update(self):
-        self.check_signed_workContract_employee_completed()
+    def get_wp_status(self):
+        """
+        This method is getting the last wp record create by this TP and checking its status.
+        TP status will be completed if the wp record is completed. 
+        """
+        if self.tp_status != "Completed" and self.work_permit_ref:
+            wp = frappe.get_doc('Work Permit',self.work_permit_ref)
+            self.db_set('work_permit_status', wp.work_permit_status)
+            if wp.work_permit_status == "Completed":
+                self.db_set('tp_status', "Completed")
 
     def check_signed_workContract_employee_completed(self):
-        if self.signed == "Yes" and "Completed" in self.workflow_state and frappe.db.exists("Employee", {"one_fm_civil_id":self.civil_id}):#employee is created <NEED TO CHECK THE EMPLOYEE LIST ALL TIME>
-            employee = frappe.db.get_value("Employee", {"one_fm_civil_id":self.civil_id})
-            if employee:
-                self.recall_create_transfer_work_permit(employee)#create wp
-                self.get_wp_status()
-                self.notify_grd_transfer_wp_record()
+        """"
+        This method create wp record after tp is submitted and notify operator 
+        """
+        if self.signed == "Yes" and self.tp_status == "Pending By GRD":
+            if frappe.db.exists("Employee", {"one_fm_civil_id":self.civil_id}):#employee is created <NEED TO CHECK THE EMPLOYEE LIST ALL TIME>
+                employee = frappe.db.get_value("Employee", {"one_fm_civil_id":self.civil_id})
+                if employee:
+                    self.recall_create_transfer_work_permit(employee)#create wp
+                    self.notify_grd_transfer_wp_record()
+            else:
+                frappe.throw("No Employee record created yet...")
 
     def notify_grd_transfer_wp_record(self):
         # Getting the wp record the one not rejected and linked to the TP
@@ -59,48 +82,24 @@ class TransferPaper(Document):
             subject = ("Apply for Transfer Work Permit Online")
             message = "<p>Please Apply for Transfer WP Online for <a href='{0}'></a>.</p>".format(page_link, wp_record.employee)
             create_notification_log(subject, message, [self.grd_operator], wp_record)
-
-    def resend_new_wp_record(self):
-        if self.tp_status == "Re-send":
-            self.set_previous_wp_record_rejected()
-            self.check_signed_workContract_employee_completed()
-
-    def set_previous_wp_record_rejected(self):
-        wp = frappe.db.get_value("Work Permit",{'transfer_paper':self.name})
-        if wp:
-            wp_record = frappe.get_doc('Work Permit', wp)
-            wp_record.work_permit_status = "Rejected"
-            wp_record.save()
             
     def recall_create_transfer_work_permit(self,employee):
-        work_permit.create_work_permit_transfer(self.name,employee)
-
-    def get_wp_status(self):
-        wp = frappe.db.get_value("Work Permit",{'transfer_paper':self.name})
-        print(wp)
-        if wp:
-            wp_record = frappe.get_doc("Work Permit",wp)
-            self.work_permit_status = wp_record.work_permit_status
-        self.save()#to set the value of the wp status in th tp dt
-
+        name = work_permit.create_work_permit_transfer(self.name,employee)
+        self.db_set('work_permit_ref', name)
 
 @frappe.whitelist()
 def resend_new_wp_record(doc_name):
-    wp = frappe.db.get_value("Work Permit",{'transfer_paper':doc_name})
+    doc = frappe.get_doc('Transfer Paper',doc_name)
+    if doc.work_permit_ref:
+        wp = frappe.get_doc("Work Permit",doc.work_permit_ref)
+        print("==>new",wp.name)
     if wp:
-        wp_record = frappe.get_doc('Work Permit', wp)
-        wp_record.work_permit_status = "Closed"
-        doc = frappe.get_doc('Transfer Paper',doc_name)
+        wp.db_set('work_permit_status', "Closed")
         doc.check_signed_workContract_employee_completed()
-    wp_record.save()
+        doc.db_set('tp_status', "Pending By GRD")
+        
+    
 
-@frappe.whitelist()
-def closed_old_wp_record(doc_name):
-    wp = frappe.db.get_value("Work Permit",{'transfer_paper':doc_name, 'work_permit_status':'Draft'})
-    if wp:
-        wp_record = frappe.get_doc('Work Permit', wp)
-        wp_record.work_permit_status = "Closed"
-    wp_record.save()
     
 
     
