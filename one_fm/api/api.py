19f9f7446c71eb508c76069709b00dd90b27cc9f
SalShai <slwshkh@gmail.com>
2021-08-12 15:48:35 +0300

    Test Push Notification

--- one_fm/api/api.py
+++ one_fm/api/api.py
@@ -40,3 +40,23 @@ def rename_post(posts):
             rename_doc("Operations Post", post.name, new_name, force=True)
         except Exception as e:
             print(frappe.get_traceback())
+
+@frappe.whitelist()
+def final_reminder_notification():
+    # This registration token comes from the client FCM SDKs.
+    registration_token = 'YOUR_REGISTRATION_TOKEN'
+
+    # See documentation on defining a message payload.
+    message = messaging.Message(
+        data={
+            'score': '850',
+            'time': '2:45',
+        },
+        token=registration_token,
+    )
+
+    # Send a message to the device corresponding to the provided
+    # registration token.
+    response = messaging.send(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
