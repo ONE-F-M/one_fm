b9a3a65c8585ad06991d2ca5b26e65eaf1675bf1
Ehtesham599 <ehtesham599@gmail.com>
2021-09-05 12:02:22 +0000

    Fix: Set values in PS fields

--- one_fm/operations/doctype/operations_post/operations_post.py
+++ one_fm/operations/doctype/operations_post/operations_post.py
@@ -11,17 +11,13 @@ from frappe.utils import cstr, getdate, add_to_date
 import pandas as pd
 
 class OperationsPost(Document):
-	def before_insert(self):
+	def after_insert(self):
 		start_date = None
 		end_date = None
-
+		post_abbrv = frappe.db.get_value("Post Type", self.post_template, ["post_abbrv"])
 		if frappe.db.exists("Contracts", {'project': self.project}):
-			contract, start_date, end_date = frappe.db.get_value("Contracts", {'project': self.project}, ["name", "start_date", "end_date"])
-			if not start_date or not end_date:
-				frappe.throw(_("Please set contract dates for contract: {contract}".format(contract=contract)))
-		else:
-			frappe.throw(_("No contract linked with project: {project}".format(project=self.project)))
-		frappe.enqueue(set_post_active, post=self, start_date=start_date, end_date=end_date, is_async=True, queue="long")
+			start_date, end_date = frappe.db.get_value("Contracts", {'project': self.project}, ["start_date", "end_date"])
+		frappe.enqueue(set_post_active, post=self, post_type=self.post_template, post_abbrv=post_abbrv, shift=self.site_shift, site=self.site, project=self.project, start_date=start_date, end_date=end_date, is_async=True, queue="long")
 
 	def on_update(self):
 		self.validate_name()
@@ -33,10 +29,15 @@ class OperationsPost(Document):
 		
 
 @frappe.whitelist()
-def set_post_active(post, start_date, end_date):
+def set_post_active(post, post_type, post_abbrv, shift, site, project, start_date, end_date):
 	for date in	pd.date_range(start=start_date, end=end_date):
 		sch = frappe.new_doc("Post Schedule")
 		sch.post = post.name
+		sch.post_type = post_type
+		sch.post_abbrv = post_abbrv
+		sch.shift = shift
+		sch.site = site
+		sch.project = project
 		sch.date = cstr(date.date())
 		sch.post_status = "Planned"
 		sch.save()
