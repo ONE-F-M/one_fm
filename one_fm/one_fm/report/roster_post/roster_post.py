f04c05d8e560dd2a600c012a317f976d0cd10cdc
osamaasim <ehtesham599@gmail.com>
2021-10-03 08:16:47 +0000

    Refactored roster reports

--- one_fm/one_fm/report/roster_post/roster_post.py
+++ one_fm/one_fm/report/roster_post/roster_post.py
@@ -8,7 +8,6 @@ from calendar import monthrange
 from frappe import msgprint
 import pandas as pd
 
-
 post_types_not_filled = set()
 
 def execute(filters=None):
@@ -16,92 +15,130 @@ def execute(filters=None):
 		days_in_month = monthrange(cint(filters.year), cint(filters.month))[1]
 		filters["start_date"] = filters.year + "-" + filters.month + "-" + "1"
 		filters["end_date"] = add_to_date(filters["start_date"], days = days_in_month-1)
-	columns = get_columns(filters) 
-	data, chart_data = get_data(filters)
-	return columns, data, None, chart_data
+	# columns = get_columns(filters) 
+	# data, chart_data = get_data(filters)
+	return RosterPost(filters).run()
 
-def get_columns(filters):
-	return [
-		_("Date") + ":Date:150",
-		_("Active Posts") + ":Int:150",
-		_("Posts Off") + ":Int:250",
-		_("Posts Filled") + ":Int:250",
-		_("Posts Not Filled") + ":Int:250",
-		_("Result") + ":Data:150"
-	]
 
-def get_data(filters):
-	data = []
-	labels = []
-	datasets = [
-		{"name": "Active Posts\n\n\n", "values": []},
-		{"name": "Posts Off\n\n\n", "values": []},
-		{"name": "Posts Filled\n\n\n", "values": []},
-		{"name": "Posts Not Filled\n\n\n", "values": []},
-	]
-	chart = {}
+class RosterPost(object):
+	def __init__(self, filters=None):
+		self.filters = frappe._dict(filters or {})
 
-	if filters:
-		for date in pd.date_range(start=filters["start_date"], end=filters["end_date"]):
-			active_posts = len(frappe.db.get_list("Post Schedule", {'post_status': 'Planned', 'date': date}, ["post_type"]))
-			posts_off = len(frappe.db.get_list("Post Schedule", {'post_status': 'Post Off', 'date': date}))
+	def run(self):
+		self.get_columns()
+		self.get_data()
 
-			posts_filled_count = 0
-			posts_not_filled_count = 0
+		return self.columns, self.data, None, self.chart
 
-			post_types = frappe.db.get_list("Post Schedule", ["distinct post_type", "post_abbrv"])
-			for post_type in post_types:
-				# For each post type, get all post schedules and employee schedules assigned to the post type
-				posts_count = len(frappe.db.get_list("Post Schedule", {'post_type': post_type.post_type, 'date': date, 'post_status': 'Planned'}))
-				posts_fill_count = len(frappe.db.get_list("Employee Schedule", {'post_type': post_type.post_type, 'date': date, 'employee_availability': 'Working'}))
+	def get_columns(self):
+		self.columns = [{
+            "label": _("Date"),
+            "fieldname": "date",
+            "fieldtype": "Date",
+            "width": 150
+        }]
 
-				# Compare count of post schedule vs employee schedule for the given post type, compute post filled/not filled count
-				if posts_count == posts_fill_count:
-					posts_filled_count = posts_filled_count + posts_fill_count
-				elif posts_count > posts_fill_count:
-					posts_filled_count = posts_filled_count + posts_fill_count
-					posts_not_filled_count = posts_not_filled_count + (posts_count - posts_fill_count)
-					global post_types_not_filled
-					post_types_not_filled.add(post_type.post_type)
+		self.columns.append({
+			"label": _("Active Posts"),
+            "fieldname": "active_posts",
+            "fieldtype": "Int",
+            "width": 150
+		})
+		self.columns.append({
+			"label": _("Posts Off"),
+            "fieldname": "posts_off",
+            "fieldtype": "Int",
+            "width": 250
+		})
+		self.columns.append({
+			"label": _("Posts_filled"),
+            "fieldname": "posts_filled",
+            "fieldtype": "Int",
+            "width": 250
+		})
+		self.columns.append({
+			"label": _("Posts Not filled"),
+            "fieldname": "posts_not_filled",
+            "fieldtype": "Int",
+            "width": 250
+		})
+		self.columns.append({
+			"label": _("Result"),
+            "fieldname": "result",
+            "fieldtype": "Data",
+            "width": 150
+		})
 
-			result = "OK"
-			if posts_not_filled_count > 0:
-				result = "NOT OK"
+	def get_data(self):
+		self.data = []
+		labels = []
+		datasets = [
+			{"name": "Active Posts\n\n\n", "values": []},
+			{"name": "Posts Off\n\n\n", "values": []},
+			{"name": "Posts Filled\n\n\n", "values": []},
+			{"name": "Posts Not Filled\n\n\n", "values": []},
+		]
+		self.chart = {}
 
-			row = [
-				cstr(date).split(" ")[0],
-				active_posts,
-				posts_off,
-				posts_filled_count,
-				posts_not_filled_count,
-				result
-			]
+		if self.filters:
+			for date in pd.date_range(start=self.filters["start_date"], end=self.filters["end_date"]):
+				active_posts = len(frappe.db.get_list("Post Schedule", {'post_status': 'Planned', 'date': date}, ["post_type"]))
+				posts_off = len(frappe.db.get_list("Post Schedule", {'post_status': 'Post Off', 'date': date}))
 
-			data.append(row)
+				posts_filled_count = 0
+				posts_not_filled_count = 0
 
-			labels.append("...")
-			datasets[0]["values"].append(active_posts)
-			datasets[1]["values"].append(posts_off)
-			datasets[2]["values"].append(posts_filled_count)
-			datasets[3]["values"].append(posts_not_filled_count)
+				post_types = frappe.db.get_list("Post Schedule", ["distinct post_type", "post_abbrv"])
+				for post_type in post_types:
+					# For each post type, get all post schedules and employee schedules assigned to the post type
+					posts_count = len(frappe.db.get_list("Post Schedule", {'post_type': post_type.post_type, 'date': date, 'post_status': 'Planned'}))
+					posts_fill_count = len(frappe.db.get_list("Employee Schedule", {'post_type': post_type.post_type, 'date': date, 'employee_availability': 'Working'}))
 
-		chart = {
-			"data": {
-				"labels": labels,
-				"datasets": datasets
+					# Compare count of post schedule vs employee schedule for the given post type, compute post filled/not filled count
+					if posts_count == posts_fill_count:
+						posts_filled_count = posts_filled_count + posts_fill_count
+					elif posts_count > posts_fill_count:
+						posts_filled_count = posts_filled_count + posts_fill_count
+						posts_not_filled_count = posts_not_filled_count + (posts_count - posts_fill_count)
+						global post_types_not_filled
+						post_types_not_filled.add(post_type.post_type)
+
+				result = "OK"
+				if posts_not_filled_count > 0:
+					result = "NOT OK"
+
+				row = [
+					cstr(date).split(" ")[0],
+					active_posts,
+					posts_off,
+					posts_filled_count,
+					posts_not_filled_count,
+					result
+				]
+
+				self.data.append(row)
+
+				labels.append("...")
+				datasets[0]["values"].append(active_posts)
+				datasets[1]["values"].append(posts_off)
+				datasets[2]["values"].append(posts_filled_count)
+				datasets[3]["values"].append(posts_not_filled_count)
+
+			self.chart = {
+				"data": {
+					"labels": labels,
+					"datasets": datasets
+				}
 			}
-		}
 
-		chart["type"] = "line"
-
-	return data, chart
+			self.chart["type"] = "line"
 
 
-def get_active_posts(date):
-	return frappe.db.get_list("Post Schedule", {'post_status': 'Planned', 'date': date}, ["post_type"])
+	def get_active_posts(self, date):
+		return frappe.db.get_list("Post Schedule", {'post_status': 'Planned', 'date': date}, ["post_type"])
 
-def get_posts_off(date):
-	return frappe.db.get_list("Post Schedule", {'post_status': 'Post Off', 'date': date})
+	def get_posts_off(self, date):
+		return frappe.db.get_list("Post Schedule", {'post_status': 'Post Off', 'date': date})
 
 @frappe.whitelist()
 def get_years():
