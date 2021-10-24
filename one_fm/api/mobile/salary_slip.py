d425d9ee78b516fb1cdde96dafa37105acfe4228
SalShai <slwshkh@gmail.com>
2021-10-24 08:55:01 +0300

    Fetch Salary Slip Details

--- one_fm/api/mobile/salary_slip.py
+++ one_fm/api/mobile/salary_slip.py
@@ -11,3 +11,14 @@ def get_salary_slip_list(employee_id):
         print(frappe.get_traceback())
         frappe.log_error(frappe.get_traceback())
         return frappe.utils.response.report_error(e)
+
+#This function returns the details of  given salary slips.
+@frappe.whitelist()
+def salary_slip_details(salary_slip_id):
+    try:
+        salary_slip_details = frappe.get_doc("Salary Slip", salary_slip_id, '*' ) 
+        return salary_slip_details
+    except Exception as e:
+        print(frappe.get_traceback())
+        frappe.log_error(frappe.get_traceback())
+        return frappe.utils.response.report_error(e)
