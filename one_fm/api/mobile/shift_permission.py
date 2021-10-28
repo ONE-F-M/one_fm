7fa467fceef61ffb15433c527a5963dcac2f3b2f
Amna-onefm <80109720+Amna-onefm@users.noreply.github.com>
2021-10-28 08:09:18 +0300

    Update comments

--- one_fm/api/mobile/shift_permission.py
+++ one_fm/api/mobile/shift_permission.py
@@ -1,18 +1,17 @@
 import frappe
 from frappe import _
-#one_fm.api.mobile.shift_permission.create_shift_permission
-#This method is creating shift permission record and setting the mendatory fields
+
+# This method is creating shift permission record and setting the the shift details
 @frappe.whitelist()
 def create_shift_permission(employee,permission_type,date,reason,leaving_time=None,arrival_time=None):
-    
     """
 	Params:
-	employee: HO employee erp id
-    permission_type,date,reason,leaving_time,arrival_time
+	employee: HO employee ERP id
+    permission_type, date, reason, leaving_time, arrival_time
+    Return: shift permission record
 	"""
-
     try:
-        shift,type,assigned_shift,shift_supervisor = set_shift_details(employee,date)
+        shift,type,assigned_shift,shift_supervisor = get_shift_details(employee,date)
         if shift and type and assigned_shift and shift_supervisor:
             doc = frappe.new_doc('Shift Permission')
             doc.employee = employee
@@ -30,27 +29,24 @@ def create_shift_permission(employee,permission_type,date,reason,leaving_time=No
             doc.save()
             frappe.db.commit()
             return doc
-        else:
-            frappe.throw(_("Shift details are missing. Please make sure date is correct."))
-  
+
     except Exception as e:
         frappe.log_error(frappe.get_traceback())
         return frappe.utils.response.report_error(e)
 
-#Fetching shift details of the employee and add it in shift permission record
-def set_shift_details(employee,date):
+# Fetching shift details of the employee and adding them in shift permission record
+def get_shift_details(employee,date):
     shift, type = frappe.db.get_value('Employee Schedule',{'employee':employee,'employee_availability':'Working','date':date,'roster_type':'Basic'},['shift','shift_type']) 
     shift_supervisor = frappe.db.get_value('Operations Shift',{'name':shift},['supervisor'])
-    assigned_shift = frappe.db.get_value('Shift Assignment',{'employee':employee,'start_date':date},['name']) #start date and end date of HO employee are the same in the shift assignment
+    assigned_shift = frappe.db.get_value('Shift Assignment',{'employee':employee,'start_date':date},['name']) # start date and end date of HO employee are the same in the shift assignment
     return shift,type,assigned_shift,shift_supervisor
     
-
-#This api is getting employee_id and returning the roles
+# This method is returning employee roles upon employee_id
 @frappe.whitelist()
 def get_employee_roles(employee_id):
     """
 	Params:
-	employee: HO employee erp id
+	employee: HO employee ERP id
     Returns: roles of employee
 	"""
     user_id = frappe.db.get_value('Employee',{'name':employee_id},['user_id'])
