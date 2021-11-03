effde8bf71303eab13b34a43c4db71501933c826
SalShai <slwshkh@gmail.com>
2021-11-03 16:20:26 +0300

    Add error response for leave overlapping

--- one_fm/api/mobile/Leave_application.py
+++ one_fm/api/mobile/Leave_application.py
@@ -6,6 +6,7 @@ import datetime
 from one_fm.one_fm.doctype.leave_application.leave_application import get_leave_approver,get_employee_schedule, notifier_leave
 import collections
 from one_fm.api.tasks import get_action_user,get_notification_user
+from one_fm.operations.page.checkpoint_scan.checkpoint_scan import response
 
 @frappe.whitelist()
 def get_leave_detail(employee_id):
@@ -73,30 +74,39 @@ def leave_notify(docname,status):
     doc.submit()
     frappe.db.commit()
 
+#This function is the api to create a new leave notification.
+#bench execute --kwargs "{'employee':'HR-EMP-00002','from_date':'2021-11-02','to_date':'2021-11-02','leave_type':'Annual Leave','reason':'fever'}"  one_fm.api.mobile.Leave_application.create_new_leave_application
 @frappe.whitelist()
-def create_new_leave_application(employee,from_date,to_date,leave_type,reason,half_day,half_day_date=None):
+def create_new_leave_application(employee,from_date,to_date,leave_type,reason):
     """
-	Params:
-	employee: erp id
+    Params:
+    employee: erp id
     from_date,to_date,half_day_date= date in YYYY-MM-DD format
     leave_type=from leave policy
     half_day=1 or 0
-	"""
+    """
     #get Leave Approver of the employee.
     leave_approver = get_leave_approver(employee)
-    if leave_approver:
-        #if sick leave, automatically accept the leave application
-        if leave_type == "Sick Leave":
-            doc = new_leave_application(employee,from_date,to_date,leave_type,"Approved",reason,half_day,half_day_date,leave_approver)
-            doc.submit()
-            frappe.db.commit()
-        #else keep it open and that sends the approval notification to the 'leave approver'.
-        else:
-            doc = new_leave_application(employee,from_date,to_date,leave_type,"Open",reason,half_day,half_day_date,leave_approver)
-    return doc
+    #check if leave exist and overlaps with the given date (StartDate1 <= EndDate2) and (StartDate2 <= EndDate1)
+    leave_exist = frappe.get_list("Leave Application", filters={"employee": employee,'from_date': ['>=', to_date],'to_date' : ['>=', from_date]})
+    print(leave_exist)
+    if leave_exist:
+        return response('You have already applied leave for this date.', 400)
+    else:
+        if leave_approver:
+            #if sick leave, automatically accept the leave application
+            if leave_type == "Sick Leave":
+                doc = new_leave_application(employee,from_date,to_date,leave_type,"Approved",reason,leave_approver)
+                doc.submit()
+                frappe.db.commit()
+            #else keep it open and that sends the approval notification to the 'leave approver'.
+            else:
+                doc = new_leave_application(employee,from_date,to_date,leave_type,"Open",reason,leave_approver)
+        return doc
 
-@frappe.whitelist()
-def new_leave_application(employee,from_date,to_date,leave_type,status,reason,half_day,half_day_date,leave_approver):
+#create new leave application doctype
+frappe.whitelist()
+def new_leave_application(employee,from_date,to_date,leave_type,status,reason,leave_approver):
      try:
         leave = frappe.new_doc("Leave Application")
         leave.employee=employee
@@ -104,8 +114,6 @@ def new_leave_application(employee,from_date,to_date,leave_type,status,reason,ha
         leave.from_date=from_date
         leave.to_date=to_date
         leave.description=reason or "None"
-        leave.half_day=half_day
-        leave.half_day_date=half_day_date
         leave.follow_via_email=1
         leave.status=status
         leave.leave_approver = leave_approver
