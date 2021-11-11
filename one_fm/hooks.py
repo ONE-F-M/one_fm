86c52fa5b081286e9c0796bf4b328f5b15885008
Ehtesham <ehtesham599@gmail.com>
2021-11-11 10:59:45 +0300

    Fixed syntax in hooks

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -230,7 +230,7 @@ doc_events = {
 		"on_submit": [
            		"one_fm.api.tasks.update_shift_details_in_attendance",
            		"one_fm.api.doc_events.create_additional_salary_for_overtime"
-		]
+		],
     		"on_cancel": "one_fm.one_fm.utils.attendance_on_cancel"
 	},
 	"Asset":{
