2a64f76d50e338ccaeaf479172aa7284b218952d
Ehtesham599 <ehtesham599@gmail.com>
2021-12-03 00:58:09 +0000

    Added button to download file

--- one_fm/public/js/doctype_js/payroll_entry.js
+++ one_fm/public/js/doctype_js/payroll_entry.js
@@ -1,9 +1,37 @@
 frappe.ui.form.on('Payroll Entry', {
     refresh: function(frm) {
-			frm.set_indicator_formatter('employee',
-					function(doc) {
-						return (doc.justification_needed_on_deduction == 1) ? "orange" : "green";
-					}
-			);
+		if (frm.doc.salary_slips_created == 1){
+			frm.add_custom_button(__("Download Payroll Bank Export File"), function() {
+				let payroll_entry = frm.doc.name
+				// Fetch URi for the export file
+				frappe.xcall("one_fm.api.doc_methods.payroll_entry.get_excel_payroll_export_file", {payroll_entry})
+					.then(res => {
+						let {filename, uri} = res
+						if(uri && filename) {
+							// Downaload file
+							download(uri, filename)
+						}else{
+							frappe.throw(_("No Export file found."))
+						}
+					})
+			}).addClass("btn-primary");
+		}
+		frm.set_indicator_formatter('employee',
+				function(doc) {
+					return (doc.justification_needed_on_deduction == 1) ? "orange" : "green";
+				}
+		);
     }
 });
+
+/**
+ * This function downloads the file from a given URi provided a filename.
+ * @param {string} uri - uri of the file
+ * @param {string} name - name of the file
+ */
+function download(uri, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    link.click();
+}
