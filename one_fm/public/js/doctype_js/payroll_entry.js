381cb5c0b48ad72761e0e0d19c6263a764ddb393
Ehtesham599 <ehtesham599@gmail.com>
2021-12-05 10:01:02 +0000

    Rework on download

--- one_fm/public/js/doctype_js/payroll_entry.js
+++ one_fm/public/js/doctype_js/payroll_entry.js
@@ -3,17 +3,19 @@ frappe.ui.form.on('Payroll Entry', {
 		if (frm.doc.salary_slips_created == 1){
 			frm.add_custom_button(__("Download Payroll Bank Export File"), function() {
 				let payroll_entry = frm.doc.name
-				// Fetch URi for the export file
-				frappe.xcall("one_fm.api.doc_methods.payroll_entry.get_excel_payroll_export_file", {payroll_entry})
+				// Fetch URL for the export file
+				frappe.xcall("one_fm.api.doc_methods.payroll_entry.download_excel_payroll_export_file", {payroll_entry})
 					.then(res => {
-						let {filename, uri} = res
-						if(uri && filename) {
-							// Downaload file
-							download(uri, filename)
+						let {app_url, path, filename} = res;
+						if (app_url && path && filename){
+							let url = app_url + path + filename
+							window.open(url, "Download");
 						}else{
-							frappe.throw(_("No Export file found."))
+							frappe.throw(_("Invalid URL"))
 						}
-					})
+					}).catch(e =>{
+						console.log(e);
+					});
 			}).addClass("btn-primary");
 		}
 		frm.set_indicator_formatter('employee',
@@ -23,15 +25,3 @@ frappe.ui.form.on('Payroll Entry', {
 		);
     }
 });
-
-/**
- * This function downloads the file from a given URi provided a filename.
- * @param {string} uri - uri of the file
- * @param {string} name - name of the file
- */
-function download(uri, name) {
-    var link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    link.click();
-}
