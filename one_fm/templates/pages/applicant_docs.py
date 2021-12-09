a03a8b8483f620dcf23214178d2d945381052133
Ehtesham599 <ehtesham599@gmail.com>
2021-12-09 05:54:27 +0000

    change filename and code refactor

--- one_fm/templates/pages/applicant_docs.py
+++ one_fm/templates/pages/applicant_docs.py
@@ -1,58 +1,42 @@
 import os, io
 from google.cloud import vision
-import pandas as pd
 import frappe
+from frappe.utils import cstr
 import json
 import base64
 
-
-@frappe.whitelist(allow_guest=True)
-def fetch_text_for_kuwaiti_civilid(image):
+@frappe.whitelist()
+def fetch_text_for_kuwaiti_civilid(images):
     """This API redirects the image fetched from frontend and 
     runs it though Google Vision API, each side at a time.
 
     Args:
-        image (json): Fetched from frontend, consist of two images(front and back)
+        images (json): Consist of two base64 encoded images(front_side, back_side).
 
     Returns:
-        text_detected: dictionary consisting of text fetched from both front and back end.
+        text_detected: dictionary consisting of text fetched from both front and back side.
     """    
-    #initialize Google Vision
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = frappe.utils.cstr(frappe.local.site)+'/private/files/inbound-theory-254808-c1818ddd7f9e.json'
+    try:
+        #initialize google vision client library
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = cstr(frappe.local.site) + frappe.local.conf.google_application_credentials
+        client = vision.ImageAnnotatorClient()
+        
+        # Load the Images
+        image_files = json.loads(images)
 
-    client = vision.ImageAnnotatorClient()
-    
-    # Load the Images
-    image_files = json.loads(image)
+        front_image = upload_image(image_files["front_side"],"image1.png")
+        back_image = upload_image(image_files["back_side"],"image2.png")
 
-    image_path_1 = upload_image(image_files["Image1"],"image1.png")
-    image_path_2 = upload_image(image_files["Image2"],"image2.png")
+        front_text = front_side_kuwaiti_civil_id(front_image, client)
+        back_text = back_side_kuwaiti_civil_id(back_image, client)
 
-    """
-    # or we can pass the image url
-    image = vision.types.Image()
-    image.source.image_uri = 'https://edu.pngfacts.com/uploads/1/1/3/2/11320972/grade-10-english_orig.png'
-    """
+        text_detected = front_text.update(back_text)
+        print(text_detected)
 
-    front_text = front_side_kuwaiti_civil_id(image_path_1, client)
-    back_text = back_side_kuwaiti_civil_id(image_path_2, client)
-
-    text_detected = front_text.update(back_text)
-    print(text_detected)
-
-    if text_detected:
         return text_detected
-    else:
-        return "Error"
-
-@frappe.whitelist()
-def upload_image(image, filename):
-    content = base64.b64decode(image)
-    OUTPUT_IMAGE_PATH = frappe.utils.cstr(frappe.local.site)+"/private/files/user/"+filename
-
-    with open(OUTPUT_IMAGE_PATH, "wb") as fh:
-        fh.write(content)
-    return OUTPUT_IMAGE_PATH
+    
+    except Exception as e:
+        frappe.throw(e)
 
 @frappe.whitelist(allow_guest=True)
 def front_side_kuwaiti_civil_id(image_path, client):
@@ -60,20 +44,16 @@ def front_side_kuwaiti_civil_id(image_path, client):
     with io.open(image_path, 'rb') as image_file:
         content = image_file.read()
 
-    # construct an iamge instanceimage = vision.Image(content=content) 
     image = vision.Image(content=content)
 
-    # annotate Image Response
     response = client.text_detection(image=image)  # returns TextAnnotation
-    df = pd.DataFrame(columns=['locale', 'description'])
 
     texts = response.text_annotations
     
     emp_det = {}
     assemble = {}
     index = 0
-    keyword_index = {}
-    #print(emp_det)
+
     for index in range(1,len(texts)):
         assemble[index] = texts[index].description
     
@@ -93,7 +73,7 @@ def front_side_kuwaiti_civil_id(image_path, client):
     emp_det["Arabic Name"]= ""
     for i in range(find_index(assemble,"No")+1,find_index(assemble,"Name")-1):
         emp_det["Arabic Name"] = emp_det["Arabic Name"] + texts[i].description + " "
-    #print(emp_det)
+
     return emp_det
 
 @frappe.whitelist(allow_guest=True)
@@ -102,20 +82,15 @@ def back_side_kuwaiti_civil_id(image_path, client):
     with io.open(image_path, 'rb') as image_file:
         content = image_file.read()
 
-    # construct an iamge instanceimage = vision.Image(content=content) 
     image = vision.Image(content=content)
 
-    # annotate Image Response
     response = client.text_detection(image=image)  # returns TextAnnotation
-    df = pd.DataFrame(columns=['locale', 'description'])
 
     texts = response.text_annotations
     
     emp_det = {}
     assemble = {}
     index = 0
-    keyword_index = {}
-    #print(emp_det)
     
     for index in range(1,len(texts)):
         assemble[index] = texts[index].description
@@ -125,24 +100,26 @@ def back_side_kuwaiti_civil_id(image_path, client):
         emp_det["PACI No."] = " "
     
     return emp_det
-    
-    """
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
-
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
-    """
 
 def find_index(dictionary, word):
     for d in dictionary:
         if dictionary[d] == word:
             return d
+        
+def upload_image(image, filename):
+    """ This method writes a file to a server directory
+
+    Args:
+        image (str): Base64 encoded image
+        filename (str): Name of the file
+
+    Returns:
+        [str]: Path to uploaded image
+    """
+    content = base64.b64decode(image)
+    image_path = cstr(frappe.local.site)+"/private/files/user/"+filename
+
+    with open(image_path, "wb") as fh:
+        fh.write(content)
+    
+    return image_path
