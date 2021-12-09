3ac4727f8c8d0123cca793d18e1b5ce6845c1e5f
Jamsheer PP <kuttypjamsheer@gmail.com>
2021-12-09 15:08:26 +0530

    fix: Magic Link - send magic link method

--- one_fm/one_fm/doctype/magic_link/magic_link.py
+++ one_fm/one_fm/doctype/magic_link/magic_link.py
@@ -5,6 +5,7 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils.password import decrypt, encrypt
+from frappe.utils import get_url
 
 class MagicLink(Document):
 	pass
@@ -80,3 +81,26 @@ def authorize_magic_link(encrypted_magic_link, doctype, link_for):
 			return magic_link_exist
 		else:
 			frappe.throw(_("Not Permitted ! Magic Link is Expired or Not exisit!!"), frappe.PermissionError)
+
+def send_magic_link(doctype, name, link_for, recipients, url_prefix, msg, subject):
+	'''
+	Method used to send the magic Link for link_for doctype to the doctype
+	args:
+		doctype: Reference Doctype name, from which the magic link created
+		name: Reference Doctype ID, from which the magic link created
+		link_for: The doctype for which the magic link is created (example: Career History)
+		recipients: array of recipient email(s) (example: [jam@one-fm.com])
+		url_prefix: prefix to the url (example: "/career_history?magic_link=")
+		msg: Message content to the Recipients without the magic link
+		subject: Subject of the email
+	'''
+	# Check if magic_link exisit for the Job Applicant and not expired
+	magic_link = get_magic_link(doctype, name, link_for)
+	if magic_link:
+		# Encrypt the magic link benfore send
+		encrypted_magic_link = get_encrypted_magic_link(magic_link)
+		# Email Magic Link to the Recipients
+		sender = frappe.get_value("Email Account", filters={"default_outgoing": 1}, fieldname="email_id") or None
+		magic_link_url = get_url(url_prefix) + encrypted_magic_link
+		msg += "<br/><a href='{0}'>Magic Link</a>".format(magic_link_url)
+		frappe.sendmail(sender=sender, recipients=recipients, content=msg, subject=subject)
