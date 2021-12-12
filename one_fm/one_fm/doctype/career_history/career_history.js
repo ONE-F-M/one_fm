9a38f09a1f4b4f5c4fb4789d3807e65e9336dcc5
Jamsheer PP <kuttypjamsheer@gmail.com>
2021-12-13 04:35:37 +0530

    fix: Career History - Doctype - Updated and Optimised

--- one_fm/one_fm/doctype/career_history/career_history.js
+++ one_fm/one_fm/doctype/career_history/career_history.js
@@ -10,20 +10,18 @@ frappe.ui.form.on('Career History', {
 				}
 			};
 		});
-		set_company_options(frm);
-		set_promotions_and_salary_hike_field(frm);
+		if(frm.doc.total_years_of_experience){
+			set_total_years_of_experience_str(frm, frm.doc.total_years_of_experience);;
+		}
 	},
 	job_applicant: function(frm) {
     set_job_applicant_details(frm);
 	},
-  number_of_companies: function(frm) {
-    if(frm.doc.number_of_companies <= 0){
-      frm.set_value('number_of_companies', 1);
-    }
-    set_career_history_company_item(frm);
-  },
 	pass_to_next_interview: function(frm) {
 		confirm_score_action(frm);
+	},
+	total_years_of_experience: function(frm) {
+		set_total_years_of_experience_str(frm, frm.doc.total_years_of_experience);
 	}
 });
 
@@ -50,141 +48,84 @@ var confirm_score_action = function(frm) {
 };
 
 frappe.ui.form.on('Career History Company', {
-	company_name: function(frm) {
-		set_company_options(frm);
+	start_date: function(frm, cdt, cdn) {
+		calculate_promotions_and_experience(frm);
 	},
-	job_start_date: function(frm, cdt, cdn) {
-    calculate_years_of_experience(frm, cdt, cdn);
+	end_date: function(frm, cdt, cdn) {
+		calculate_promotions_and_experience(frm);
 	},
-  job_end_date: function(frm, cdt, cdn) {
-    calculate_years_of_experience(frm, cdt, cdn);
-  },
-  years_of_experience: function(frm) {
-    calculate_total_years_of_experience(frm);
-  },
-  career_history_company_remove: function(frm, cdt, cdn) {
-    if(frm.doc.career_history_company.length < frm.doc.number_of_companies){
-      frappe.msgprint(__('Not Permitted, Please Update the Number of Companies to Remove Row.'));
-      frappe.model.add_child(frm.doc, 'Career History Company', 'career_history_company');
-    }
-  },
-	did_you_get_any_promotion: function(frm, cdt, cdn) {
-		validate_promotion_item_exists(frm, cdt, cdn);
-		set_company_options(frm);
-		set_promotions_and_salary_hike_field(frm);
+	job_title: function(frm, cdt, cdn) {
+		calculate_promotions_and_experience(frm);
 	},
-	did_you_get_any_salary_increase: function(frm, cdt, cdn) {
-		validate_salary_hikes_item_exists(frm, cdt, cdn);
-		set_company_options(frm);
-		set_promotions_and_salary_hike_field(frm);
+	monthly_salary_in_kwd: function(frm, cdt, cdn) {
+		calculate_promotions_and_experience(frm);
 	}
 });
 
-var validate_promotion_item_exists = function(frm, cdt, cdn) {
-	var child = locals[cdt][cdn];
-	if(child.did_you_get_any_promotion == 'No' && frm.doc.promotions){
-		frm.doc.promotions.forEach((item) => {
-	    if(item.company_name == child.company_name){
-				frappe.model.set_value(child.doctype, child.name, 'did_you_get_any_promotion', 'Yes');
-				frm.refresh_field('promotions');
-				frappe.throw(__("Please Clear the Promotion Details for the Company"))
-			}
-	  });
-	}
-};
-
-var validate_salary_hikes_item_exists = function(frm, cdt, cdn) {
-	var child = locals[cdt][cdn];
-	if(child.did_you_get_any_salary_increase == 'No' && frm.doc.salary_hikes){
-		frm.doc.salary_hikes.forEach((item) => {
-	    if(item.company_name == child.company_name){
-				frappe.model.set_value(child.doctype, child.name, 'did_you_get_any_salary_increase', 'Yes');
-				frm.refresh_field('salary_hikes');
-				frappe.throw(__("Please Clear the Salary Hike Details for the Company"))
-			}
-	  });
-	}
-};
-
-var set_promotions_and_salary_hike_field = function(frm) {
-	let promotions = false;
-	let salary_hikes = false;
+var calculate_promotions_and_experience = function(frm) {
+	let total_years_of_experience = 0;
+	let total_number_of_promotions = 0;
+	let total_number_of_salary_changes = 0;
 	if(frm.doc.career_history_company){
+		var start_date_in_company = {};
+		var promotions = {};
+		var salary_hikes = {};
+		var end_date_in_company = {};
+
 		frm.doc.career_history_company.forEach((item) => {
-	    if(item.did_you_get_any_promotion == 'Yes'){
-				promotions = true;
+			if(!start_date_in_company[item.company_name]){
+				start_date_in_company[item.company_name] = item.start_date;
 			}
-			if(item.did_you_get_any_salary_increase == 'Yes'){
-				salary_hikes = true;
+			if(!end_date_in_company[item.company_name] && item.end_date){
+				end_date_in_company[item.company_name] = item.end_date;
 			}
-	  });
-	}
-	frm.set_df_property('promotions_section', 'hidden', !promotions);
-	frm.set_df_property('salary_hikes_section', 'hidden', !salary_hikes);
-	frm.set_df_property('promotions', 'reqd', promotions);
-	frm.set_df_property('salary_hikes', 'reqd', salary_hikes);
-	if(!promotions){
-		frm.clear_table('promotions');
-	}
-	if(!salary_hikes){
-		frm.clear_table('salary_hikes');
-	}
-};
+			if(!promotions[item.company_name]){
+				promotions[item.company_name] = [item.job_title];
+			}
+			else if(promotions[item.company_name] != item.job_title){
+				promotions[item.company_name].push(item.job_title);
+			}
+			if(!salary_hikes[item.company_name]){
+				salary_hikes[item.company_name] = [item.monthly_salary_in_kwd];
+			}
+			else if(salary_hikes[item.company_name] != item.monthly_salary_in_kwd){
+				salary_hikes[item.company_name].push(item.monthly_salary_in_kwd);
+			}
+		});
 
-var set_company_options = function(frm) {
-  var promotion_company_options = [''];
-	var salary_hike_company_options = [''];
-  frm.doc.career_history_company.forEach((item, i) => {
-		if(item.did_you_get_any_promotion == 'Yes'){
-			promotion_company_options[i+1] = item.company_name;
+		for (var company in start_date_in_company) {
+			if (start_date_in_company.hasOwnProperty(company)) {
+				var start_date = start_date_in_company[company];
+				if(end_date_in_company[company]){
+					total_years_of_experience += calculate_total_years_of_experience(start_date, end_date_in_company[company]);
+				}
+			}
+			if(promotions[company]){
+				total_number_of_promotions += promotions[company].length-1;
+			}
+			if(salary_hikes[company]){
+				total_number_of_salary_changes += salary_hikes[company].length-1;
+			}
 		}
-		if(item.did_you_get_any_salary_increase == 'Yes'){
-			salary_hike_company_options[i+1] = item.company_name;
-		}
-  });
-  frappe.meta.get_docfield("Career History Promotion", "company_name", frm.doc.name).options = promotion_company_options;
-	frappe.meta.get_docfield("Career History Salary Hike", "company_name", frm.doc.name).options = salary_hike_company_options;
-  frm.refresh_field('promotions');
-	frm.refresh_field('salary_hikes');
+	}
+	frm.set_value('total_number_of_promotions_and_salary_changes', total_number_of_promotions+total_number_of_salary_changes);
+	frm.set_value('total_years_of_experience', total_years_of_experience);
+	calculate_career_history_score(frm);
 };
 
-frappe.ui.form.on('Career History Promotion', {
-  recruiter_validation_score: function(frm) {
-    calculate_number_promotions_and_salary_changes(frm);
-  }
-});
-
-frappe.ui.form.on('Career History Salary Hike', {
-  recruiter_validation_score: function(frm) {
-    calculate_number_promotions_and_salary_changes(frm);
-  }
-});
-
-var set_career_history_company_item = function(frm) {
-  if(frm.doc.number_of_companies > 0){
-    if(frm.doc.career_history_company){
-        if(frm.doc.career_history_company.length < frm.doc.number_of_companies){
-          create_career_history_company_item(frm);
-        }
-    }
-    else{
-      create_career_history_company_item(frm);
-    }
-  }
-};
-
-var create_career_history_company_item = function(frm) {
-  var the_factor = frm.doc.number_of_companies - frm.doc.career_history_company.length
-  for (var i = 0; i < the_factor; i++) {
-    frappe.model.add_child(frm.doc, 'Career History Company', 'career_history_company');
-  }
-  frm.refresh_field('career_history_company');
+var calculate_total_years_of_experience = function(start_date, end_date) {
+	let	date_diff_ms = Date.parse(end_date) - Date.parse(start_date);
+	let	date_obj = new Date();
+	date_obj.setTime(date_diff_ms);
+	let	years =  date_obj.getFullYear() - 1970;
+	let month_factor = parseFloat(date_obj.getMonth())/12;
+	return years+month_factor;
 };
 
 var calculate_career_history_score = function(frm) {
   var career_history_score = 0;
-  if(frm.doc.number_promotions_and_salary_changes && frm.doc.total_years_of_experience){
-    var the_factor = frm.doc.number_promotions_and_salary_changes/frm.doc.total_years_of_experience;
+  if(frm.doc.total_number_of_promotions_and_salary_changes && frm.doc.total_years_of_experience){
+    var the_factor = frm.doc.total_number_of_promotions_and_salary_changes/frm.doc.total_years_of_experience;
     if(the_factor >= 0 && the_factor < 0.25){
       career_history_score = 1
     }
@@ -204,58 +145,7 @@ var calculate_career_history_score = function(frm) {
   frm.set_value('career_history_score', career_history_score);
 };
 
-var calculate_number_promotions_and_salary_changes = function(frm) {
-  let total = 0;
-	let total_promotion = 0;
-	let total_salary_hike = 0;
-  if(frm.doc.promotions){
-    frm.doc.promotions.forEach((item) => {
-      total += item.recruiter_validation_score?item.recruiter_validation_score:0;
-			total_promotion += item.recruiter_validation_score?item.recruiter_validation_score:0;
-    });
-  }
-	if(frm.doc.salary_hikes){
-    frm.doc.salary_hikes.forEach((item) => {
-      total += item.recruiter_validation_score?item.recruiter_validation_score:0;
-			total_salary_hike += item.recruiter_validation_score?item.recruiter_validation_score:0;
-    });
-  }
-	frm.set_value('promotion_total_score', total_promotion);
-	frm.set_value('salary_hike_total_score', total_salary_hike);
-  frm.set_value('number_promotions_and_salary_changes', total);
-  calculate_career_history_score(frm);
-};
-
-var calculate_years_of_experience = function(frm, cdt, cdn) {
-  var child = locals[cdt][cdn];
-  if(child.job_start_date && child.job_end_date){
-    let	date_diff_ms = Date.parse(child.job_end_date) - Date.parse(child.job_start_date);
-  	let	date_obj = new Date();
-  	date_obj.setTime(date_diff_ms);
-  	let	years =  date_obj.getFullYear() - 1970;
-    let month_factor = parseFloat(date_obj.getMonth())/12;
-		let years_of_experience_str = '';
-		if(years > 0){
-			years_of_experience_str = years + " Year(s) ";
-		}
-		if(date_obj.getMonth() && date_obj.getMonth() > 0){
-			years_of_experience_str += date_obj.getMonth() + " Month(s) ";
-		}
-		child.years_of_experience_str = years_of_experience_str;
-    child.years_of_experience = years+month_factor;
-  }
-  frm.refresh_field('career_history_company');
-  calculate_total_years_of_experience(frm);
-};
-
-var calculate_total_years_of_experience = function(frm) {
-  let total_years_of_experience = 0;
-  if(frm.doc.career_history_company){
-    frm.doc.career_history_company.forEach((item) => {
-      total_years_of_experience += item.years_of_experience?item.years_of_experience:0;
-    });
-  }
-  frm.set_value('total_years_of_experience', total_years_of_experience);
+var set_total_years_of_experience_str = function(frm, total_years_of_experience) {
 	let years = total_years_of_experience - (total_years_of_experience%1);
 	let months = Math.round((total_years_of_experience%1)*12);
 	let total_years_of_experience_str = '';
@@ -265,8 +155,7 @@ var calculate_total_years_of_experience = function(frm) {
 	if(months > 0){
 		total_years_of_experience_str += months + " Month(s) ";
 	}
-	frm.set_value('total_years_of_experience_str', total_years_of_experience_str);
-	calculate_career_history_score(frm);
+	frm.set_df_property('total_years_of_experience', 'description', total_years_of_experience_str);
 };
 
 var set_job_applicant_details = function(frm) {
@@ -294,14 +183,13 @@ var set_job_applicant_details = function(frm) {
 
 var set_current_job_details = function(frm, job_applicant) {
   if(job_applicant.one_fm_i_am_currently_working){
-    frm.set_value('number_of_companies', 1);
     let current_job = frappe.model.add_child(frm.doc, 'Career History Company', 'career_history_company');
     frappe.model.set_value(current_job.doctype, current_job.name, 'current_job', true);
     frappe.model.set_value(current_job.doctype, current_job.name, 'company_name', job_applicant.one_fm_current_employer);
     frappe.model.set_value(current_job.doctype, current_job.name, 'job_title', job_applicant.one_fm_current_job_title);
 		frappe.model.set_value(current_job.doctype, current_job.name, 'country_of_employment', job_applicant.one_fm_country_of_employment);
-    frappe.model.set_value(current_job.doctype, current_job.name, 'job_start_date', job_applicant.one_fm_employment_start_date);
-    frappe.model.set_value(current_job.doctype, current_job.name, 'job_end_date', job_applicant.one_fm_employment_end_date);
+    frappe.model.set_value(current_job.doctype, current_job.name, 'start_date', job_applicant.one_fm_employment_start_date);
+    frappe.model.set_value(current_job.doctype, current_job.name, 'end_date', job_applicant.one_fm_employment_end_date);
     frappe.model.set_value(current_job.doctype, current_job.name, 'monthly_salary_in_dolor', job_applicant.one_fm_current_salary);
   }
 };
--- one_fm/one_fm/doctype/career_history/career_history.py
+++ one_fm/one_fm/doctype/career_history/career_history.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe import _
-from frappe.utils import getdate
+from frappe.utils import getdate, month_diff
 from one_fm.utils import validate_applicant_overseas_transferable
 
 class OverlapError(frappe.ValidationError): pass
@@ -16,14 +16,12 @@ class CareerHistory(Document):
 
 	def validate(self):
 		self.validate_with_applicant()
-		self.validate_dates()
-		set_totals_in_career_history_company(self)
-		self.validate_date_overlap_within_childs()
-		self.validate_pormotions_and_salary_during_company()
-		self.validate_pormotions_exist_for_company()
-		self.validate_salary_hikes_exist_for_company()
+		self.calculate_promotions_and_experience()
 		self.calculate_career_history_score()
 		self.career_history_score_action()
+		for company in self.career_history_company:
+			# Validate Overlaping of Career History Company Details with Date
+			validate_overlap(self, company)
 
 	def career_history_score_action(self):
 		if self.career_history_score and self.career_history_score > 0 and self.career_history_score < 2.99:
@@ -32,8 +30,8 @@ class CareerHistory(Document):
 
 	def calculate_career_history_score(self):
 		career_history_score = 0;
-		if self.number_promotions_and_salary_changes and self.total_years_of_experience:
-			the_factor = self.number_promotions_and_salary_changes/self.total_years_of_experience
+		if self.total_number_of_promotions_and_salary_changes and self.total_years_of_experience:
+			the_factor = self.total_number_of_promotions_and_salary_changes/self.total_years_of_experience
 			if the_factor >= 0 and the_factor < 0.25:
 				career_history_score = 1
 			elif the_factor >= 0.25 and the_factor < 0.33:
@@ -64,65 +62,44 @@ class CareerHistory(Document):
 	def on_trash(self):
 		update_career_history_score_of_applicant(self, True)
 
-	def validate_salary_hikes_exist_for_company(self):
-		for company in self.career_history_company:
-			if company.did_you_get_any_salary_increase == 'Yes':
-				hikes_for_company = False
-				if self.salary_hikes:
-					for salary_hike in self.salary_hikes:
-						if company.company_name == salary_hike.company_name:
-							hikes_for_company = True
-				if not hikes_for_company:
-					frappe.throw(_('You have to add Salary Hikes for the Company {0}').format(company.company_name))
+	def calculate_promotions_and_experience(self):
+		'''
+			Method to calculate and set Promotion and Experience
+			args: self: Career History Object
+		'''
+		total_number_of_promotions = 0
+		total_number_of_salary_changes = 0
+		total_months_of_experience = 0
+		if self.career_history_company:
+			start_date_in_company = {}
+			promotions = {}
+			salary_hikes = {}
+			end_date_in_company = {}
+			for item in self.career_history_company:
+				if item.company_name not in start_date_in_company:
+					start_date_in_company[item.company_name] = item.start_date
+				if item.company_name not in end_date_in_company and item.end_date:
+					end_date_in_company[item.company_name] = item.end_date
+				if item.company_name not in promotions:
+					promotions[item.company_name] = [item.job_title]
+				elif promotions[item.company_name] != item.job_title:
+					promotions[item.company_name].append(item.job_title)
+				if item.company_name not in salary_hikes:
+					salary_hikes[item.company_name] = [item.monthly_salary_in_kwd]
+				elif salary_hikes[item.company_name] != item.monthly_salary_in_kwd:
+					salary_hikes[item.company_name].append(item.monthly_salary_in_kwd)
 
-	def validate_pormotions_exist_for_company(self):
-		for company in self.career_history_company:
-			if company.did_you_get_any_promotion == 'Yes':
-				promotions_for_company = False
-				if self.promotions:
-					for promotion in self.promotions:
-						if company.company_name == promotion.company_name:
-							promotions_for_company = True
-				if not promotions_for_company:
-					frappe.throw(_('You have to add Promotions for the Company {0}').format(company.company_name))
-
-	def validate_pormotions_and_salary_during_company(self):
-		for company in self.career_history_company:
-			if self.promotions:
-				for promotion in self.promotions:
-					if company.company_name == promotion.company_name:
-						if promotion.start_date < company.job_start_date or promotion.end_date > company.job_end_date:
-							frappe.throw(_("Row {0}: Start Date and End Date of Promotion not during the ({1}) days")
-								.format(promotion.idx, promotion.company_name), OverlapError)
-			if self.salary_hikes:
-				for salary_hike in self.salary_hikes:
-					if company.company_name == salary_hike.company_name:
-						if salary_hike.start_date < company.job_start_date or salary_hike.end_date > company.job_end_date:
-							frappe.throw(_("Row {0}: Start Date and End Date of Salary Hike not during the ({1}) days")
-								.format(salary_hike.idx, salary_hike.company_name), OverlapError)
-
-	def validate_date_overlap_within_childs(self):
-		for company in self.career_history_company:
-			validate_overlap(self, company, 'Company')
-		if self.promotions:
-			for promotion in self.promotions:
-				validate_overlap(self, promotion, 'Promotions')
-
-		if self.salary_hikes:
-			for salary_hike in self.salary_hikes:
-				validate_overlap(self, salary_hike, 'Salary Hikes')
-
-	def validate_dates(self):
-		self.validate_dates_in_child('career_history_company', 'job_start_date', 'job_end_date', 'Career History')
-		self.validate_dates_in_child('promotions', 'start_date', 'end_date', 'Promotions')
-		self.validate_dates_in_child('salary_hikes', 'start_date', 'end_date', 'Salary Hikes')
-
-
-	def validate_dates_in_child(self, child_name, start_date_field, end_date_field, table_label):
-		for child in self.get(child_name):
-			if child.get(start_date_field) > child.get(end_date_field):
-				frappe.throw(_("Row {0}: End Date cannot be before Start Date in {1}")
-					.format(child.idx, table_label), OverlapError)
+		for company in start_date_in_company:
+			start_date = start_date_in_company[company]
+			if end_date_in_company[company]:
+				total_months_of_experience += month_diff(getdate(end_date_in_company[company]), getdate(start_date))-1
+			if promotions[company]:
+				total_number_of_promotions += len(promotions[company])-1
+			if salary_hikes[company]:
+				total_number_of_salary_changes += len(salary_hikes[company])-1
+		self.total_number_of_promotions_and_salary_changes = total_number_of_promotions+total_number_of_salary_changes
+		if total_months_of_experience:
+			self.total_years_of_experience = total_months_of_experience/12
 
 def update_career_history_score_of_applicant(doc, delete=False):
 	job_applicant = frappe.get_doc('Job Applicant', doc.job_applicant)
@@ -147,7 +124,14 @@ def update_career_history_score_of_applicant(doc, delete=False):
 		job_applicant.status = 'Rejected' if not delete else 'Open'
 	job_applicant.save(ignore_permissions=True)
 
-def validate_overlap(doc, child_doc, table):
+def validate_overlap(doc, child_doc):
+	'''
+		Method used Validate Overlaping of Career History Company Details with Date
+		args:
+			doc: Object of Career History
+			child_doc: Object of Career History Company
+	'''
+
 	query = """
 		select
 			name
@@ -155,64 +139,22 @@ def validate_overlap(doc, child_doc, table):
 			`tab{0}`
 		where
 			name != %(name)s and parent = %(parent)s
+			and (start_date between %(start_date)s and %(end_date)s
+			or end_date between %(start_date)s and %(end_date)s
+			or (start_date < %(start_date)s and end_date > %(end_date)s))
 		"""
-	query += get_doc_condition(table)
 
 	if not child_doc.name:
 		# hack! if name is null, it could cause problems with !=
 		child_doc.name = "New "+child_doc.doctype
 
-	query_filter = {"name": child_doc.name, "parent": doc.name}
-	if table == 'Company':
-		query_filter['start_date'] = child_doc.job_start_date
-		query_filter['end_date'] = child_doc.job_end_date
-	else:
-		query_filter['start_date'] = child_doc.start_date
-		query_filter['end_date'] = child_doc.end_date
-		query_filter['company_name'] = child_doc.company_name
+	query_filter = {"name": child_doc.name, "parent": doc.name, "start_date": child_doc.start_date, "end_date": child_doc.end_date}
 
 	overlap_doc = frappe.db.sql(query.format(child_doc.doctype), query_filter, as_dict = 1)
 
 	if overlap_doc:
-		frappe.throw(_("Row {0}: Start Date and End Date of Career History ({1}) is overlapping with {2}")
-			.format(child_doc.idx, table, overlap_doc[0].company), OverlapError)
-
-def get_doc_condition(table):
-	if table == 'Company':
-		return """
-			and (job_start_date between %(start_date)s and %(end_date)s
-			or job_end_date between %(start_date)s and %(end_date)s
-			or (job_start_date < %(start_date)s and job_end_date > %(end_date)s))
-		"""
-	return """
-		and company_name = %(company_name)s and (start_date between %(start_date)s and %(end_date)s
-		or end_date between %(start_date)s and %(end_date)s
-		or (start_date < %(start_date)s and end_date > %(end_date)s))
-	"""
-
-def set_totals_in_career_history_company(doc):
-	if doc.career_history_company:
-		for company in doc.career_history_company:
-			total_promotion = 0
-			total_salary_hike = 0
-			recruiter_validation_score_promotion = 0
-			recruiter_validation_score_salary_change = 0
-			if doc.promotions:
-				for promotion in doc.promotions:
-					if company.company_name == promotion.company_name:
-						total_promotion += 1
-						if promotion.recruiter_validation_score:
-							recruiter_validation_score_promotion += promotion.recruiter_validation_score
-			if doc.salary_hikes:
-				for salary_hike in doc.salary_hikes:
-					if company.company_name == salary_hike.company_name:
-						total_salary_hike += 1
-						if salary_hike.recruiter_validation_score:
-							recruiter_validation_score_salary_change += salary_hike.recruiter_validation_score
-			company.total_promotions = total_promotion
-			company.recruiter_validation_score_promotion = recruiter_validation_score_promotion
-			company.total_salary_changes = total_salary_hike
-			company.recruiter_validation_score_salary_change = recruiter_validation_score_salary_change
+		frappe.throw(_("Row {0}: Start Date and End Date in Career History Company is overlapping with {1}")
+			.format(child_doc.idx, overlap_doc[0].idx), OverlapError)
 
 @frappe.whitelist()
 def get_career_history(job_applicant):
@@ -246,7 +188,8 @@ def get_career_history_html_template():
 						<td><b>Company Name</b></td>
 						<td><b>Job Title</b></td>
 						<td><b>Country of Experience</b></td>
-						<td><b>Years of Experience</b></td>
+						<td><b>Start Date</b></td>
+						<td><b>End Date</b></td>
 					</tr>
 				</thead>
 				<tbody>
@@ -260,7 +203,10 @@ def get_career_history_html_template():
 								{{ item.country_of_employment }}
 							</td>
 							<td>
-								{{ item.years_of_experience_str }}
+								{{ item.start_date }}
+							</td>
+							<td>
+								{{ item.end_date }}
 							</td>
 						</tr>
 					{% endfor %}
