2cfec9432af307e72c298ba48d40f99a84c4a817
Ehtesham599 <ehtesham599@gmail.com>
2021-12-12 08:38:15 +0000

    Refactor Kuwaiti civil ID method

--- one_fm/templates/pages/applicant-docs.py
+++ one_fm/templates/pages/applicant-docs.py
@@ -4,14 +4,16 @@ import frappe
 from frappe.utils import cstr
 import json
 import base64
+import hashlib
 
 @frappe.whitelist(allow_guest=True)
-def get_civil_id_text(images):
+def get_civil_id_text(images, is_kuwaiti=0):
     """This API redirects the image fetched from frontend and 
     runs it though Google Vision API, each side at a time.
 
     Args:
         images (json): Consist of two base64 encoded strings(front_side, back_side).
+        is_kuwaiti (int): 0 for non-Kuwaiti and 1 for Kuwaiti
 
     Returns:
         result: dictionary consisting of text fetched from both front and back side.
@@ -27,11 +29,11 @@ def get_civil_id_text(images):
         # Load the Images
         image_files = json.loads(images)
 
-        front_image = upload_image(image_files["front_side"],"image1.png")
-        back_image = upload_image(image_files["back_side"],"image2.png")
+        front_image_path = upload_image(image_files["front_side"],hashlib.md5((image_files["front_side"]).encode('utf-8')).hexdigest())
+        back_image_path = upload_image(image_files["back_side"],hashlib.md5((image_files["back_side"]).encode('utf-8')).hexdigest())
 
-        front_text = get_front_side_civil_id_text(front_image, client)
-        back_text = get_back_side_civil_id_text(back_image, client)
+        front_text = get_front_side_civil_id_text(front_image_path, client, is_kuwaiti)
+        back_text = get_back_side_civil_id_text(back_image_path, client, is_kuwaiti)
 
         result.update({'front_text': front_text})
         result.update({'back_text': back_text})
@@ -41,13 +43,14 @@ def get_civil_id_text(images):
     except Exception as e:
         frappe.throw(e)
 
-def get_front_side_civil_id_text(image_path, client):
+def get_front_side_civil_id_text(image_path, client, is_kuwaiti):
     """ This method fetches the image from the provided image path, calls the vision api to exrtract text from the image
         and parses through the obtained texts to get relevant texts for the required fields in the front side of the civil ID.
 
     Args:
         image_path (str): Path to image file
         client (obj): Vision API client library object
+        is_kuwaiti (int): 0 for non-Kuwaiti and 1 for Kuwaiti
 
     Returns:
         dict: Dictionary of obtained texts for the required fields.
@@ -69,32 +72,34 @@ def get_front_side_civil_id_text(image_path, client):
     for index in range(1,len(texts)):
         assemble[index] = texts[index].description
     
-    result["Civil ID No."] = texts[find_index(assemble,"CARD")+1].description
-    result["Nationality"] = texts[find_index(assemble,"Nationality")+1].description
-    result["Date Of Birth"] = texts[find_index(assemble,"Birth")+2].description
-    result["Expiry Date"] = texts[find_index(assemble,"Birth")+3].description
-    if texts[find_index(assemble,"Sex")-1].description == "M" or texts[find_index(assemble,"Sex")-1].description == "F":
-        result["Gender"] = texts[find_index(assemble,"Sex")-1].description
-    else:
-        result["Gender"] = ""
+    if is_kuwaiti:
+        result["Civil ID No."] = texts[find_index(assemble,"CARD")+1].description
+        result["Nationality"] = texts[find_index(assemble,"Nationality")+1].description
+        result["Date Of Birth"] = texts[find_index(assemble,"Birth")+2].description
+        result["Expiry Date"] = texts[find_index(assemble,"Birth")+3].description
+        if texts[find_index(assemble,"Sex")-1].description == "M" or texts[find_index(assemble,"Sex")-1].description == "F":
+            result["Gender"] = texts[find_index(assemble,"Sex")-1].description
+        else:
+            result["Gender"] = ""
 
-    result["Name"] = ""
-    for i in range(find_index(assemble,"Name")+1,find_index(assemble,"Nationality")-2):
-        result["Name"] = result["Name"] + texts[i].description + " "
-    
-    result["Arabic Name"]= ""
-    for i in range(find_index(assemble,"No")+1,find_index(assemble,"Name")-1):
-        result["Arabic Name"] = result["Arabic Name"] + texts[i].description + " "
+        result["Name"] = ""
+        for i in range(find_index(assemble,"Name")+1,find_index(assemble,"Nationality")-2):
+            result["Name"] = result["Name"] + texts[i].description + " "
+        
+        result["Arabic Name"]= ""
+        for i in range(find_index(assemble,"No")+1,find_index(assemble,"Name")-1):
+            result["Arabic Name"] = result["Arabic Name"] + texts[i].description + " "
 
     return result
 
-def get_back_side_civil_id_text(image_path, client):
+def get_back_side_civil_id_text(image_path, client, is_kuwaiti):
     """ This method fetches the image from the provided image path, calls the vision api to exrtract text from the image
         and parses through the obtained texts to get relevant texts for the required fields in the back side side of the civil ID.
 
     Args:
         image_path (str): Path to image file
         client (obj): Vision API client library object
+        is_kuwaiti (int): 0 for non-Kuwaiti and 1 for Kuwaiti
 
     Returns:
         dict: Dictionary of obtained texts for the required fields.
@@ -115,10 +120,12 @@ def get_back_side_civil_id_text(image_path, client):
     
     for index in range(1,len(texts)):
         assemble[index] = texts[index].description
-    if find_index(assemble,"منزل"):
-        result["PACI No."] = texts[find_index(assemble,"منزل")+1].description
-    else:
-        result["PACI No."] = " "
+    
+    if is_kuwaiti:
+        if find_index(assemble,":iall"):
+            result["PACI No."] = texts[find_index(assemble,":iall")-1].description
+        else:
+            result["PACI No."] = " "
     
     return result
 
@@ -126,7 +133,7 @@ def find_index(dictionary, word):
     for d in dictionary:
         if dictionary[d] == word:
             return d
-        
+
 def upload_image(image, filename):
     """ This method writes a file to a server directory
 
@@ -135,7 +142,7 @@ def upload_image(image, filename):
         filename (str): Name of the file
 
     Returns:
-        [str]: Path to uploaded image
+        str: Path to uploaded image
     """
     content = base64.b64decode(image)
     image_path = cstr(frappe.local.site)+"/private/files/user/"+filename
