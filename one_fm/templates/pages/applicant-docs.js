53bc3aabd171f89033bff80ffb0458595c95c181
SalShai <slwshkh@gmail.com>
2021-12-12 16:23:52 +0300

    Resolve Conflicts

--- one_fm/templates/pages/applicant-docs.js
+++ one_fm/templates/pages/applicant-docs.js
@@ -6,10 +6,7 @@ frappe.ready = function (fn) {
 window.dev_server = {{dev_server}};
 window.socketio_port = {{frappe.socketio_port}};
 
-//Name and Nationality should be fetched from link.
-const nationality = "Kuwaiti"; //or Non-Kuwaiti
-
-var front , back;
+var front_civil , back_civil, front_passport, back_passport;
 
 function front_civil_extract(input){
     let file = input.files[0];
@@ -19,8 +16,8 @@ function front_civil_extract(input){
     reader.readAsDataURL(file);
 
     reader.onload = function() {
-      front = reader.result;
-      front = front.replace(/^data:image\/\w+;base64,/, "");
+      front_civil = reader.result;
+      front_civil = front_civil.replace(/^data:image\/\w+;base64,/, "");
     };
 
     reader.onerror = function() {
@@ -36,19 +33,78 @@ function back_civil_extract(input){
     reader.readAsDataURL(file);
 
     reader.onload = function() {
-      back = reader.result;
-      back = back.replace(/^data:image\/\w+;base64,/, "");
+      back_civil = reader.result;
+      back_civil = back_civil.replace(/^data:image\/\w+;base64,/, "");
     };
 
     reader.onerror = function() {
       console.log(reader.error);
     };
 };
+
+function front_passport_extract(input){
+  let file = input.files[0];
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  reader.onload = function() {
+    front_passport = reader.result;
+    front_passport = front_passport.replace(/^data:image\/\w+;base64,/, "");
+  };
+
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+};
+
+function back_passport_extract(input){
+  let file = input.files[0];
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  reader.onload = function() {
+    back_passport = reader.result;
+    back_passport = back_passport.replace(/^data:image\/\w+;base64,/, "");
+  };
+
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+};
 function test(input){
-  var date = input.value;
-  console.log(date)
+var date = input.value;
+console.log(date)
 }
 
+function populate_nationality(){
+  frappe.call({
+    type: "GET",
+    method: "one_fm.templates.pages.applicant-docs.populate_nationality",
+    callback: function(r) {
+
+      langArray = r.message;
+      var select = document.getElementById("nationality");
+      for (let i=0; i<=langArray.length;i++) {
+        select.options[select.options.length] = new Option(langArray[i], langArray[i]);
+     }
+    }
+  }); 
+}
+
+function fetchNationality(input){
+  var code = input.value;
+  frappe.call({
+    type: "GET",
+    method: "one_fm.templates.pages.applicant-docs.fetch_nationality",
+    args: {code :code},
+    callback: function(r) {
+      console.log(r)
+      document.getElementById("nationality").value = r.message;
+    }
+  }); 
+}
 function upload(){
   civilid_check = document.getElementById("noCivilID").checked;
   console.log(civilid_check)
@@ -95,9 +151,9 @@ function fill_form(data){
     document.getElementById("ar_name").value = data['Arabic Name'];
     document.getElementById("gender").value = data['Gender']
     document.getElementById("civilid").value = data['Civil ID No.']
-    document.getElementById("nationality").value = data['Nationality']
+    document.getElementById("country_code").value = data['Nationality']
     document.getElementById("dob").value = data['Date Of Birth']
-    document.getElementById("expiry_date").value = data['Expiry Date']
+    document.getElementById("civil_expiry_date").value = data['Expiry Date']
     document.getElementById("paci_no").value = data['PACI No.'] 
   }
 };
