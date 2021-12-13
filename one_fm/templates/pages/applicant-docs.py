7efc23185bb6d50082f7f2fd81948eb9d6c4822c
SalShai <slwshkh@gmail.com>
2021-12-13 11:53:26 +0300

    Join Frontend And Backend for Passport OCR

--- one_fm/templates/pages/applicant-docs.py
+++ one_fm/templates/pages/applicant-docs.py
@@ -6,6 +6,7 @@ import json
 import base64
 import datetime
 import hashlib
+from dateutil.parser import parse
 
 @frappe.whitelist(allow_guest=True)
 def populate_nationality():
@@ -247,18 +248,115 @@ def get_passport_back_text(image_path, client):
         
     return result
 
+@frappe.whitelist(allow_guest=True)
+def get_passport_text(images):
+    try:
+        result = {}
+        
+        #initialize google vision client library
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = cstr(frappe.local.site) + frappe.local.conf.google_application_credentials
+        client = vision.ImageAnnotatorClient()
+        
+        # Load the Images
+        image_files = json.loads(images)
+
+        front_image_path = upload_image(image_files["front_side"],hashlib.md5((image_files["front_side"]).encode('utf-8')).hexdigest())
+        back_image_path = upload_image(image_files["back_side"],hashlib.md5((image_files["back_side"]).encode('utf-8')).hexdigest())
+
+        front_text = get_passport_front_text(front_image_path, client)
+        back_text = get_passport_back_text(back_image_path, client)
+        
+        result.update({'front_text': front_text})
+        result.update({'back_text': back_text})
+        
+        return result
+    
+    except Exception as e:
+        frappe.throw(e)
+
+def get_passport_front_text(image_path, client):
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)  # returns TextAnnotation
+
+    texts = response.text_annotations
+    
+    result = {}
+    assemble = {}
+    index = 0
+    
+    for index in range(1,len(texts)):
+        assemble[index] = texts[index].description
+        
+    text_length = len(texts)
+    print(text_length)
+    fuzzy = False
+    if(text_length >= 5):
+        if is_date(texts[text_length - 4].description, fuzzy):
+            result["Date of Issue"] = datetime.datetime.strptime(texts[text_length - 4].description, '%d/%m/%Y').strftime('%Y-%m-%d')
+        else:
+            result["Date of Issue"] = ""
+        if is_date(texts[text_length - 3].description, fuzzy):
+            result["Date of Expiry"] = datetime.datetime.strptime(texts[text_length - 3].description, '%d/%m/%Y').strftime('%Y-%m-%d')
+        else:
+           result["Date of Expiry"] = "" 
+        
+        mrz = texts[text_length - 1].description
+        
+        result["Passport Number"] = mrz.split("<")[0]
+        
+    return result
+
+def get_passport_back_text(image_path, client):
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)  # returns TextAnnotation
+
+    texts = response.text_annotations
+    
+    result = {}
+    assemble = {}
+    index = 0
+    
+    for index in range(1,len(texts)):
+        assemble[index] = texts[index].description
+    
+    result["Place of Issue"] = ""
+    if find_index(assemble, "Place") and find_index(assemble, "of") and find_index(assemble, "Issue"):
+        result["Place of Issue"] = texts[find_index(assemble, "Issue") + 3].description
+        
+    return result
+
 def find_index(dictionary, word):
     for d in dictionary:
         if dictionary[d] == word:
             return d
 
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
+
 def upload_image(image, filename):
     """ This method writes a file to a server directory
-
     Args:
         image (str): Base64 encoded image
         filename (str): Name of the file
-
     Returns:
         str: Path to uploaded image
     """
