fac3fb24e34d1da90d0cc77f61ad639ce7a998c1
SalShai <slwshkh@gmail.com>
2021-12-21 10:52:40 +0300

    remove Unused Function

--- one_fm/hiring/doctype/electronic_signature_declaration/electronic_signature_declaration.js
+++ one_fm/hiring/doctype/electronic_signature_declaration/electronic_signature_declaration.js
@@ -2,14 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Electronic Signature Declaration', {
-	update: function(frm) {
-		console.log("This")
-		set_signature(frm);
-	}
-});
+	// refresh: function(frm) {
 
-var set_signature = function(frm) {
-	if(frm.applicant_signature){
-		frappe.db.set_value('Onboard Employee', self.onboarding_employee, 'electronic_signature_declaration_status', 1)
-	}
-}
+	// }
+});
