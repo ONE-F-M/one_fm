b9340d803a5b1bc1f2fc4a8b4df648927153c299
Ehtesham599 <ehtesham599@gmail.com>
2021-12-27 06:57:48 +0000

    Removed backend call to fetch url

--- one_fm/public/js/doctype_js/payroll_entry.js
+++ one_fm/public/js/doctype_js/payroll_entry.js
@@ -3,43 +3,11 @@ frappe.ui.form.on('Payroll Entry', {
 		if (frm.doc.salary_slips_created == 1){
 			frm.add_custom_button(__("Download Payroll Bank Export"), function() {
 				let payroll_entry = frm.doc.name
-				// Fetch URL for the export file
-				frappe.xcall("one_fm.api.doc_methods.payroll_entry.download_payroll_export_file", {payroll_entry})
-					.then(res => {
-						let {app_url, path, filename} = res;
-						if (app_url && path && filename){
-							let url = app_url + path + filename
-							window.open(url, "Download");
-						}else{
-							frappe.msgprint(
-								msg='No export file found',
-								title='Error',
-								raise_exception=1
-							)
-						}
-					}).catch(e =>{
-						console.log(e);
-					});
+				window.open("/files/payroll-entry/" + payroll_entry + ".xlsx", "Download");
 			}).addClass("btn-primary");
 			frm.add_custom_button(__("Download Payroll Cash Export"), function() {
 				let payroll_entry = "Cash-" + frm.doc.name
-				// Fetch URL for the export file
-				frappe.xcall("one_fm.api.doc_methods.payroll_entry.download_payroll_export_file", {payroll_entry})
-					.then(res => {
-						let {app_url, path, filename} = res;
-						if (app_url && path && filename){
-							let url = app_url + path + filename
-							window.open(url, "Download");
-						}else{
-							frappe.msgprint(
-								msg='No export file found',
-								title='Error',
-								raise_exception=1
-							)
-						}
-					}).catch(e =>{
-						console.log(e);
-					});
+				window.open("/files/payroll-entry/" + payroll_entry + ".xlsx", "Download");
 			}).addClass("btn-primary");
 		}
 		frm.set_indicator_formatter('employee',
