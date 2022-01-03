34c9756d182eaa30d9a5af19b765872eb8e3f23e
mymi14s <mymi14s@hotmail.com>
2022-01-03 11:06:47 +0000

    feat: Employee shift summary data API

--- one_fm/api/dashboard_utils.py
+++ one_fm/api/dashboard_utils.py
@@ -1,7 +1,10 @@
-import frappe
+from datetime import datetime, date
+import calendar
+import frappe, json
 from frappe import _
-import json
 from frappe.desk.notifications import get_filters_for
+from erpnext.hr.report.employee_leave_balance.employee_leave_balance import get_data as get_leave_balance
+from one_fm.api.utils import response
 
 @frappe.whitelist()
 def get_open_count(doctype, name, links):
@@ -48,3 +51,108 @@ def get_open_count(doctype, name, links):
 		out['timeline_data'] = module.get_timeline_data(doctype, name)
 	# print(out)
 	return out
+
+
+@frappe.whitelist()
+def get_employee_shift(employee_id, date_type='month'):
+	"""
+		Fetch employee information relating to
+		leave_balance, shift, penalties and attendance
+	"""
+
+	# prepare dates
+	today = datetime.today()
+	_start = today.replace(day=1).date()
+	_end = today.replace(day = calendar.monthrange(today.year, today.month)[1]).date()
+	if(date_type=='year'):
+		_start = date(today.year, 1, 31)
+		_end = date(today.year, 12, 31)
+
+	data = {
+		"leave_balance": "",
+		"penalties": "",
+		"days_worked": "",
+		"shift_time_from": "",
+		"shift_time_to": "",
+		"shift_location": "",
+		"shift_latitude_and_longitude": ""
+	}
+	try:
+		employee = frappe.get_doc('Employee', employee_id)
+		shift_assignment =  frappe.db.get_list('Shift Assignment',
+     		filters=[
+		         ['employee', '=', employee.name],
+		     ],
+		     fields=['name', 'employee', 'site', 'shift_type'],
+		     order_by='modified desc',
+		     limit=1,
+		 )
+		if(len(shift_assignment)):
+			shift_location = frappe.get_doc('Location', shift_assignment[0].site)
+			shift_type = frappe.get_doc('Shift Type', shift_assignment[0].shift_type)
+			days_worked = frappe.db.get_list('Attendance',
+		    filters=[
+	             ['employee', '=', employee.name],
+	             ['status', '=', 'Present'],
+	             ['attendance_date', 'BETWEEN', [_start, _end]],
+	        ],
+		    fields=['COUNT(*) as count', 'name', 'employee', 'status', 'attendance_date'],
+		    order_by='modified desc',
+		 	)[0].count
+			penalties = frappe.db.sql(f"""
+				SELECT COUNT(*) as count, pi.name, pie.employee_id
+				FROM `tabPenalty Issuance Employees` pie
+				JOIN `tabPenalty Issuance` pi ON pie.parent=pi.name
+				WHERE pie.employee_id="{employee.name}" AND pi.docstatus=1
+				AND pi.issuing_time BETWEEN "{_start} 00:00:00" AND "{_end} 23:59:59";
+			""", as_dict=1)[0].count
+			# get leav balance filters
+			filters=frappe._dict({'from_date':_start, 'to_date':_end, 'employee':employee.name})
+			leave_balance = sum([i.closing_balance for i in get_leave_balance(filters)])
+			response(
+				code=201, title='Result found!',
+				msg="Successful"
+			)
+			return {
+				'message': f"Employee shift record retrieved from {_start} to {_end}",
+				'data':{
+					'employee': employee.name,
+					'leave_balance':leave_balance,
+					'penalties': penalties,
+					'days_worked':days_worked,
+					'shift_time_from': shift_type.start_time,
+					'shift_time_to': shift_type.end_time,
+					'shift_location': shift_location.name,
+					'shift_latitude_and_longitude': {
+						'latitude': shift_location.latitude,
+						'longitude': shift_location.longitude,
+					}
+				},
+				'status': 201,
+				'succuess': True
+			}
+		else:
+			# return no shift found
+			response(
+				code=201, title='No shift found',
+				msg=f"No shift found for {employee.name}"
+			)
+			return {
+				'message':f"No shift found for {employee.name}",
+				'data':{},
+				'status': 201,
+				'succuess': False
+			}
+
+	except Exception as e:
+		# an error occured
+		response(
+			code=404, title='Employee Shift Summary',
+			msg=str(e)
+		)
+		return {
+			'message':str(e),
+			'data':{},
+			'status': 404,
+			'succuess': False
+		}
