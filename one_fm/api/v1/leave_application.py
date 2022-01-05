63f80134f14af52b78277ebded6b4f106d1d0b40
Ehtesham599 <ehtesham599@gmail.com>
2022-01-05 07:46:28 +0000

    removed leave approver method from API file

--- one_fm/api/v1/leave_application.py
+++ one_fm/api/v1/leave_application.py
@@ -219,7 +219,7 @@ def new_leave_application(employee: str, from_date: str,to_date: str,leave_type:
     return leave.as_dict()
 
 
-def get_leave_approver(employee):
+def get_leave_approver(employee: str):
     """This function fetches the leave approver for a given employee.
     The leave approver is fetched  either Report_to or Leave Approver. 
     But, if both don't exist, Operation manager is the Leave Approver.
@@ -242,35 +242,3 @@ def get_leave_approver(employee):
                 return operation_manager_user_id
     
     return leave_approver
-    
-
-def notify_leave_approver(doc):
-    """
-    This function is to notify the leave approver and request his action. 
-    The Message sent through mail consist of 2 action: Approve and Reject.(It is sent only when the not sick leave.)
-
-    Param: doc -> Leave Application Doc (which needs approval)
-
-    It's a action that takes place on update of Leave Application.
-    """
-    #If Leave Approver Exist
-    if doc.leave_approver:
-        parent_doc = frappe.get_doc('Leave Application', doc.name)
-        args = parent_doc.as_dict() #fetch fields from the doc.
-
-        #Fetch Email Template for Leave Approval. The email template is in HTML format.
-        template = frappe.db.get_single_value('HR Settings', 'leave_approval_notification_template')
-        if not template:
-            frappe.msgprint(_("Please set default template for Leave Approval Notification in HR Settings."))
-            return
-        email_template = frappe.get_doc("Email Template", template)
-        message = frappe.render_template(email_template.response_html, args)
-
-        #send notification
-        doc.notify({
-            # for post in messages
-            "message": message,
-            "message_to": doc.leave_approver,
-            # for email
-            "subject": email_template.subject
-        })
