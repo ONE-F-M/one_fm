421f3b617b6b5676cdadf797c8d7372818b35ba5
mymi14s <mymi14s@hotmail.com>
2022-01-23 14:36:38 +0100

    feat: Employee Indemnity Report [Finishes #180848410]

--- one_fm/one_fm/report/employee_indemnity_summary/employee_indemnity_summary.py
+++ one_fm/one_fm/report/employee_indemnity_summary/employee_indemnity_summary.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, omar jaber and contributors
+# Copyright (c) 2022, omar jaber, Anthony Emmanuel and contributors
 # For license information, please see license.txt
 
 import frappe
