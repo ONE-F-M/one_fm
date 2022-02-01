c512bba00fc38b423ec17e1af425bb33ab0f001b
Ehtesham599 <ehtesham599@gmail.com>
2022-02-01 09:54:08 +0000

    Override item amount calculation

--- one_fm/operations/doctype/contracts/contracts.py
+++ one_fm/operations/doctype/contracts/contracts.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.utils import cstr,month_diff,today,getdate,date_diff,add_years, cint, add_to_date, get_first_day, get_last_day, get_datetime
+from frappe.utils import cstr,month_diff,today,getdate,date_diff,add_years, cint, add_to_date, get_first_day, get_last_day, get_datetime, flt
 from frappe import _
 
 class Contracts(Document):
@@ -415,3 +415,46 @@ def get_item_monthly_amount(item, project, first_day_of_month, last_day_of_month
 		'rate': item_rate,
 		'amount': amount,
 	}
+
+def calculate_item_values(doc):
+	if not doc.discount_amount_applied:
+		for item in doc.doc.get("items"):
+			doc.doc.round_floats_in(item)
+
+			if item.discount_percentage == 100:
+				item.rate = 0.0
+			elif item.price_list_rate:
+				if not item.rate or (item.pricing_rules and item.discount_percentage > 0):
+					item.rate = flt(item.price_list_rate *
+						(1.0 - (item.discount_percentage / 100.0)), item.precision("rate"))
+					item.discount_amount = item.price_list_rate * (item.discount_percentage / 100.0)
+				elif item.discount_amount and item.pricing_rules:
+					item.rate =  item.price_list_rate - item.discount_amount
+
+			if item.doctype in ['Quotation Item', 'Sales Order Item', 'Delivery Note Item', 'Sales Invoice Item', 'POS Invoice Item', 'Purchase Invoice Item', 'Purchase Order Item', 'Purchase Receipt Item']:
+				item.rate_with_margin, item.base_rate_with_margin = doc.calculate_margin(item)
+				if flt(item.rate_with_margin) > 0:
+					item.rate = flt(item.rate_with_margin * (1.0 - (item.discount_percentage / 100.0)), item.precision("rate"))
+
+					if item.discount_amount and not item.discount_percentage:
+						item.rate = item.rate_with_margin - item.discount_amount
+					else:
+						item.discount_amount = item.rate_with_margin - item.rate
+
+				elif flt(item.price_list_rate) > 0:
+					item.discount_amount = item.price_list_rate - item.rate
+			elif flt(item.price_list_rate) > 0 and not item.discount_amount:
+				item.discount_amount = item.price_list_rate - item.rate
+
+			item.net_rate = item.rate
+
+			# if not item.qty and self.doc.get("is_return"):
+			# 	item.amount = flt(-1 * item.rate, item.precision("amount"))
+			# else:
+			# 	item.amount = flt(item.rate * item.qty,	item.precision("amount"))
+
+			item.net_amount = item.amount
+
+			doc._set_in_company_currency(item, ["price_list_rate", "rate", "net_rate", "amount", "net_amount"])
+
+			item.item_tax_amount = 0.0
