443de9da3502b280db27030377595c8df5c142c1
SalShai <slwshkh@gmail.com>
2022-02-08 08:26:36 +0300

    Correct Indemnity Alloocation Calculation

--- one_fm/one_fm/hr_utils.py
+++ one_fm/one_fm/hr_utils.py
@@ -3,8 +3,10 @@
 from __future__ import unicode_literals
 from frappe import _
 import frappe
+from datetime import date
 from frappe.utils import getdate, nowdate
 from frappe.model.document import Document
+from dateutil.relativedelta import relativedelta
 
 def daily_indemnity_allocation_builder():
     query = """
@@ -15,6 +17,7 @@ def daily_indemnity_allocation_builder():
     """
     employee_list = frappe.db.sql(query, as_dict=True)
     print(employee_list)
+    indemnity_allocation_builder(employee_list)
     # frappe.enqueue(indemnity_allocation_builder, timeout=600, employee_list=employee_list)
 
 def indemnity_allocation_builder(employee_list):
@@ -22,16 +25,29 @@ def indemnity_allocation_builder(employee_list):
         create_indemnity_allocation(employee)
 
 def create_indemnity_allocation(employee):
-    indemnity_allcn = frappe.new_doc('Indemnity Allocation')
-    indemnity_allcn.employee = employee.name
-    salary = get_reference_salary(employee)
-    # indemnity_allcn.from_date = employee.date_of_joining
-    # indemnity_allcn.new_indemnity_allocated = 30/365
-    # indemnity_allcn.total_indemnity_allocated = 30/365
-    # indemnity_allcn.submit()
+    indemnity_amount = frappe.get_value("Salary Structure Assignment",{"employee":employee.name},["indemnity_amount"])
 
-def get_reference_salary(employee):
-    print(employee.project)
+    if indemnity_amount:
+        total_indemnity_allocated = get_total_indemnity(employee.date_of_joining,indemnity_amount)
+        indemnity_allcn = frappe.new_doc('Indemnity Allocation')
+        indemnity_allcn.employee = employee.name
+        total_indemnity_allocated = get_total_indemnity(employee.date_of_joining,indemnity_amount)
+        indemnity_allcn.from_date = employee.date_of_joining
+        indemnity_allcn.new_indemnity_allocated = total_indemnity_allocated
+        indemnity_allcn.total_indemnity_allocated = total_indemnity_allocated
+        indemnity_allcn.submit()
+
+def get_total_indemnity(date_of_joining, indemnity_amount):
+    total_working_year = relativedelta(date.today(), date_of_joining).years
+    total_working_days = (date.today() - date_of_joining).days
+
+    if total_working_year < 5:
+        total_amount = 15 * indemnity_amount / 365 * total_working_days
+    elif total_working_year == 5 and total_working_days > 5*365:
+        amount_1 = 15 * indemnity_amount / 365 * total_working_days
+        amount_2 = 30 * indemnity_amount / 365 * (total_working_days-5*365)
+        total_amount = amount_1+amount_2
+    return total_amount
 
 
 def allocate_daily_indemnity():
