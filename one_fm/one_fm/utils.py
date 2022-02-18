e1355ef8a31f8e1659fbd5a0163cc4e665952755
Jamsheer PP <kuttypjamsheer@gmail.com>
2022-02-18 15:01:03 +0530

    feat: Method for Creating Role if Not Exists

--- one_fm/one_fm/utils.py
+++ one_fm/one_fm/utils.py
@@ -390,6 +390,12 @@ def cancel_compensatory_leave_request_from_attendance(attendance):
         frappe.get_doc('Compensatory Leave Request', exist_compensatory_leave_request).cancel()
 
 def get_workflow_sates(doctype):
+    '''
+        Method used to Get active Workflow States of a Doctype
+        args:
+            doctype: Name of Doctype
+        return list of Workflow States object in Active Workflow
+    '''
     workflow_states = []
     exists_workflow = frappe.db.exists('Workflow', {'is_active': True, 'document_type': doctype})
     if exists_workflow:
@@ -397,3 +403,18 @@ def get_workflow_sates(doctype):
         for state in workflow.states:
             workflow_states.append(state)
     return workflow_states
+
+def create_role_if_not_exists(role, desk_access=True):
+    '''
+        Method is used to create Role.
+        args:
+            role: Name of the Role, Text
+            desk_access: Boolean for Desk Access
+    '''
+    if not frappe.db.exists("Role", {"role_name": role}):
+        doc = frappe.new_doc("Role")
+        doc.update({
+            "role_name": role,
+            "desk_access": desk_access
+        })
+        doc.insert(ignore_permissions=True)
