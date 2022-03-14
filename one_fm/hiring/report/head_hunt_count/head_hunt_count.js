db0eabb6e429031c928ff55dba7488470ea60cd0
SalShai <slwshkh@gmail.com>
2022-03-14 12:12:49 +0300

    Fix: The count of Applicant Item

---
+++ one_fm/hiring/report/head_hunt_count/head_hunt_count.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, omar jaber and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Head Hunt Count"] = {
+	"filters": [
+
+	]
+};
--- one_fm/hiring/report/head_hunt_count/head_hunt_count.py
+++ one_fm/hiring/report/head_hunt_count/head_hunt_count.py
@@ -18,17 +18,10 @@ def get_columns():
 
 def get_data():
 	data=[]
+    
 	head_hunt = frappe.db.get_all("Head Hunt", ["*"])
 	for h in head_hunt:
-		doc = frappe.get_list("Head Hunt Item",{"parent":h},['count(applicant_name) as count'])
-		row = [ h.lead_owner, creation, doc ]
-    # for jop in jop_list:
-	# 	row = [
-	# 		jop.applicant_name,
-	# 		jop.job_title,
-	# 		jop.one_fm_designation,
-	# 		jop.status,
-	# 		jop.one_fm_applicant_status,
-	# 		jop.one_fm_erf
-	# 	]
-	# 	data.append(row)
+		doc = frappe.db.sql("""SELECT count(*) as count from `tabHead Hunt Item` WHERE parenttype="Head Hunt" AND parent=%s""",(h.name), as_dict=1)
+		row = [ h.lead_owner, h.creation, doc[0].count ]
+		data.append(row)
+	return data
