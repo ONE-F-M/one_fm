37b57eb05cea1b80ce8fa0bea0a0ca58bbe99c27
mymi14s <mymi14s@hotmail.com>
2022-03-17 06:39:00 +0100

    chore: filter job opening by published

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -343,7 +343,7 @@ scheduler_events = {
 		'one_fm.one_fm.depreciation_custom.post_depreciation_entries',
 		'one_fm.operations.doctype.contracts.contracts.auto_renew_contracts',
 		'one_fm.hiring.utils.update_leave_policy_assignments_expires_today',
-		'one_fm.tasks.execute.hourly'
+		'one_fm.tasks.execute.daily'
 	],
 	"hourly": [
 		# "one_fm.api.tasks.send_checkin_hourly_reminder",
