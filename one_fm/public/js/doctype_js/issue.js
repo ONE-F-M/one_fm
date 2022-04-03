bb6fc0c9f413cfd2a617d8ee3d955093ae77d734
mymi14s <mymi14s@hotmail.com>
2022-04-03 11:29:55 +0100

    chore: Pivotal Tracker improvement

--- one_fm/public/js/doctype_js/issue.js
+++ one_fm/public/js/doctype_js/issue.js
@@ -29,26 +29,106 @@ let pivotal_tracker_button = frm => {
   // add a link in dashboard section if issue has been added to pivotal tracker
   if(frm.doc.status=='Open' && frappe.user.has_role('System Manager') && !frm.doc.pivotal_tracker){
     frm.add_custom_button('Pivotal Tracker', () => {
-      frappe.confirm('Are you sure you create Pivotal Tracker story?',
-        () => {
-            // action to perform if Yes is selected
-            frappe.call({
-                method: "one_fm.api.doc_methods.issue.log_pivotal_tracker", //dotted path to server method
-                freeze_message: 'Logging story to Pivotal Tracker',
-                args:frm.doc,
-                callback: function(r) {
-                    // code snippet
-                    if(r.message.status=='success'){
-                      frappe.msgprint("Pivotal Tracker story created successfully");
-                      frm.refresh();
-                      frm.reload_doc();
-                    }
+      // check if description exists
+            let description_content = `<div class="ql-editor read-mode"><p><br></p></div>`;
+            let description = frm.doc.description;
+            console.log(description)
+            if (description==description_content){
+              // get comments
+              const table_fields = [
+          			{
+          				fieldname: "comment", fieldtype: "Text Editor",
+          				in_list_view: 1, label: "Commment",
+          				// options: "company:company_currency",
+          				// change: function () {
+          				// 	dialog.fields_dict.balance_details.df.data.some(d => {
+          				// 		if (d.idx == this.doc.idx) {
+          				// 			d.opening_amount = this.value;
+          				// 			dialog.fields_dict.balance_details.grid.refresh();
+          				// 			return true;
+          				// 		}
+          				// 	});
+          				// }
+          			}
+          		];
+              let d = new frappe.ui.Dialog({
+              title: 'Get description from Commment',
+              fields: [
+                  {
+                      fieldname: "comments",
+                      fieldtype: "Table",
+                      label: "Comments",
+                      reqd: 1,
+                      data: [],
+                      fields: table_fields
+
+                  },
+              ],
+              primary_action_label: 'Submit',
+              primary_action(values) {
+
+                  frappe.confirm('Are you sure you want to proceed?',
+                  () => {
+                      // action to perform if Yes is selected
+                      console.log(values);
+                      frappe.call({
+                          method: "one_fm.api.doc_methods.issue.log_pivotal_tracker", //dotted path to server method
+                          freeze_message: 'Logging story to Pivotal Tracker',
+                          args:{name:frm.doc.name, comments: values.comments},
+                          callback: function(r) {
+                              // code snippet
+                              if(r.message.status=='success'){
+                                frappe.msgprint("Pivotal Tracker story created successfully");
+                                frm.refresh();
+                                frm.reload_doc();
+                              }
+                          }
+                      })
+                  }, () => {
+                      // action to perform if No is selected
+                  })
+                  d.hide();
+              }
+          });
+
+          d.show();
+          // populate table
+
+          frm.timeline.timeline_items.forEach((item, i) => {
+            if(item.icon=="small-message"){
+              d.fields_dict.comments.df.data.push(
+        				{
+                  comment: `${item.content[0].querySelector('.ql-editor.read-mode').outerHTML}`
                 }
-            })
-        }, () => {
-            // action to perform if No is selected
-        }
-      )
+        			);
+            }
+          });
+          d.fields_dict.comments.grid.refresh()
+
+
+        } else {
+            frappe.confirm('Are you sure you create Pivotal Tracker story?',
+              () => {
+                  // action to perform if Yes is selected
+                  frappe.call({
+                      method: "one_fm.api.doc_methods.issue.log_pivotal_tracker", //dotted path to server method
+                      freeze_message: 'Logging story to Pivotal Tracker',
+                      args:frm.doc,
+                      callback: function(r) {
+                          // code snippet
+                          if(r.message.status=='success'){
+                            frappe.msgprint("Pivotal Tracker story created successfully");
+                            frm.refresh();
+                            frm.reload_doc();
+                          }
+                      }
+                  })
+              }, () => {
+                  // action to perform if No is selected
+              }
+            )
+          }
+
     }, 'Create')
   }
 }
