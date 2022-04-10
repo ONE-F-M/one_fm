46147ed04c597dc6765a2eb633cf98854246ab1b
mymi14s <mymi14s@hotmail.com>
2022-04-10 09:42:33 +0100

    Desk link to Knowlwdge Base [181804931 #Finishes]

---
+++ one_fm/public/js/desk.js
@@ -0,0 +1,20 @@
+// dom ready
+document.addEventListener("DOMContentLoaded", (event)=>{
+  // Add knowledge base to help button
+  knowledgeBase();
+});
+
+
+// KNOWLEDGE BASE
+let knowledgeBase = () => {
+  // Add knowledge base to help button
+  // let helpbtn = $('#toolbar-help')[0].querySelector('.dropdown-divider.documentation-links');
+  let helpbtn = $('#toolbar-help')[0]
+  let faq = document.createElement('a');
+  faq.id="faq";
+  faq.className = "dropdown-item";
+  faq.href="/knowledge-base";
+  faq.innerText = "knowledge Base";
+  helpbtn.appendChild(faq);
+  // helpbtn.parentNode.insertBefore(faq, helpbtn.nextSibling);
+}
