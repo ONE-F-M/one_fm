28bc5db3e0dd242a8f155d52ff5e52567bd32103
mymi14s <mymi14s@hotmail.com>
2022-04-18 09:11:55 +0100

    fea: hide earning in salary slip

--- one_fm/public/js/doctype_js/salary_slip.js
+++ one_fm/public/js/doctype_js/salary_slip.js
@@ -3,5 +3,19 @@ frappe.ui.form.on('Salary Slip', {
 		if (frm.doc.justification_needed_on_deduction == 1){
 			frm.set_intro(__("Justification Needed on Deduction is True, Prepare Justification for PAM"), 'yellow');
 		}
+		// SET FIELDS DISPLAY FOR ROLES
+		setFieldDisplay(frm);
 	}
 });
+
+
+let setFieldDisplay = frm => {
+	// hide earnings and gross for employee
+	if (!(frappe.user_roles.includes('HR User')||frappe.user_roles.includes('HR Manager'))){
+		['earnings', 'gross_pay', 'gross_year_to_date',
+			'year_to_date', 'month_to_date'].forEach((item, i) => {
+				frm.toggle_display(item, 0);
+			});
+
+	}
+}
