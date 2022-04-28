2b5d552dd302bdd385bab5e8da005fdd8ad0a895
Ehtesham599 <ehtesham599@gmail.com>
2022-04-28 10:55:57 +0000

    Refactor opening object assignment

--- one_fm/www/careers/opening/index.py
+++ one_fm/www/careers/opening/index.py
@@ -7,10 +7,11 @@ def get_context(context):
 
     opening = {}
 
-    if job_id:
-        designation, description = frappe.db.get_value("Job Opening", {'name': job_id}, ["designation", "description"])
-        opening.update({'name': job_id})
-        opening.update({'designation': designation})
-        opening.update({'description': description})
+    job_opening = frappe.get_doc("Job Opening", {'name': job_id})
+    
+    if job_opening:
+        opening.update({'name': job_opening.name})
+        opening.update({'designation': job_opening.designation})
+        opening.update({'description': job_opening.description if job_opening.description else ""})
 
     context.opening = opening
