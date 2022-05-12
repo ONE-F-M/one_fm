75c63c5c7a55053e0818ca422136ef61de740034
SalShai <slwshkh@gmail.com>
2022-05-12 13:14:10 +0300

    Fetch Employee's Name in Arabic

--- one_fm/hiring/doctype/onboard_employee/onboard_employee.py
+++ one_fm/hiring/doctype/onboard_employee/onboard_employee.py
@@ -170,8 +170,8 @@ class OnboardEmployee(Document):
 					employee.one_fm_first_name_in_arabic = job_applicant.one_fm_first_name_in_arabic
 					employee.one_fm_last_name_in_arabic = job_applicant.one_fm_last_name_in_arabic
 				else:
-					employee.one_fm_first_name_in_arabic = self.employee_name_in_arabic
-					employee.one_fm_last_name_in_arabic = self.employee_name_in_arabic
+					employee.one_fm_first_name_in_arabic = self.employee_name_in_arabic.split()[len(doc.employee_name_in_arabic.split())-1]
+					employee.one_fm_last_name_in_arabic = self.employee_name_in_arabic.split()[0]
 				if self.declaration_of_electronic_signature:
 					employee.employee_signature = frappe.get_value("Electronic Signature Declaration",self.declaration_of_electronic_signature,['applicant_signature'])
 
