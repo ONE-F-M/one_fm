dda480336ff829393c07267e2bef9f5fb807b3aa
Ehtesham599 <ehtesham599@gmail.com>
2022-05-29 13:58:58 +0000

    Remove year filter

--- one_fm/one_fm/report/roster_projection/roster_projection.js
+++ one_fm/one_fm/report/roster_projection/roster_projection.js
@@ -32,28 +32,6 @@ frappe.query_reports["Roster Projection"] = {
 				{ "value": 12, "label": __("Dec") },
 			],
 			"default": frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth() + 1
-		},
-		{
-			"fieldname":"year",
-			"label": __("Year"),
-			"fieldtype": "Select",
-			"reqd": 1,
-			"options": [
-				{ "value": 2016, "label": __("2016") },
-				{ "value": 2017, "label": __("2017") },
-				{ "value": 2018, "label": __("2018") },
-				{ "value": 2019, "label": __("2019") },
-				{ "value": 2020, "label": __("2020") },
-				{ "value": 2021, "label": __("2021") },
-				{ "value": 2022, "label": __("2022") },
-				{ "value": 2023, "label": __("2023") },
-				{ "value": 2024, "label": __("2024") },
-				{ "value": 2025, "label": __("2025") },
-				{ "value": 2026, "label": __("2026") },
-				{ "value": 2027, "label": __("2027") },
-				{ "value": 2028, "label": __("2028") },
-			],
-			"default": frappe.datetime.str_to_obj(frappe.datetime.get_today()).getFullYear()
 		}
 	]
 };
