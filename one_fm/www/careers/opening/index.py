f416265ef676533b16926fc54d3d6df2d912d8f0
mymi14s <mymi14s@hotmail.com>
2022-06-08 13:54:45 +0100

    hotfix: website job section

--- one_fm/www/careers/opening/index.py
+++ one_fm/www/careers/opening/index.py
@@ -2,13 +2,13 @@ import frappe
 from ..utils import remove_html_tags
 
 def get_context(context):
-
+    no_cache = 1
     job_id = frappe.form_dict.job_id
 
     opening = {}
 
     job_opening = frappe.get_doc("Job Opening", {'name': job_id})
-    
+
     if job_opening:
         opening.update({'name': job_opening.name})
         opening.update({'designation': job_opening.designation})
