020825c3cfeaf76ea18a0d457497face2daee3f8
pjamsheer <kuttypjamsheer@gmail.com>
2022-07-17 23:28:08 +0530

    feat: Amend - Attendace request [#182629395]

--- one_fm/public/js/doctype_js/attendance_request.js
+++ one_fm/public/js/doctype_js/attendance_request.js
@@ -1,6 +1,7 @@
 frappe.ui.form.on('Attendance Request', {
     refresh: (frm)=>{
       frm.trigger('check_workflow');
+			set_update_request_btn(frm);
     },
     check_workflow: (frm)=>{
       if(frm.doc.workflow_state=='Pending Approval'){
@@ -14,3 +15,53 @@ frappe.ui.form.on('Attendance Request', {
       }
     }
 });
+
+function set_update_request_btn(frm) {
+	if(frm.doc.docstatus == 1 && frm.doc.workflow_state == 'Approved' && !frm.doc.update_request){
+		if(frappe.user.has_role('Shift Supervisor')){
+			frm.add_custom_button(__('Update Request'), function() {
+				update_request(frm);
+			});
+		}
+	}
+};
+
+function update_request(frm) {
+	var dialog = new frappe.ui.Dialog({
+		title: 'Update Request',
+		fields: [
+			{fieldtype: "Date", label: "From Date", fieldname: "from_date", reqd: true},
+			{fieldtype: "Date", label: "To Date", fieldname: "to_date", reqd: true},
+		],
+		primary_action_label: __("Update"),
+		primary_action : function(){
+			frappe.confirm(
+				__('Are you sure to proceed?'),
+				function(){
+					// Yes
+					frappe.call({
+						method: 'one_fm.overrides.attendance_request.update_request',
+						args: {
+							attendance_request: frm.doc.name,
+							from_date: dialog.get_value('from_date'),
+							to_date: dialog.get_value('to_date'),
+						},
+						callback: function(r) {
+							if(!r.exc) {
+								frm.reload_doc();
+							}
+						},
+						freaze: true,
+						freaze_message: __("Update Request ..")
+					});
+					dialog.hide();
+				},
+				function(){
+					// No
+					dialog.hide();
+				}
+			);
+		}
+	});
+	dialog.show();
+};
