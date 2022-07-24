5b0a1dff0369991e11dd9fc2b07fcc0b9d974fec
Ehtesham599 <ehtesham599@gmail.com>
2022-07-24 11:51:11 +0000

    comment unused functions and imports for face recognition enroll/verify

--- one_fm/one_fm/page/face_recognition/face_recognition.py
+++ one_fm/one_fm/page/face_recognition/face_recognition.py
@@ -7,10 +7,10 @@ from frappe.utils import now_datetime, cstr, nowdate, cint , getdate
 import numpy as np
 import datetime
 from json import JSONEncoder
-import cv2, os
-import face_recognition
+# import cv2, os
+# import face_recognition
 import json
-from imutils import face_utils, paths
+# from imutils import face_utils, paths
 from one_fm.api.doc_events import haversine
 from one_fm.api.v1.roster import get_current_shift
 
@@ -184,28 +184,28 @@ def update_onboarding_employee(employee):
         onboard_employee.save(ignore_permissions=True)
         frappe.db.commit()
 
-def create_dataset(video):
-	OUTPUT_DIRECTORY = frappe.utils.cstr(frappe.local.site)+"/private/files/dataset/"+frappe.session.user+"/"
-	count = 0 
+# def create_dataset(video):
+# 	OUTPUT_DIRECTORY = frappe.utils.cstr(frappe.local.site)+"/private/files/dataset/"+frappe.session.user+"/"
+# 	count = 0 
 	
-	cap = cv2.VideoCapture(video)
-	success, img = cap.read()
-	count = 0
-	while success:
-		#Resizing the image
-		img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-		#Limiting the number of images for training. %5 gives 10 images %5.8 -> 8 images %6.7 ->7 images
-		if count%5 == 0 :
-			cv2.imwrite(OUTPUT_DIRECTORY + "{0}.jpg".format(count+1), img)
-		count = count + 1
-		success, img = cap.read()
+# 	cap = cv2.VideoCapture(video)
+# 	success, img = cap.read()
+# 	count = 0
+# 	while success:
+# 		#Resizing the image
+# 		img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+# 		#Limiting the number of images for training. %5 gives 10 images %5.8 -> 8 images %6.7 ->7 images
+# 		if count%5 == 0 :
+# 			cv2.imwrite(OUTPUT_DIRECTORY + "{0}.jpg".format(count+1), img)
+# 		count = count + 1
+# 		success, img = cap.read()
 
-	create_encodings(OUTPUT_DIRECTORY)
-	doc = frappe.get_doc("Employee", {"user_id": frappe.session.user})
-	print(doc.as_dict())
-	doc.enrolled = 1
-	doc.save(ignore_permissions=True)
-	frappe.db.commit()
+# 	create_encodings(OUTPUT_DIRECTORY)
+# 	doc = frappe.get_doc("Employee", {"user_id": frappe.session.user})
+# 	print(doc.as_dict())
+# 	doc.enrolled = 1
+# 	doc.save(ignore_permissions=True)
+# 	frappe.db.commit()
 
 def update_onboarding_employee(employee):
     onboard_employee_exist = frappe.db.exists('Onboard Employee', {'employee': employee.name})
@@ -216,55 +216,55 @@ def update_onboarding_employee(employee):
         onboard_employee.save(ignore_permissions=True)
         frappe.db.commit()
 
-def create_encodings(directory, detection_method="hog"):# detection_method can be "hog" or "cnn". cnn is more cpu and memory intensive.
-	"""
-		directory : directory path containing dataset 
-	"""
-	print(directory)
-	OUTPUT_ENCODING_PATH_PREFIX = frappe.utils.cstr(frappe.local.site)+"/private/files/facial_recognition/"
-	user_id = frappe.session.user
-	# grab the paths to the input images in our dataset
-	imagePaths = list(paths.list_images(directory))
-	print(imagePaths)
-	#encodings file output path
-	encoding_path = OUTPUT_ENCODING_PATH_PREFIX + user_id +".json"
-	# initialize the list of known encodings and known names
-	knownEncodings = []
-	# knownNames = []
+# def create_encodings(directory, detection_method="hog"):# detection_method can be "hog" or "cnn". cnn is more cpu and memory intensive.
+# 	"""
+# 		directory : directory path containing dataset 
+# 	"""
+# 	print(directory)
+# 	OUTPUT_ENCODING_PATH_PREFIX = frappe.utils.cstr(frappe.local.site)+"/private/files/facial_recognition/"
+# 	user_id = frappe.session.user
+# 	# grab the paths to the input images in our dataset
+# 	imagePaths = list(paths.list_images(directory))
+# 	print(imagePaths)
+# 	#encodings file output path
+# 	encoding_path = OUTPUT_ENCODING_PATH_PREFIX + user_id +".json"
+# 	# initialize the list of known encodings and known names
+# 	knownEncodings = []
+# 	# knownNames = []
 
-	for (i, imagePath) in enumerate(imagePaths):
-		# extract the person name from the image path i.e User Id
-		print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
-		name = imagePath.split(os.path.sep)[-2]
+# 	for (i, imagePath) in enumerate(imagePaths):
+# 		# extract the person name from the image path i.e User Id
+# 		print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+# 		name = imagePath.split(os.path.sep)[-2]
 
-		# load the input image and convert it from BGR (OpenCV ordering)
-		# to dlib ordering (RGB)
-		image = cv2.imread(imagePath)
-		#BGR to RGB conversion
-		rgb =  image[:, :, ::-1]
+# 		# load the input image and convert it from BGR (OpenCV ordering)
+# 		# to dlib ordering (RGB)
+# 		image = cv2.imread(imagePath)
+# 		#BGR to RGB conversion
+# 		rgb =  image[:, :, ::-1]
 
-		# detect the (x, y)-coordinates of the bounding boxes
-		# corresponding to each face in the input image
-		boxes = face_recognition.face_locations(rgb, model=detection_method)
+# 		# detect the (x, y)-coordinates of the bounding boxes
+# 		# corresponding to each face in the input image
+# 		boxes = face_recognition.face_locations(rgb, model=detection_method)
 
-		# compute the facial embedding for the face
-		encodings = face_recognition.face_encodings(rgb, boxes)
+# 		# compute the facial embedding for the face
+# 		encodings = face_recognition.face_encodings(rgb, boxes)
 
-		# loop over the encodings
-		for encoding in encodings:
-			# add each encoding + name to our set of known names and
-			# encodings
-			knownEncodings.append(encoding)
+# 		# loop over the encodings
+# 		for encoding in encodings:
+# 			# add each encoding + name to our set of known names and
+# 			# encodings
+# 			knownEncodings.append(encoding)
 
-	# dump the facial encodings + names to disk	
-	data = {"encodings": knownEncodings}
-	print(data)
-	if len(knownEncodings) == 0:
-		frappe.throw(_("No face found in the video. Please make sure you position your face correctly in front of the camera."))
-	data = json.dumps(data, cls=NumpyArrayEncoder)
-	with open(encoding_path,"w") as f:
-		f.write(data)
-		f.close()
+# 	# dump the facial encodings + names to disk	
+# 	data = {"encodings": knownEncodings}
+# 	print(data)
+# 	if len(knownEncodings) == 0:
+# 		frappe.throw(_("No face found in the video. Please make sure you position your face correctly in front of the camera."))
+# 	data = json.dumps(data, cls=NumpyArrayEncoder)
+# 	with open(encoding_path,"w") as f:
+# 		f.write(data)
+# 		f.close()
 
 
 @frappe.whitelist()
@@ -311,52 +311,52 @@ def check_existing():
 	else:
 		return True
 
-def recognize_face(image):
-	try:
-		ENCODINGS_PATH = frappe.utils.cstr(
-			frappe.local.site)+"/private/files/facial_recognition/"+frappe.session.user+".json"
-		# values should be "hog" or "cnn" . cnn is CPU and memory intensive.
-		DETECTION_METHOD = "hog"
+# def recognize_face(image):
+# 	try:
+# 		ENCODINGS_PATH = frappe.utils.cstr(
+# 			frappe.local.site)+"/private/files/facial_recognition/"+frappe.session.user+".json"
+# 		# values should be "hog" or "cnn" . cnn is CPU and memory intensive.
+# 		DETECTION_METHOD = "hog"
 
-		# load the known faces and embeddings
-		face_data = json.loads(open(ENCODINGS_PATH, "rb").read())
+# 		# load the known faces and embeddings
+# 		face_data = json.loads(open(ENCODINGS_PATH, "rb").read())
 
-		# load the input image and convert it from BGR to RGB
-		image = cv2.imread(image)
-		rgb =  image[:, :, ::-1]
+# 		# load the input image and convert it from BGR to RGB
+# 		image = cv2.imread(image)
+# 		rgb =  image[:, :, ::-1]
 
-		# detect the (x, y)-coordinates of the bounding boxes corresponding
-		# to each face in the input image, then compute the facial embeddings
-		# for each face
-		boxes = face_recognition.face_locations(rgb,
-												model=DETECTION_METHOD)
-		encodings = face_recognition.face_encodings(rgb, boxes)
+# 		# detect the (x, y)-coordinates of the bounding boxes corresponding
+# 		# to each face in the input image, then compute the facial embeddings
+# 		# for each face
+# 		boxes = face_recognition.face_locations(rgb,
+# 												model=DETECTION_METHOD)
+# 		encodings = face_recognition.face_encodings(rgb, boxes)
 
-		if not encodings:
-			return False
-		return match_encodings(encodings, face_data)
+# 		if not encodings:
+# 			return False
+# 		return match_encodings(encodings, face_data)
 
-	except Exception as e:
-		print(frappe.get_traceback())
+# 	except Exception as e:
+# 		print(frappe.get_traceback())
 
 
-def match_encodings(encodings, face_data):
-	try:
-		# loop over the facial embeddings
-		for encoding in encodings:
-			# attempt to match each face in the input image to our known
-			# encodings
-			matches = face_recognition.compare_faces(
-				face_data["encodings"], encoding)
-			# check to see if we have found a match
-			if True in matches:
-				# find the indexes of all matched faces
-				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-				print(matchedIdxs, matches)
-				return True if ((len(matchedIdxs) / len(matches)) * 100 > 80) else False
-			else:
-				return False
-		else:
-			return False
-	except Exception as identifier:
-		print(frappe.get_traceback())
+# def match_encodings(encodings, face_data):
+# 	try:
+# 		# loop over the facial embeddings
+# 		for encoding in encodings:
+# 			# attempt to match each face in the input image to our known
+# 			# encodings
+# 			matches = face_recognition.compare_faces(
+# 				face_data["encodings"], encoding)
+# 			# check to see if we have found a match
+# 			if True in matches:
+# 				# find the indexes of all matched faces
+# 				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+# 				print(matchedIdxs, matches)
+# 				return True if ((len(matchedIdxs) / len(matches)) * 100 > 80) else False
+# 			else:
+# 				return False
+# 		else:
+# 			return False
+# 	except Exception as identifier:
+# 		print(frappe.get_traceback())
