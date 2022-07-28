95c1d596cf8cc69445f925d0c41652c866a3d7c2
mymi14s <mymi14s@hotmail.com>
2022-07-28 10:34:05 +0100

    complete: Roster Projection View

--- one_fm/operations/report/roster_projection_view/roster_projection_view.py
+++ one_fm/operations/report/roster_projection_view/roster_projection_view.py
@@ -29,7 +29,7 @@ def get_data(filters):
 
 	contracts = frappe.db.get_list('Contracts')
 	for contracts in contracts:
-		query = f"""
+		es_query = f"""
 			SELECT con.client, con.project, con_item.item_code, con_item.count, con_item.rate, 
 			con_item.count*con_item.rate as total_rate, COUNT(es.name) as projection
 			FROM `tabContracts` con
@@ -41,9 +41,8 @@ def get_data(filters):
 			AND con_item.item_code=pt.sale_item
 			AND es.date BETWEEN "{filters.year}-{filters.month}-01" AND "{filters.year}-{filters.month}-31"
 			GROUP BY con_item.item_code
-			
-			UNION 
-			
+		"""
+		ps_query = f"""
 			SELECT con.client, con.project, con_item.item_code, con_item.count, con_item.rate, 
 			con_item.count*con_item.rate as total_rate, COUNT(ps.name) as projection
 			FROM `tabContracts` con
@@ -56,32 +55,54 @@ def get_data(filters):
 			AND ps.date BETWEEN "{filters.year}-{filters.month}-01" AND "{filters.year}-{filters.month}-31"
 			GROUP BY con_item.item_code
 		"""
-		data = frappe.db.sql(query, as_dict=1)
-		# if len(data) == 2:
-		# 	data[0].projection = data[0].projection/data[1].projection
-		# 	data[0].projection_rate = data[0].rate*data[0].projection
-		#
-		# 	# Compute live projection
-		# 	if use_attendance:
-		# 		attendance_data = get_live_attendance_query(contracts, filters, to_date)
-		# 		if attendance_data:
-		# 			if add_schedule:
-		# 				schedule_data = get_live_schedule_query(contracts, filters, live_today)
-		# 				if schedule_data:
-		# 					attendance_data[0].attendance += schedule_data[0].projection
-		# 			data[0].live_projection = attendance_data[0].attendance/data[1].projection
-		# 		data[0].live_projection = data[0].projection
-		# 	else:
-		# 		data[0].live_projection = data[0].projection
-		#
-		# 	data[0].live_projection_rate = data[0].live_projection * data[0].rate
-		# 	results.append(data[0])
-		# data = None
-		if contracts.name == 'Wafra Real Estate Company-Wafra Living-2021-08-01':
-			print(data)
-		results += data
-	return results
+		es_data = frappe.db.sql(es_query, as_dict=1)
+		ps_data = frappe.db.sql(ps_query, as_dict=1)
+		ps_data_dict = {}
+		at_data_dict = {}
+		for i in ps_data:
+			ps_data_dict[i.project+'-'+i.item_code] = i
 
+		for i in es_data:
+			ps_key = ps_data_dict.get(i.project+'-'+i.item_code)
+			if ps_key:
+				i.projection = i.projection/ps_key.projection
+				i.projection_rate = i.rate*i.projection
+				i.ps_projection = ps_key.projection
+			else:
+				i.projection = 0
+				i.projection_rate = 0
+				i.ps_projection = 0
+
+		# 	# Compute live projection
+		if es_data and use_attendance:
+			attendance_data = get_live_attendance_query(contracts, filters, to_date)
+
+			if attendance_data:
+				for i in attendance_data:
+					at_data_dict[i.project+'-'+i.item_code] = i
+				if add_schedule:
+					schedule_data = get_live_schedule_query(contracts, filters, live_today)
+					if schedule_data:
+						for i in schedule_data:
+							sdt_key = at_data_dict.get(i.project+'-'+i.item_code)
+							if sdt_key:
+								sdt_key.attendance += i.projection
+
+
+				for i in es_data:
+					at_key = at_data_dict.get(i.project+'-'+i.item_code)
+					if at_key:
+						i.live_projection = at_key.attendance/i.ps_projection if i.ps_projection else 0
+						i.live_projection_rate = i.live_projection * i.rate
+					else:
+						i.live_projection = 0
+						i.live_projection_rate = 0
+
+
+		if es_data:
+			results+=es_data
+
+	return results
 
 def get_live_schedule_query(contracts, filters, start_date):
 	schedule_query = f"""
