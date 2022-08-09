e81f202ac5f6f8cdb381f627fd081c09afa1141b
pjamsheer <kuttypjamsheer@gmail.com>
2022-08-09 12:56:32 +0530

    feat: Payroll Cycle configuration [#182825885]

--- one_fm/one_fm/doctype/hr_and_payroll_additional_settings/hr_and_payroll_additional_settings.py
+++ one_fm/one_fm/doctype/hr_and_payroll_additional_settings/hr_and_payroll_additional_settings.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2021, omar jaber and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class HRandPayrollAdditionalSettings(Document):
-	pass
+	@frappe.whitelist()
+	def get_projects_not_set_in_payroll_cycle_but_assigned_in_employee(self):
+		project_list = ', '.join(['"{}"'.format(payroll_cycle.project) for payroll_cycle in self.project_payroll_cycle])
+		query = '''
+			select
+				distinct project
+			from
+				tabEmployee
+			where
+				status = "Active"
+				and
+				project NOT IN ({0})
+		'''
+		return frappe.db.sql(query.format(project_list))
