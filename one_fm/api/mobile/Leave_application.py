5843939f7ccd97699bfd5268d3bee07f3a1a60ae
SalShai <slwshkh@gmail.com>
2022-08-11 12:30:19 +0300

    Correct Message

--- one_fm/api/mobile/Leave_application.py
+++ one_fm/api/mobile/Leave_application.py
@@ -221,7 +221,7 @@ def notify_leave_approver(doc):
         else:
             date = "from "+cstr(doc.from_date)+" to "+cstr(doc.to_date)
 
-        push_notication_message = doc.employee_name+" has applied for "+doc.leave_type+" Application "+date+". Kindly, take action."
+        push_notication_message = doc.employee_name+" has applied for "+doc.leave_type+" "+date+". Kindly, take action."
         push_notification_rest_api_for_leave_application(employee_id,"Leave Application", push_notication_message, False)
 
 
