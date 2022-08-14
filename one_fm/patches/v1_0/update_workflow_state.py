6abe81c6d141d203b5b2f658b2efab934d194b7d
SalShai <slwshkh@gmail.com>
2022-08-14 13:31:44 +0300

    Fix:comment

--- one_fm/patches/v1_0/update_workflow_state.py
+++ one_fm/patches/v1_0/update_workflow_state.py
@@ -4,6 +4,6 @@ import frappe
 def execute():
     frappe.enqueue(update_workflowstate, queue='long')
 
-#This Function is patch to update the Notification log with category, title and body. 
+#This Function is patch to update the Workflow State from ''Draft' to 'Open'. 
 def update_workflowstate():
     frappe.db.sql("""UPDATE `tabLeave Application` SET workflow_state='Open' WHERE workflow_state='Draft';""")
