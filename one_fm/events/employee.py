1473a96fb7af51b88cd7b83e442ec3dd9f5d61a3
Anthony C. Emmanuel <mymi14s@hotmail.com>
2022-10-23 13:10:36 +0100

    Update employee.py

--- one_fm/events/employee.py
+++ one_fm/events/employee.py
@@ -5,7 +5,7 @@ from frappe.utils import getdate
 def before_save(doc, event):
     current_doc = frappe.get_doc("Employee", doc.name)
     if (doc.shift != current_doc.shift) and (doc.shift_working != current_doc.shift_working):
-        schedules = frappe.get_list("Employee Schedule", filters={'employee':'HR-EMP-00808', 'date': ['>', getdate()]})
+        schedules = frappe.get_list("Employee Schedule", filters={'employee':doc.name, 'date': ['>', getdate()]})
         for name in schedules:
             frappe.get_doc("Employee Schedule", name).delete()
 
