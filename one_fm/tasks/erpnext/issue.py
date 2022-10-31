0d0a538e5b218c1f15caa76580890931098c2ecd
NoBoneZ <adekunleabraham09@gmail.com>
2022-10-31 13:35:45 +0100

    fixed log issues, filter via external ip

--- one_fm/tasks/erpnext/issue.py
+++ one_fm/tasks/erpnext/issue.py
@@ -1,4 +1,4 @@
-import socket
+import os
 
 import frappe
 from one_fm.api.slack import send_slack_message
@@ -10,22 +10,14 @@ def daily_open():
         send and notifify slack about issue exceeding 2hrs since
         status was last changed to open
     """
-
     """
         Checks if it is the production ip or any allowed ip that is sending the request, if not, it fails silently
 
     """
     allowed_ip = frappe.get_doc("Default API Integration").allowed_ip_address
-
     allowed_ip_list = [i.address for i in allowed_ip]
-    
-
-    host_name = socket.gethostname()
-
-    ip = socket.gethostbyname(host_name)
-
-
-    if ip == "34.105.145.77" or ip in allowed_ip_list:
+    external_IP = os.popen('curl -s ifconfig.me').readline()
+    if external_IP in allowed_ip_list:
         query = frappe.db.sql("""
             SELECT COUNT(status) as status_count FROM `tabIssue` WHERE status='Open' AND DATEDIFF(NOW(), modified)>0;
         """, as_dict=1)
@@ -33,5 +25,6 @@ def daily_open():
         if query:
             # send slack message if open issues
             send_slack_message(f"There are {query} open issue(s) that have not been replied to in the last 24 hours")
+
     else:
         pass
