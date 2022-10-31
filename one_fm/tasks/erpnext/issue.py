30875cac2e859bc2423c981e77cf43b5a0ea4d8c
NoBoneZ <adekunleabraham09@gmail.com>
2022-10-31 10:56:32 +0100

    fixed log issues, filter via IP

--- one_fm/tasks/erpnext/issue.py
+++ one_fm/tasks/erpnext/issue.py
@@ -1,3 +1,5 @@
+import socket
+
 import frappe
 from one_fm.api.slack import send_slack_message
 
@@ -5,12 +7,31 @@ from one_fm.api.slack import send_slack_message
 
 def daily_open():
     """
-        send and notifify slact about issue exceeding 2hrs since
+        send and notifify slack about issue exceeding 2hrs since
         status was last changed to open
     """
-    query = len(frappe.db.sql("""
-        SELECT status FROM `tabIssue` WHERE status='Open' AND DATEDIFF(NOW(), modified)>0;
-    """, as_dict=1))
-    if query:
-        # send slack message if open issues
-        send_slack_message(f"There are {query} open issue(s) that have not been replied to in the last 24 hours")
+
+    """
+        Checks if it is the production ip or any allowed ip that is sending the request, if not, it fails silently
+
+    """
+    allowed_ip = frappe.get_doc("Default API Integration").allowed_ip_address
+
+    allowed_ip_list = [i.address for i in allowed_ip]
+    
+
+    host_name = socket.gethostname()
+
+    ip = socket.gethostbyname(host_name)
+
+
+    if ip == "34.105.145.77" or ip in allowed_ip_list:
+        query = frappe.db.sql("""
+            SELECT COUNT(status) as status_count FROM `tabIssue` WHERE status='Open' AND DATEDIFF(NOW(), modified)>0;
+        """, as_dict=1)
+
+        if query:
+            # send slack message if open issues
+            send_slack_message(f"There are {query} open issue(s) that have not been replied to in the last 24 hours")
+    else:
+        pass
