83dcb30f93520245ed46d9ec8e2f3d7e6353671a
NoBoneZ <adekunleabraham09@gmail.com>
2022-11-21 16:21:06 +0100

    created more schedules

--- one_fm/events/operations_post.py
+++ one_fm/events/operations_post.py
@@ -1,29 +1,40 @@
 import frappe
 import datetime
+import pandas as pd
 
 from frappe.utils import getdate, add_to_date, cstr
 
 
 def after_insert(doc, method=None):
-    try:
-        current_doc = frappe.get_doc("Operations Post", doc.name)   
-        new_doc = frappe.get_doc({
-            "doctype": "Post Schedule",
-            "date": cstr(datetime.datetime.now().date() + datetime.timedelta(days=1)),
-            "post": current_doc.name,
-            "post_status": "Planned",
+    return frappe.enqueue(set_post_schedule(doc=doc), is_async=True, queue="long")
 
-        })
-        new_doc.save()
-    except:
-            pass
 
-    
+def set_post_schedule(doc):
+    project = frappe.get_doc("Project", doc.project)
+
+    if project.expected_end_date is None:
+        end_date = add_to_date(getdate(), days=365)
+    else:
+        year, month, day = str(project.expected_end_date).split("-")
+        in_days = (datetime.datetime(year, month, day) - datetime.datetime.now()).days
+        end_date = add_to_date(getdate(), days=in_days)
+
+    for date in pd.date_range(start=getdate(), end=end_date):
+                check_doc = frappe.get_doc({
+                "doctype": "Post Schedule",
+                "date": cstr(date.date()),
+                "post": doc.name,
+                "post_status": "Planned"
+                    })
+                check_doc.save()
+    frappe.db.commit()
+
+
+
 
 def on_trash(doc, method=None):
     try:
-        current_doc = frappe.get_doc("Operations Post", doc.name)
-        check_list = frappe.db.get_list("Post Schedule", filters={"post": current_doc.name, "date": [">", getdate()]})
+        check_list = frappe.db.get_list("Post Schedule", filters={"post": doc.name, "date": [">", getdate()]})
         for schedule in check_list:
             frappe.get_doc("Post Schedule", schedule.get("name")).delete()
     except:
