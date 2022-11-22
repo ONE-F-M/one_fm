2d9bf1c76b76c49ba881ffc52582cdd74b959548
mymi14s <mymi14s@hotmail.com>
2022-11-22 07:20:23 +0100

    Fix: force push email instantly

--- one_fm/events/email_queue.py
+++ one_fm/events/email_queue.py
@@ -1,6 +1,7 @@
 import frappe
-
+from frappe.email.doctype.email_queue.email_queue import send_now
 def after_insert(doc, event):
+    found = False
     for row in doc.recipients:
         if row.recipient.startswith('2'):
             frappe.db.sql("""
@@ -8,4 +9,7 @@ def after_insert(doc, event):
                 WHERE name IN (SELECT e.name FROM `tabEmail Queue` e JOIN `tabEmail Queue Recipient` r 
                 ON r.parent=e.name WHERE r.recipient LIKE '2%');
             """)
+            found = True
             break
+    if found:
+        send_now(name=doc.name)
