42ac4894993970b7d9f053965bf8ced9fa75477b
mymi14s <mymi14s@hotmail.com>
2022-11-22 22:02:32 +0100

    periodic flush email

--- one_fm/events/email_queue.py
+++ one_fm/events/email_queue.py
@@ -1,15 +1,41 @@
 import frappe
 from frappe.email.doctype.email_queue.email_queue import send_now
 def after_insert(doc, event):
+    """
+    Check created email log if sent to employee ID and force push if not
+    :param doc:
+    :param event:
+    :return:
+    """
     found = True
     for row in doc.recipients:
         if row.recipient.startswith('2'):
-            frappe.db.sql("""
-                DELETE FROM `tabEmail Queue` 
-                WHERE name IN (SELECT e.name FROM `tabEmail Queue` e JOIN `tabEmail Queue Recipient` r 
-                ON r.parent=e.name WHERE r.recipient LIKE '2%');
-            """)
+            delete_eid_emails()
             found = False
             break
     if found:
         send_now(name=doc.name)
+
+
+def flush_emails():
+    """
+    This function flush emails not sent in queue
+    :return:
+    """
+    delete_eid_emails()
+    emails_in_queue = frappe.get_list('Email Queue', filters={'status': 'Not Sent'})
+    for row in emails_in_queue:
+        try:send_now(name=row.name)
+        except:pass
+    frappe.db.commit()
+
+def delete_eid_emails():
+    """
+    This function delete emails sent to employee ID
+    :return:
+    """
+    frappe.db.sql("""
+        DELETE FROM `tabEmail Queue` 
+        WHERE name IN (SELECT e.name FROM `tabEmail Queue` e JOIN `tabEmail Queue Recipient` r 
+        ON r.parent=e.name WHERE r.recipient LIKE '2%');
+    """)
--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -495,7 +495,8 @@ scheduler_events = {
 		],
 		"0/15 * * * *": [ #At every 15th minute from 0 through 59.”
 			"one_fm.legal.doctype.penalty.penalty.automatic_reject",
-			'one_fm.api.tasks.process_attendance'
+			'one_fm.api.tasks.process_attendance',
+			"one_fm.events.email_queue.flush_emails",
 		],
 		"0/5 * * * *": [
 			"one_fm.api.tasks.checkin_checkout_supervisor_reminder",
