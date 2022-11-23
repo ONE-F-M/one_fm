283ad6345235d2053dfb6bb6152fb20fd3981e7f
NoBoneZ <adekunleabraham09@gmail.com>
2022-11-23 11:26:27 +0100

    made changes on js file

--- one_fm/operations/doctype/operations_post/operations_post_list.js
+++ one_fm/operations/doctype/operations_post/operations_post_list.js
@@ -1,15 +1,3 @@
-// frappe.listview_settings['Operations Post'] = {
-// 	add_fields: ["paused"],
-// 	get_indicator: function(doc) {
-// 		if(!doc.paused) {
-// 			return [__("Active"), "green", "paused,=,No"];
-// 		} else if(doc.paused) {
-// 			return [__("Paused"), "red", "paused,=,Yes"];
-// 		}
-// 	}
-// };
-
-
 frappe.listview_settings['Operations Post'] = {
 	add_fields: ["paused"],
 	get_indicator: function(doc) {
