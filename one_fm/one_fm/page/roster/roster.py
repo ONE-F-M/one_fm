02f98e93f6bc445fa7d33bf5d83b4163e1b83759
Ebuka <ebukaakeru@manqala.com>
2022-12-10 01:38:56 +0100

    Enhanced roster page

--- one_fm/one_fm/page/roster/roster.py
+++ one_fm/one_fm/page/roster/roster.py
@@ -125,7 +125,7 @@ def get_roster_view(start_date, end_date, assigned=0, scheduled=0, employee_sear
 	#--------------------- Fetch Employee list ----------------------------#
 	if isOt:
 		employee_filters.update({'employee_availability' : 'Working'})
-		employees = frappe.db.get_list("Employee Schedule", employee_filters, ["distinct employee", "employee_name"], order_by="employee_name asc" ,limit_start=limit_start, limit_page_length=25, ignore_permissions=True)
+		employees = frappe.db.get_list("Employee Schedule", employee_filters, ["distinct employee", "employee_name"], order_by="employee_name asc" ,limit_start=limit_start, limit_page_length=limit_page_length, ignore_permissions=True)
 		master_data.update({'total' : len(employees)})
 		employee_filters.update({'date': ['between', (start_date, end_date)], 'post_status': 'Planned'})
 		employee_filters.pop('employee_availability')
