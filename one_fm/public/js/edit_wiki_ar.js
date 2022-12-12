5407a7ea38a9fc92052121797128a26c84538316
NoBoneZ <adekunleabraham09@gmail.com>
2022-12-12 23:19:50 +0100

    resolved conflicts

--- one_fm/public/js/edit_wiki_ar.js
+++ one_fm/public/js/edit_wiki_ar.js
@@ -91,8 +91,10 @@ window.EditWikiAr = class EditWikiAr extends Wiki {
 				primary_action: function (fields) {
 					if (fields.type == "Add Wiki Page") {
 						me.add_wiki_page(fields);
-					} else {
+					} else if(fields.type == "Group") {
 						me.add_wiki_sidebar(fields);
+					} else if(fields.type == "Page"){
+						me.add_wiki_sidebar_page(fields);
 					}
 					dialog.hide();
 				},
@@ -189,6 +191,37 @@ window.EditWikiAr = class EditWikiAr extends Wiki {
 					pull: ["qux"],
 				},
 			});
+		});
+	}
+
+	add_wiki_sidebar_page(fields) {
+		var me = this;
+		frappe.call({
+			method: "frappe.client.get",
+			args: {
+				doctype: "Wiki Page",
+				name: fields.wiki_page,
+			},
+			callback: function (r) {
+				let $new_page = me.get_wiki_sidebar_html(r.message);
+
+				$new_page.appendTo(
+					$(".doc-sidebar .sidebar-items")
+						.children(".list-unstyled")
+						.not(".hidden")
+						.first()
+				);
+
+				$(".web-sidebar ul").each(function () {
+					new Sortable(this, {
+						group: {
+							name: "qux",
+							put: ["qux"],
+							pull: ["qux"],
+						},
+					});
+				});
+			},
 		});
 	}
 
