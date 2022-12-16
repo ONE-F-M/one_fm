36604ca4a7b4c375e40268b27cc939681d427ebc
mymi14s <mymi14s@hotmail.com>
2022-12-16 10:47:16 +0100

    fix: Job Offer Patch

--- one_fm/patches/v14_0/set_job_offer_field_valuse_from_applicant.py
+++ one_fm/patches/v14_0/set_job_offer_field_valuse_from_applicant.py
@@ -4,17 +4,20 @@ def execute():
 	job_offers = frappe.get_list("Job Offer", {'docstatus': ['<', 2]})
 	for job_offer in job_offers:
 		doc = frappe.get_doc("Job Offer", job_offer.name)
-		if doc.docstatus == 0:
-			doc.save(ignore_permissions=True)
-		elif doc.docstatus == 1:
-			job_applicant = frappe.get_doc('Job Applicant', doc.job_applicant)
-			query = """
-				Update
-					`tabJob Offer`
-				set
-					agency='{0}', nationality='{1}', department='{2}'
-				where
-					name='{3}'
-			"""
-			frappe.db.sql(query.format(job_applicant.one_fm_agency or '', job_applicant.one_fm_nationality or '', job_applicant.department or '', doc.name))
-			frappe.db.commit()
+		if doc.one_fm_erf:
+			erf = frappe.get_doc("ERF", doc.one_fm_erf, "docstatus")
+			if not erf.docstatus==2 and erf.salary_structure:
+				if doc.docstatus == 0:
+					doc.save(ignore_permissions=True)
+				elif doc.docstatus == 1:
+					job_applicant = frappe.get_doc('Job Applicant', doc.job_applicant)
+					query = """
+						Update
+							`tabJob Offer`
+						set
+							agency='{0}', nationality='{1}', department='{2}'
+						where
+							name='{3}'
+					"""
+					frappe.db.sql(query.format(job_applicant.one_fm_agency or '', job_applicant.one_fm_nationality or '', job_applicant.department or '', doc.name))
+					frappe.db.commit()
