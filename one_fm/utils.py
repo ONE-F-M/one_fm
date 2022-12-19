5d3129e89b7f3cdeaa9b19495dd444234203c2bf
NoBoneZ <adekunleabraham09@gmail.com>
2022-12-19 22:54:35 +0100

    more details to roster post actions

--- one_fm/utils.py
+++ one_fm/utils.py
@@ -1944,64 +1944,43 @@ def create_roster_post_actions():
 
 
     # For each supervisor, create post actions to fill post type specifying the post types not filled
-    for index, res in enumerate(result):
-        if index > 0:
-            supervisor = res[0]
-            operations_roles = res[1].split(",")
+    for res in result:
+        supervisor = res[0]
+        operations_roles = res[1].split(",")
 
-            check_list = []
-            for val in list_of_dict_of_operations_roles_not_filled:
-                if val["operations_role"] in operations_roles:
-                    check_list.append(val)
-            
-            
-            print(len(check_list))
-            # print(check_list)
-            for ind, val in enumerate(check_list):
-                if ind < (len(check_list) - 1):
-                    next = check_list[ind + 1]
-                    if val["date"] == next["date"] and val["operations_role"] == next["operations_role"] and val["shift"] == next["shift"]:
-                        val["quantity"] = val["quantity"] + 1 if "quantity" in val.keys() else 1
-                        val["checked"] = True
-                        check_list.pop(ind + 1)
+        check_list = []
+        for val in list_of_dict_of_operations_roles_not_filled:
+            if val["operations_role"] in operations_roles:
+                check_list.append(val)
 
-                    # else:
-                    #     if not "checked" in val.keys():
-                    #         val["quantity"] = 1
-
-
-                elif ind == (len(check_list) - 1):
-                    previous = check_list[ind - 1]
-                    if val["date"] == previous["date"] and val["operations_role"] == next["operations_role"] and val["shift"] == next["shift"]:
-                        previous["quantity"] = previous["quantity"] + 1 if "quantity" in previous.keys() else 1
-                        previous.update({"checked": True})
-                        check_list.pop(ind)
-
-                    # else:
-                    #     if not "checked" in val.keys():
-                    #         val["quantity"] = 1
-
-            print(len(check_list))
-            print(check_list)
+        list_of_not_filled_roles = []
+        for val in check_list:
+            for role in list_of_not_filled_roles:
+                if val["date"] == role["date"] and val["operations_role"] == role["operations_role"] and val["shift"] == role["shift"]:
+                    role["quantity"] += 1
+                    break
+            else:
+                val.update({"quantity": 1})
+                list_of_not_filled_roles.append(val)    
             
 
-            roster_post_actions_doc = frappe.new_doc("Roster Post Actions")
-            roster_post_actions_doc.start_date = start_date
-            roster_post_actions_doc.end_date = end_date
-            roster_post_actions_doc.status = "Pending"
-            roster_post_actions_doc.action_type = "Fill Post Type"
-            roster_post_actions_doc.supervisor = supervisor
+        roster_post_actions_doc = frappe.new_doc("Roster Post Actions")
+        roster_post_actions_doc.start_date = start_date
+        roster_post_actions_doc.end_date = end_date
+        roster_post_actions_doc.status = "Pending"
+        roster_post_actions_doc.action_type = "Fill Post Type"
+        roster_post_actions_doc.supervisor = supervisor
 
-            for obj in check_list:
-                roster_post_actions_doc.append('operations_roles_not_filled', {
-                    'operations_role': obj.get("operations_role"),
-                    "operations_shift": obj.get("shift"),
-                    "date": obj.get("date"),
-                    "quantity": obj.get("quantity") if obj.get("quantity") else 1
-                })
+        for obj in list_of_not_filled_roles:
+            roster_post_actions_doc.append('operations_roles_not_filled', {
+                'operations_role': obj.get("operations_role"),
+                "operations_shift": obj.get("shift"),
+                "date": obj.get("date"),
+                "quantity": obj.get("quantity") if obj.get("quantity") else 1
+            })
 
-            roster_post_actions_doc.save()
-            frappe.db.commit()
+        roster_post_actions_doc.save()
+        frappe.db.commit()
 
 def send_roster_report():
     # Enqueue roster report generation to background
