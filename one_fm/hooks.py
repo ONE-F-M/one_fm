6c3f19c61716f6417895b33c7c65bd0ab887dfe6
mymi14s <mymi14s@hotmail.com>
2023-01-03 17:41:09 +0100

    fix: roster schedule highlight

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -598,15 +598,9 @@ scheduler_events = {
 		"15 0 * * *": [
 			'one_fm.api.tasks.assign_am_shift'
 		],
-		# "45 1 * * *": [
-		# 	'one_fm.api.tasks.validate_am_shift_assignment'
-		# ],
 		"15 12 * * *": [
 			'one_fm.api.tasks.assign_pm_shift'
 		],
-		# "45 13 * * *": [
-		# 	'one_fm.api.tasks.validate_pm_shift_assignment'
-		# ],
 	}
 }
 
