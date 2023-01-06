ca9c53caf1f5b456858a102a1aaecc2495d83fd0
Ebuka <ebukaakeru@gmail.com>
2023-01-06 04:34:02 +0100

    add leave type to roster page

--- one_fm/one_fm/page/roster/employee_map.py
+++ one_fm/one_fm/page/roster/employee_map.py
@@ -161,7 +161,7 @@ class CreateMap():
             self.schedule_query  = f"SELECT  es.employee, es.employee_name, es.date, es.operations_role, es.post_abbrv, \
                 es.shift, es.roster_type, es.employee_availability, es.day_off_ot from `tabEmployee Schedule`es  where \
                     es.employee in {self.employees} and {self.str_filter} order by es.employee "
-            self.attendance_query = f"SELECT at.status,at.leave_application, at.attendance_date,at.employee,at.employee_name from `tabAttendance`at  where at.employee in {self.employees}  and at.attendance_date between '{self.start}' and '{add_to_date(cstr(getdate()), days=-1)}' order by at.employee """
+            self.attendance_query = f"SELECT at.status,at.leave_type,at.leave_application, at.attendance_date,at.employee,at.employee_name from `tabAttendance`at  where at.employee in {self.employees}  and at.attendance_date between '{self.start}' and '{add_to_date(cstr(getdate()), days=-1)}' order by at.employee """
             self.employee_query = f"SELECT name,  employee_id, employee_name,day_off_category,number_of_days_off from `tabEmployee` where name in {self.employees} order by employee_name"
 
 
@@ -258,6 +258,7 @@ class CreateMap():
                     'employee': one.employee,
                     'employee_name': one.employee_name,
                     'leave_application': one.leave_application,
+                    'leave_type':one.leave_type,
                     'date': one.attendance_date,
                     'attendance': one.status,
                     'day_off_category': self.employee_period_details[row[1]].get('day_off_category'),
