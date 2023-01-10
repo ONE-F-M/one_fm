4d101fc9e64baeebeb9ee52f6560bc57b4d67c39
mymi14s <mymi14s@hotmail.com>
2023-01-10 10:20:06 +0100

    fix: missing shift in Employee Checkin

--- one_fm/overrides/employee_checkin.py
+++ one_fm/overrides/employee_checkin.py
@@ -1,9 +1,24 @@
 import frappe
 from hrms.hr.doctype.employee_checkin.employee_checkin import EmployeeCheckin
+from one_fm.api.v1.roster import get_current_shift
 
 class EmployeeCheckinOverride(EmployeeCheckin):
-#overrode employee checkin method
+	#overrode employee checkin method
 	@frappe.whitelist()
 	def fetch_shift(self):
 		pass
+
+
+	def after_insert(self):
+		print(self.as_dict())
+		try:
+			# update shift if not exists
+			if not self.shift_assignment or not self.operations_shift:
+				curr_shift = get_current_shift(self.employee)
+				print(curr_shift, '\n\n')
+				self.db_set('shift_assignment', curr_shift.name)
+				self.db_set('operations_shift', curr_shift.shift)
+				self.db_set('shift_type', curr_shift.shift_type)
+		except Exception as e:
+			print(e)
 
