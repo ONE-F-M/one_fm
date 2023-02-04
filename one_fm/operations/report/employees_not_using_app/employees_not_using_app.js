842a76c0c3d42ec1781e55e2e817ed4a230e49be
mymi14s <mymi14s@hotmail.com>
2023-02-04 09:49:47 +0100

    clean up

--- one_fm/operations/report/employees_not_using_app/employees_not_using_app.js
+++ one_fm/operations/report/employees_not_using_app/employees_not_using_app.js
@@ -16,10 +16,7 @@ frappe.query_reports["Employees Not Using App"] = {
             "label": __("Supervisor"),
             "fieldtype": "Link",
             "reqd": 0,
-            "options": "Employee",
-			// get_data: function(txt) {
-			// 	return frappe.db.get_link_options('Employee', 'HR-EMP-00022');
-			// }
+            "options": "Employee"
         },
 		{
             "fieldname": "company",
