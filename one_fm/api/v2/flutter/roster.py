bad28401931bf51458b2bf269fa4276a923bf967
mymi14s <mymi14s@hotmail.com>
2023-02-13 07:31:37 +0100

    added traceback to v2 roster

--- one_fm/api/v2/flutter/roster.py
+++ one_fm/api/v2/flutter/roster.py
@@ -410,7 +410,7 @@ def schedule_staff(employees, shift, operations_role, otRoster, start_date, proj
             update_roster(key="roster_view")
             response("success", 200, {'employees':employees_list, 'date_range':date_range, 'message':'Sucessfully rostered employees'})
     except Exception as e:
-        response("error", 500, None, str(e))
+        response("error", 500, None, str(frappe.get_traceback()))
 
 def update_roster(key):
     frappe.publish_realtime(key, "Success")
