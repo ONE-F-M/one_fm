5b3fa002b72b30b6ef10fe1650a67516a6663f4c
Ebuka <ebukaakeru@gmail.com>
2023-02-14 20:39:01 +0100

    Leave application patch

--- one_fm/patches/v14_0/fix_sick_leaves.py
+++ one_fm/patches/v14_0/fix_sick_leaves.py
@@ -4,7 +4,7 @@ def execute():
 
     all_leave_attachments = frappe.get_all("File",{'attached_to_doctype':"Leave Application"},['attached_to_name'])
     all_leave_a = [each.attached_to_name for each in all_leave_attachments]
-    all_leaves = frappe.get_all("Leave Application",{'name':['Not In', all_leave_a],'leave_type':"Sick Leave","docstatus":0})
+    all_leaves = frappe.get_all("Leave Application",{'leave_type':"Sick Leave","docstatus":0})
     for each in all_leaves:
         try:
             leave_doc = frappe.get_doc("Leave Application",each.name)
