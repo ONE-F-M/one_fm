c038751bbdbfecd05c81d3c00e5cacbf1c051f20
NoBoneZ <adekunleabraham09@gmail.com>
2023-02-19 13:26:47 +0100

    reverted commented out fixtures

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -668,41 +668,41 @@ scheduler_events = {
 # ]
 
 fixtures = [
-	# {
-	# 	"dt": "Custom Field",
-	# 	# 'filters': [['dt', 'in', ['Shift Request', 'Shift Permission', 'Employee', 'Project', 'Location', 'Employee Checkin', 'Shift Assignment', 'Shift Type', 'Operations Site']]]
-	# },
-	# {
-	# 	"dt": "Property Setter"
-	# },
-	# {
-	# 	"dt": "Workflow State"
-	# },
-	# {
-	# 	"dt": "Workflow Action Master"
-	# },
-	# {
-	# 	"dt": "Workflow"
-	# },
-	# {
-	# 	"dt": "Client Script",
-	# 	'filters': [['dt', 'in', ['Stock Entry', 'Warehouse',
-	# 	'Payment Entry', 'Payment Request', 'Purchase Receipt', 'Purchase Order']]]
-	# },
-	# {
-	# 	"dt": "Print Format"
-	# },
-	# {
-	# 	"dt": "Role",
-	# 	"filters": [["name", "in",["Operations Manager", "Shift Supervisor", "Site Supervisor", "Projects Manager"]]]
-	# },
-	# {
-	# 	"dt": "Custom DocPerm",
-	# 	"filters": [["role", "in",["Operations Manager", "Shift Supervisor", "Site Supervisor", "Projects Manager"]]]
-	# },
-	# {
-	# 	"dt": "Email Template"
-	# }
+	{
+		"dt": "Custom Field",
+		# 'filters': [['dt', 'in', ['Shift Request', 'Shift Permission', 'Employee', 'Project', 'Location', 'Employee Checkin', 'Shift Assignment', 'Shift Type', 'Operations Site']]]
+	},
+	{
+		"dt": "Property Setter"
+	},
+	{
+		"dt": "Workflow State"
+	},
+	{
+		"dt": "Workflow Action Master"
+	},
+	{
+		"dt": "Workflow"
+	},
+	{
+		"dt": "Client Script",
+		'filters': [['dt', 'in', ['Stock Entry', 'Warehouse',
+		'Payment Entry', 'Payment Request', 'Purchase Receipt', 'Purchase Order']]]
+	},
+	{
+		"dt": "Print Format"
+	},
+	{
+		"dt": "Role",
+		"filters": [["name", "in",["Operations Manager", "Shift Supervisor", "Site Supervisor", "Projects Manager"]]]
+	},
+	{
+		"dt": "Custom DocPerm",
+		"filters": [["role", "in",["Operations Manager", "Shift Supervisor", "Site Supervisor", "Projects Manager"]]]
+	},
+	{
+		"dt": "Email Template"
+	},
 	{
 		"doctype": "DocType Layout",
 		"filters": {
