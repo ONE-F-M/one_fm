3ddfae94cea16c6cadbb3c2acaa2a8621c18d168
Ebuka <ebukaakeru@gmail.com>
2023-02-21 20:54:45 +0100

    Leave Application Submit FIx

--- one_fm/overrides/leave_application.py
+++ one_fm/overrides/leave_application.py
@@ -24,7 +24,7 @@ def close_leaves(all_leaves,user=None):
     frappe.db.commit()
     if user:
         message = "Please note that all open sick leaves have been approved"
-        create_notification_log('Leaves Closed!', 'Please', [user],leave_doc)
+        create_notification_log('Leaves Closed!', 'Please Note that all sick leaves have been closed ', [user],leave_doc)
         
 
 @frappe.whitelist()
