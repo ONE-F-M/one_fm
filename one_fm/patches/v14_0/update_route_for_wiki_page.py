743e5324a6b3d867c03ecadd1445d3611d9ac1c1
NoBoneZ <adekunleabraham09@gmail.com>
2023-02-22 11:46:15 +0100

    corrected patch logic

--- one_fm/patches/v14_0/update_route_for_wiki_page.py
+++ one_fm/patches/v14_0/update_route_for_wiki_page.py
@@ -6,7 +6,11 @@ import frappe
 def execute():
     list_of_pages = frappe.db.sql(f""" select name, route from `tabWiki Page`; """)
     for obj in list_of_pages:
-        route = "wiki/" + obj[1] + f"-{str(uuid4()).split('-')[0]}"
+        if not obj[1].startswith("wiki/"):
+        route = "wiki/" + obj[1]
+        check = frappe.db.sql(f""" select name from `tabWiki Page` where route = '{route}' """, as_dict=1)
+        if check:
+            route += f"-{str(uuid4()).split('-')[0]}"
         frappe.db.set_value("Wiki Page", obj[0], {
             "route": route
         })
