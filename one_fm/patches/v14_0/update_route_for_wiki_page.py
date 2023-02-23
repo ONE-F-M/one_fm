03c9160e669c1779617fb7c59e98d730102c9763
NoBoneZ <adekunleabraham09@gmail.com>
2023-02-23 08:40:54 +0100

    corrected indentation error

--- one_fm/patches/v14_0/update_route_for_wiki_page.py
+++ one_fm/patches/v14_0/update_route_for_wiki_page.py
@@ -7,10 +7,10 @@ def execute():
     list_of_pages = frappe.db.sql(f""" select name, route from `tabWiki Page`; """)
     for obj in list_of_pages:
         if not obj[1].startswith("wiki/"):
-        route = "wiki/" + obj[1]
-        check = frappe.db.sql(f""" select name from `tabWiki Page` where route = '{route}' """, as_dict=1)
-        if check:
-            route += f"-{str(uuid4()).split('-')[0]}"
-        frappe.db.set_value("Wiki Page", obj[0], {
-            "route": route.lower()
-        })
+            route = "wiki/" + obj[1]
+            check = frappe.db.sql(f""" select name from `tabWiki Page` where route = '{route}' """, as_dict=1)
+            if check:
+                route += f"-{str(uuid4()).split('-')[0]}"
+            frappe.db.set_value("Wiki Page", obj[0], {
+                "route": route.lower()
+            })
