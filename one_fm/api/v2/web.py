0592e25fff07c673b2c5c8f058f0ed58e8c1dbbf
mymi14s <mymi14s@hotmail.com>
2023-02-25 09:01:00 +0100

    log error on mobile web

--- one_fm/api/v2/web.py
+++ one_fm/api/v2/web.py
@@ -164,7 +164,7 @@ def check_in(log_type, skip_attendance, latitude, longitude):
 		checkin.log_type = log_type
 		checkin.device_id = cstr(latitude)+","+cstr(longitude)
 		checkin.skip_auto_attendance = cint(skip_attendance)
-		checkin.shift_assignment = get_current_shift(employee.name)
+		checkin.shift_assignment = get_current_shift(employee)
 		# checkin.time = now_datetime()
 		# checkin.actual_time = now_datetime()
 		checkin.save()
@@ -172,7 +172,7 @@ def check_in(log_type, skip_attendance, latitude, longitude):
 		return _('Check {log_type} successful! {docname}'.format(log_type=log_type.lower(), docname=checkin.name))
 	except:
 		frappe.log_error(frappe.get_traceback(), 'Mobile Web Checkin')
-		
+
 @frappe.whitelist()
 def forced_checkin(employee, log_type, time):
 	checkin = frappe.new_doc("Employee Checkin")
