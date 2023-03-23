82c0042d1d9500133533c8439c7df4ce43d5496f
SalShai <slwshkh@gmail.com>
2023-03-23 10:54:51 +0300

    Fix: Get Site ID

--- one_fm/one_fm/doctype/google_sheet_data_export/exporter.py
+++ one_fm/one_fm/doctype/google_sheet_data_export/exporter.py
@@ -27,7 +27,7 @@ import gspread
 from google.oauth2 import service_account
 
 #initialize Google Sheet Service
-SERVICE_ACCOUNT_FILE = cstr(frappe.utils.get_site_path()) + frappe.local.conf.google_sheet
+SERVICE_ACCOUNT_FILE = cstr(frappe.utils.get_site_id().split('@')[0]) + frappe.local.conf.google_sheet
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
