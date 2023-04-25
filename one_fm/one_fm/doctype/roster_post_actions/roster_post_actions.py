43b9d8feb72a620c7610e5485b6bba191507db88
Ebuka <ebukaakeru@gmail.com>
2023-04-25 14:18:48 +0100

    Update Roster Post Actions Permissions

--- one_fm/one_fm/doctype/roster_post_actions/roster_post_actions.py
+++ one_fm/one_fm/doctype/roster_post_actions/roster_post_actions.py
@@ -6,6 +6,7 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils import nowdate, add_to_date, cstr, cint, getdate, get_link_to_form
 from one_fm.processor import sendemail
+from frappe.permissions import get_doctype_roles
 
 
 class RosterPostActions(Document):
@@ -24,3 +25,38 @@ class RosterPostActions(Document):
 				Please review the Post Type for the specified date in the roster, take necessary actions and update the status.<br>
 				Link: {link}""".format(link=link))
 			sendemail([user_id], subject=subject, message=message, reference_doctype=self.doctype, reference_name=self.name)
+
+
+
+
+@frappe.whitelist()
+def get_permission_query_conditions(user):
+	"""
+		Method used to set the permission to get the list of docs (Example: list view query)
+		Called from the permission_query_conditions of hooks for the DocType Roster Employee Actions
+		args:
+			user: name of User object or current user
+		return conditions query
+	"""
+	if not user: user = frappe.session.user
+
+	if user == "Administrator":
+		return ""
+
+	# Fetch all the roles associated with the current user
+	user_roles = frappe.get_roles(user)
+
+	if "System Manager" in user_roles:
+		return ""
+
+	# Get roles allowed to Roster Employee Actions doctype
+	doctype_roles = get_doctype_roles('Roster Employee Actions')
+	# If doctype roles is in user roles, then user permitted
+	role_exist = [role in doctype_roles for role in user_roles]
+
+	if role_exist and len(role_exist) > 0 and True in role_exist:
+		employee = frappe.get_value("Employee", {"user_id": user}, ["name"])
+		if "Shift Supervisor" in user_roles or "Site Supervisor" in user_roles:
+			return '(`tabRoster Post Actions`.`supervisor`="{employee}" or `tabRoster Post Actions`.`site_supervisor`="{employee}")'.format(employee = employee)
+
+	return ""
