65afe0cff9b091ff77458298029ad294572b84cb
mymi14s <mymi14s@hotmail.com>
2023-05-01 21:15:50 +0100

    added: leave balance by type

---
+++ one_fm/one_fm/report/leave_balance_by_type/leave_balance_by_type.js
@@ -0,0 +1,79 @@
+// Copyright (c) 2023, omar jaber and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Leave Balance by Type"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"reqd": 1,
+			"default": frappe.defaults.get_default("year_start_date")
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"reqd": 1,
+			"default": frappe.defaults.get_default("year_end_date")
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname": "department",
+			"label": __("Department"),
+			"fieldtype": "Link",
+			"options": "Department",
+		},
+		{
+			"fieldname": "employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+		},
+		{
+			"fieldname": "employee_status",
+			"label": __("Employee Status"),
+			"fieldtype": "Select",
+			"options": [
+				"",
+				{ "value": "Active", "label": __("Active") },
+				{ "value": "Inactive", "label": __("Inactive") },
+				{ "value": "Suspended", "label": __("Suspended") },
+				{ "value": "Left", "label": __("Left") },
+			],
+			"default": "Active",
+		},
+		{
+			"fieldname": "leave_type",
+			"label": __("Leave Type"),
+			"fieldtype": "Link",
+			"options": "Leave Type",
+		},
+	],
+
+	onload: () => {
+		frappe.call({
+			type: "GET",
+			method: "hrms.hr.utils.get_leave_period",
+			args: {
+				"from_date": frappe.defaults.get_default("year_start_date"),
+				"to_date": frappe.defaults.get_default("year_end_date"),
+				"company": frappe.defaults.get_user_default("Company")
+			},
+			freeze: true,
+			callback: (data) => {
+				frappe.query_report.set_filter_value("from_date", data.message[0].from_date);
+				frappe.query_report.set_filter_value("to_date", data.message[0].to_date);
+			}
+		});
+	}
+}
+
