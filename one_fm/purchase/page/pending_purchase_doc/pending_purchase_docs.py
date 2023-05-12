7252bb4bf13d83454bea8bce044792460af04972
Ebuka <ebukaakeru@gmail.com>
2023-05-12 01:50:12 +0100

    update pending docs

--- one_fm/purchase/page/pending_purchase_doc/pending_purchase_docs.py
+++ one_fm/purchase/page/pending_purchase_doc/pending_purchase_docs.py
@@ -4,5 +4,6 @@ def get_data():
         Fetch the pending workflow actions for the RFM,RFP and PO for that user
     """
     current_user = frappe.session.user
-    actions = frappe.get_all("Workflow Action")
-    
+    rfm_docs = frappe.get_all("Request for Material",{'docstatus':1,'request_for_material_approver':current_user,'status':'Draft'},['name','status'])
+    rfq_docs = frappe.get_all('ToDo',{'status':'Open','allocated_to':current_user,'reference_type':'Request for Purchase'},['reference_name'])
+    po_docs = frappe.get_all("Workflow Action")
