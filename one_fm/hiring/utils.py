4c625219f76c272181384f108fca44dcf6dba665
mymi14s <mymi14s@hotmail.com>
2023-05-15 08:19:56 +0100

    fix Leave allocation issue

--- one_fm/hiring/utils.py
+++ one_fm/hiring/utils.py
@@ -350,27 +350,6 @@ def create_leave_policy_assignment(doc):
         assignment.save()
         assignment.submit()
 
-@frappe.whitelist()
-def grant_leave_alloc_for_employee(doc):
-    if not doc.leaves_allocated:
-        from hrms.hr.doctype.leave_policy_assignment.leave_policy_assignment import get_leave_type_details
-        leave_allocations = {}
-        leave_type_details = get_leave_type_details()
-
-        leave_policy = frappe.get_doc("Leave Policy", doc.leave_policy)
-        date_of_joining = frappe.db.get_value("Employee", doc.employee, "date_of_joining")
-
-        for leave_policy_detail in leave_policy.leave_policy_details:
-            if not leave_type_details.get(leave_policy_detail.leave_type).is_lwp:
-                leave_allocation, new_leaves_allocated = doc.create_leave_allocation(
-                    leave_policy_detail.leave_type, leave_policy_detail.annual_allocation,
-                    leave_type_details, date_of_joining
-                )
-
-                leave_allocations[leave_policy_detail.leave_type] = {"name": leave_allocation, "leaves": new_leaves_allocated}
-
-        doc.db_set("leaves_allocated", 1)
-        return leave_allocations
 
 def create_wp_for_transferable_employee(doc):
     """
