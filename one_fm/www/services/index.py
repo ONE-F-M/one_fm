e1b061f454281eff5bddef9d9628a741d2dc1ada
SalShai <slwshkh@gmail.com>
2023-05-22 17:21:05 +0300

    Adjust context display

--- one_fm/www/services/index.py
+++ one_fm/www/services/index.py
@@ -1,9 +1,20 @@
 import frappe
+import re
 
 def get_context(context):
-    
-    services = {}
+    services = []
     category = frappe.get_value("HD Article Category", {'category_name':'Company Service'},["name"])
-    services = frappe.get_list("HD Article", {'category': category},['name','title', 'content'])
-    
+    service = frappe.get_list("HD Article", {'category': category},['name','title', 'content'])
+    for s in service:
+        services.append({
+            'title':s.title,
+            'content': ((remove_html_tags(s.content)[0:250] + "...") if s.content else ""),
+        })
+
     context.services = services
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    filtered = re.sub(clean, ' ', text)
+    return re.sub(' +', ' ', filtered)
