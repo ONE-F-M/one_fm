abffb023d00182d462555260ebef2763458bb728
mymi14s <mymi14s@hotmail.com>
2023-06-05 08:01:16 +0100

    fix sales invoice PF

--- one_fm/public/js/doctype_js/sales_invoice.js
+++ one_fm/public/js/doctype_js/sales_invoice.js
@@ -1,34 +1,34 @@
 $.extend(frappe.meta, {
-    get_print_formats: function(doctype) {
-        var print_format_list = ["Standard"];
-        var default_print_format = locals.DocType[doctype].default_print_format;
-        let enable_raw_printing = frappe.model.get_doc(":Print Settings", "Print Settings").enable_raw_printing;
-        var print_formats = frappe.get_list("Print Format", {doc_type: doctype})
-            .sort(function(a, b) { return (a > b) ? 1 : -1; });
-        $.each(print_formats, function(i, d) {
-            if (
-                !in_list(print_format_list, d.name)
-                && d.print_format_type !== 'JS'
-                && (cint(enable_raw_printing) || !d.raw_printing)
-            ) {
-                print_format_list.push(d.name);
-            }
-        });
+    // get_print_formats: function(doctype) {
+    //     var print_format_list = ["Standard"];
+    //     var default_print_format = locals.DocType[doctype].default_print_format;
+    //     let enable_raw_printing = frappe.model.get_doc(":Print Settings", "Print Settings").enable_raw_printing;
+    //     var print_formats = frappe.get_list("Print Format", {doc_type: doctype})
+    //         .sort(function(a, b) { return (a > b) ? 1 : -1; });
+    //     $.each(print_formats, function(i, d) {
+    //         if (
+    //             !in_list(print_format_list, d.name)
+    //             && d.print_format_type !== 'JS'
+    //             && (cint(enable_raw_printing) || !d.raw_printing)
+    //         ) {
+    //             print_format_list.push(d.name);
+    //         }
+    //     });
 
-        if(default_print_format && default_print_format != "Standard") {
-            var index = print_format_list.indexOf(default_print_format);
-            print_format_list.splice(index, 1).sort();
-            print_format_list.unshift(default_print_format);
-        }
+    //     if(default_print_format && default_print_format != "Standard") {
+    //         var index = print_format_list.indexOf(default_print_format);
+    //         print_format_list.splice(index, 1).sort();
+    //         print_format_list.unshift(default_print_format);
+    //     }
 
-        if(cur_frm.doc.format){ //newly added if condition
-            var index = print_format_list.indexOf(cur_frm.doc.format);
-            print_format_list.splice(index, 1).sort();
-            print_format_list.unshift(cur_frm.doc.format);
-        }
+    //     if(cur_frm.doc.format){ //newly added if condition
+    //         var index = print_format_list.indexOf(cur_frm.doc.format);
+    //         print_format_list.splice(index, 1).sort();
+    //         print_format_list.unshift(cur_frm.doc.format);
+    //     }
 
-        return print_format_list;
-    },
+    //     return print_format_list;
+    // },
 });
 frappe.ui.form.on('Sales Invoice', {
     validate: function(frm){
