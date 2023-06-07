fd76c8e8e524fee18a58d2000806f32435fe1bdc
mymi14s <mymi14s@hotmail.com>
2023-06-07 18:09:33 +0100

    added patches

---
+++ one_fm/patches/v14_0/set_shift_permission_roster_type.py
@@ -0,0 +1,7 @@
+import frappe
+
+
+def execute():
+    shift_permissions = frappe.get_all("Shift Permission", filters={'date':[">=", "2023-06-01"]}, fields="*")
+    for i in shift_permissions:
+        frappe.db.set_value("Shift Permission", i.name, 'roster_type', frappe.db.get_value("Shift Assignment", i.shift_assignment, 'roster_type'))
