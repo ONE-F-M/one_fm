77b5d42af9bef4d5075927ce932f017641471799
NoBoneZ <adekunleabraham09@gmail.com>
2023-06-19 10:37:59 +0100

    added try and except

--- one_fm/patches/v14_0/update_non_kuwaiti_residents_in_employee.py
+++ one_fm/patches/v14_0/update_non_kuwaiti_residents_in_employee.py
@@ -2,7 +2,10 @@ import frappe
 
 
 def execute():
-    db = frappe.databse.get_db()
-    if 'non_kuwaiti_residents' in db.get_table_columns("Employee"):
-        frappe.db.sql(
-            """UPDATE `tabEmployee` SET non_kuwaiti_residents = 1 WHERE employment_type = 'Service Provider'""")
+    try:
+        db = frappe.database.get_db()
+        if 'non_kuwaiti_residents' in db.get_table_columns("Employee"):
+            frappe.db.sql(
+                """UPDATE `tabEmployee` SET non_kuwaiti_residents = 1 WHERE employment_type = 'Service Provider'""")
+    except:
+        pass
