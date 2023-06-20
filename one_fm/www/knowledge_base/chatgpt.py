04c18d5f09a6623550431cb0f92596d8210ef619
SalShai <slwshkh@gmail.com>
2023-06-20 08:18:16 +0300

    Setup ChatGPT API

--- one_fm/www/knowledge_base/chatgpt.py
+++ one_fm/www/knowledge_base/chatgpt.py
@@ -3,22 +3,30 @@ import os
 import pandas as pd
 import time
 import requests
+import frappe
 
 API_KEY = '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'
 API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
 
+@frappe.whitelist()
 def get_completion(prompt):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {API_KEY}'
-    }
+    try:
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {API_KEY}'
+        }
 
-    data = {
-        'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
-                     {'role': 'user', 'content': prompt}],
-    }
+        data = {
+            'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                        {'role': 'user', 'content': prompt}],
+        }
 
-    response = requests.post(API_ENDPOINT, headers=headers, json=data)
-    response_json = response.json()
+        response = requests.post(API_ENDPOINT, headers=headers, json=data)
+        response_json = response.json()
 
-    return response_json['choices'][0]['message']['content']
+        if response_json:
+            return response_json['choices'][0]['message']['content']
+        else:
+            return None
+    except:
+        return None
