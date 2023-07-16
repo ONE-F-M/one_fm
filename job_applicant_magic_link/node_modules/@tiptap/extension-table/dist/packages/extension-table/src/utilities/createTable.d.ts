ea9aa04742da74e41841414dd96dba99ad737c3c
mymi14s <mymi14s@hotmail.com>
2023-07-16 07:17:53 +0100

    fix: shift approval

--- job_applicant_magic_link/node_modules/@tiptap/extension-table/dist/packages/extension-table/src/utilities/createTable.d.ts
+++
@@ -1,2 +0,0 @@
-import { Fragment, Node as ProsemirrorNode, Schema } from '@tiptap/pm/model';
-export declare function createTable(schema: Schema, rowsCount: number, colsCount: number, withHeaderRow: boolean, cellContent?: Fragment | ProsemirrorNode | Array<ProsemirrorNode>): ProsemirrorNode;
