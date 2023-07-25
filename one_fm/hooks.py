63328863db426893a948216f3f43d40adbdd217d
mymi14s <mymi14s@hotmail.com>
2023-07-25 09:21:49 +0100

    fix: Job Applicant auto name

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -484,7 +484,8 @@ override_doctype_class = {
 	"Employee Checkin": "one_fm.overrides.employee_checkin.EmployeeCheckinOverride",
 	"Timesheet": "one_fm.overrides.timesheet.TimesheetOveride",
     "Job Offer": "one_fm.overrides.job_offer.JobOfferOverride",
-    "Notification Log": "one_fm.overrides.notification_log.NotificationLogOverride"
+    "Notification Log": "one_fm.overrides.notification_log.NotificationLogOverride",
+    "Job Applicant": "one_fm.overrides.job_applicant.JobApplicantOverride",
 	# "User": "one_fm.overrides.user.UserOverrideLMS",
 }
 
