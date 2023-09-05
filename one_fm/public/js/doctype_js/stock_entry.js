e5a9d3112273a923e243ed94e84a46003d93059c
Jamsheer PP <kuttypjamsheer@gmail.com>
2023-09-05 13:08:32 +0530

    feat: stock entry type - Material Issue - Stock Issuer

--- one_fm/public/js/doctype_js/stock_entry.js
+++ one_fm/public/js/doctype_js/stock_entry.js
@@ -1,9 +1,47 @@
 frappe.ui.form.on('Stock Entry', {
-    refresh(frm) {
-        
+  refresh(frm) {
+    set_stock_entry_type_for_issuer(frm);
+  },
+  onload(frm) {
+    if(frappe.user.has_role('Stock Issuer')){
+      frm.events.remove_stock_entry_type_create_btn(frm);
     }
+  },
+  remove_stock_entry_type_create_btn(frm) {
+		frm.fields_dict.stock_entry_type.$wrapper.on("keyup", () => {
+			setTimeout(()=>{
+				var list_tags = frm.fields_dict.stock_entry_type.$wrapper.find('ul li')
+				for (let list_tag of list_tags) {
+					if (list_tag.innerText === __(" Create a new Stock Entry Type")) {
+						list_tag.remove();
+					}
+				}
+			}, 1000);
+		});
+	},
 })
 
+var set_stock_entry_type_for_issuer = function(frm) {
+  if(frappe.user.has_role('Stock Issuer')){
+    set_stock_entry_type_filter(frm, ["Material Issue"]);
+    frappe.db.get_value("Stock Entry Type", {"purpose": "Material Issue"}, "name").then(res=>{
+      if(res.message && res.message.name){
+        frm.set_value('stock_entry_type', res.message.name);
+      }
+    });
+  }
+}
+
+var set_stock_entry_type_filter = function(frm, purpose) {
+  // purpose should be a list of value
+  frm.set_query("stock_entry_type", function() {
+      return {
+          filters:{
+              purpose: ['in', purpose]
+          }
+      };
+  });
+}
 
 frappe.ui.form.on('Stock Entry Detail', {
     t_warehouse(frm, cdt, cdn) {
