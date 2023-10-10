32055121dfe5542ce8fef758a09706ad95f571fe
SalShai <slwshkh@gmail.com>
2023-10-10 15:37:04 +0300

    Fatch value

--- one_fm/www/careers/opening/index.py
+++ one_fm/www/careers/opening/index.py
@@ -4,15 +4,22 @@ from ..utils import remove_html_tags
 def get_context(context):
     no_cache = 1
     job_id = frappe.form_dict.job_id
+    lang = frappe.form_dict.lang
+    print(lang)
 
     opening = {}
 
     job_opening = frappe.get_doc("Job Opening", {'name': job_id})
 
     if job_opening:
-        opening.update({'name': job_opening.name})
+        if lang == 'en':
+            opening.update({'name': job_opening.name})
+            opening.update({'description': job_opening.description if job_opening.description else ""})
+        else:
+            opening.update({'name': job_opening.job_title_in_arabic})
+            opening.update({'description': job_opening.description_in_arabic if job_opening.description_in_arabic else ""})
         opening.update({'job_title': job_opening.job_title})
         opening.update({'designation': job_opening.designation})
-        opening.update({'description': job_opening.description if job_opening.description else ""})
+
 
     context.opening = opening
