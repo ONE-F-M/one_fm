49c44bb13e1ab61364f9b7d18af97caa64144570
SalShai <slwshkh@gmail.com>
2023-10-18 12:35:00 +0300

    Fix: Patch

--- one_fm/patches/v14_0/update_operation_shift.py
+++ one_fm/patches/v14_0/update_operation_shift.py
@@ -1,10 +1,12 @@
 import frappe
 
 def execute():
-    att = frappe.db.sql("""SELECT * FROM `tabAttendance` WHERE operations_shift='None' AND status IN ('Present','Absent', 'On Leave')""", as_dict=1)
-    for a in att:
-        _a = frappe.get_doc("Attendance", a.name)
-        if a.shift_assignment:
-            sa = frappe.get_value("Shift Assignment", a.shift_assignment, ['shift'])
-            _a.db_set('operations_shift', sa)
-    frappe.db.commit()
+    att = frappe.db.sql("""UPDATE
+                            `tabAttendance` a,
+                            `tabShift Assignment` sa
+                        SET
+                            a.operations_shift = sa.shift
+                        WHERE
+                            a.shift_assignment = sa.name
+                            AND a.operations_shift='None'
+                        """)
