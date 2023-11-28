8406bb46e1fadd5c094fa8829af6b10ae9b3b710
Jamsheer PP <kuttypjamsheer@gmail.com>
2023-11-28 14:42:05 +0530

    feat: init Onboard Subcontract Employee

---
+++ one_fm/subcontract/doctype/onboard_subcontract_employee/onboard_subcontract_employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, omar jaber and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Onboard Subcontract Employee', {
+	// refresh: function(frm) {
+
+	// }
+});
