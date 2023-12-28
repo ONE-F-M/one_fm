c15c9289d7b3c5c0f0ff5301f26a9e3742af8d73
NoBoneZ <adekunleabraham09@gmail.com>
2023-12-28 12:01:04 +0100

    added logic to exclude child goals with diffrent kra

--- one_fm/public/js/doctype_js/goal.js
+++ one_fm/public/js/doctype_js/goal.js
@@ -8,5 +8,32 @@ frappe.ui.form.on("Goal", {
 				}
 			}
 		});
-	}
+	},
+
+	kra: function(frm) {
+		if (!frm.doc.appraisal_cycle) {
+			frm.set_value("kra", "");
+
+			frappe.msgprint({
+				message: __("Please select the Appraisal Cycle first."),
+				title: __("Mandatory")
+			});
+
+			return;
+		}
+
+		if (frm.doc.__islocal || !frm.doc.is_group) return;
+
+		let msg = __("Modifying the KRA in the parent goal will specifically impact those child goals that share the same KRA; any other child goals with different KRAs will remain unaffected.");
+		msg += "<br> <br>";
+		msg += __("Do you still want to proceed?");
+
+		frappe.confirm(
+			msg,
+			() => {},
+			() => {
+				frappe.db.get_value("Goal", frm.doc.name, "kra", (r) => frm.set_value("kra", r.kra));
+			}
+		);
+	},
 })
