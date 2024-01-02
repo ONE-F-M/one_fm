17aff397fe7e0a007639ebf3586ff42eb237b765
SalShai <slwshkh@gmail.com>
2024-01-02 13:01:01 +0300

    Minor Fix

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -676,7 +676,7 @@ scheduler_events = {
 		"15 3 * * *": [ # create shift assignment
 			'one_fm.overrides.employee_checkin.auto_generate_checkin'
 		],
-		"25 0 * * *"": [ # mark day attendance 12:25 AM
+		"25 0 * * *": [ # mark day attendance 12:25 AM
 			'one_fm.overrides.attendance.mark_day_attendance'
 		],
 		"45 12 * * *": [ # mark night attendance for previous day at 12:45 pm today
