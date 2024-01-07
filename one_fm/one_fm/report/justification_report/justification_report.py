b77c5a1e02d4f0c69416b5ccfccfcc04fdda8939
NoBoneZ <adekunleabraham09@gmail.com>
2024-01-07 18:03:39 +0100

    added basic and overtime total

--- one_fm/one_fm/report/justification_report/justification_report.py
+++ one_fm/one_fm/report/justification_report/justification_report.py
@@ -25,13 +25,19 @@ def update_data(cur_date, column_date_field, datapack):
     # Set total attendance check in the day, row 1
     set_day_total_attendance_check(cur_date, column_date_field, datapack)
 
-    # Set total pending approval attendance check in the day, row 2
+    # Set total basic roster-type attendance check in the day, row 2
+    set_day_total_basic_attendance_check(cur_date, column_date_field, datapack)
+    
+    # Set total over-time roster-type attendance check in the day, row 3
+    set_day_total_overtime_attendance_check(cur_date, column_date_field, datapack)
+
+    # Set total pending approval attendance check in the day, row 4
     set_day_total_pending_approval_acheck(cur_date, column_date_field, datapack)
 
-    # Set total approved attendance check in the day, row 3
+    # Set total approved attendance check in the day, row 5
     set_day_total_approved_attendance_check(cur_date, column_date_field, datapack)
 
-    # Set attendance status count for the day, starts from row 4
+    # Set attendance status count for the day, starts from row 6
     datapack, row_index = set_attendance_status_day_count(cur_date, column_date_field, datapack)
 
     # Set Justification details of attendance check for the day
@@ -44,32 +50,57 @@ def set_day_total_attendance_check(cur_date, column_date_field, datapack):
     total_attendance_check = frappe.db.count('Attendance Check', {'date':cur_date, 'docstatus': ['<', 2]})
     if len(datapack) == 0:
         # Add new row Total and  column value to the report
-        datapack.append({'justification_value': 'Total', column_date_field: total_attendance_check})
+        datapack.append({'justification_value': 'Cumulative Total', column_date_field: total_attendance_check})
     else:
         # Add cloumn value to the row in the report
         datapack[0].update({column_date_field: total_attendance_check})
 
     return datapack
 
+def set_day_total_basic_attendance_check(cur_date, column_date_field, datapack):
+    # Get total attendance check in the day for basic roster type
+    
+    total_basic_attendance_check = frappe.db.count('Attendance Check', {'date':cur_date, 'docstatus': ['<', 2], "roster_type": "Basic"})
+    if len(datapack) == 1:
+        # Add new row Total for Basic Roster types  and column value to the report
+        datapack.append({'justification_value': 'Total (Basic)', column_date_field: total_basic_attendance_check})
+    else:
+        # Add column value to the row in the report
+        datapack[1].update({column_date_field: total_basic_attendance_check})
+
+    return datapack
+
+def set_day_total_overtime_attendance_check(cur_date, column_date_field, datapack):
+    # Get total attendance check in the day for overtime roster-type
+    total_attendance_check = frappe.db.count('Attendance Check', {'date':cur_date, 'docstatus': ['<', 2],  "roster_type": "Over-Time"})
+    if len(datapack) == 2:
+        # Add new row Total for Over-time Roster types and column value to the report
+        datapack.append({'justification_value': 'Total (Overtime)', column_date_field: total_attendance_check})
+    else:
+        # Add column value to the row in the report
+        datapack[2].update({column_date_field: total_attendance_check})
+
+    return datapack
+
 def set_day_total_pending_approval_acheck(cur_date, column_date_field, datapack):
     pending_apporval = frappe.db.count('Attendance Check', {'date':cur_date, 'docstatus': ['<', 2], 'workflow_state': 'Pending Approval'})
-    if len(datapack) == 1:
+    if len(datapack) == 3:
         # Add new row Pending Approval and column value to the report
         datapack.append({'justification_value': 'Pending Approval', column_date_field: pending_apporval})
     else:
         # Add cloumn value to the row in the report
-        datapack[1].update({column_date_field: pending_apporval})
+        datapack[3].update({column_date_field: pending_apporval})
 
     return datapack
 
 def set_day_total_approved_attendance_check(cur_date, column_date_field, datapack):
     total_approved = frappe.db.count('Attendance Check', {'date':cur_date, 'docstatus': 1, 'workflow_state': 'Approved'})
-    if len(datapack) == 2:
+    if len(datapack) == 4:
         # Add new row Total Approved and column value to the report
         datapack.append({'justification_value': 'Total Approved', column_date_field: total_approved})
     else:
         # Add cloumn value to the row in the report
-        datapack[2].update({column_date_field: total_approved})
+        datapack[4].update({column_date_field: total_approved})
 
     return datapack
 
@@ -164,7 +195,7 @@ def get_columns(filters):
         )
     return columns
 
-def get_date_range(start_date, end_date,as_dict = False):
+def get_date_range(start_date, end_date, as_dict = False):
     dates = [] if not as_dict else {}
     for n in range(int((end_date - start_date).days)):
         if not as_dict:
