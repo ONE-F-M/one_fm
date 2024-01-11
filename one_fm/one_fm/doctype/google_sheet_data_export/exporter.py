2f90119460000ab0fc39cae481f7bf796be4ae1e
SalShai <slwshkh@gmail.com>
2024-01-11 14:48:40 +0300

    Minor revert

--- one_fm/one_fm/doctype/google_sheet_data_export/exporter.py
+++ one_fm/one_fm/doctype/google_sheet_data_export/exporter.py
@@ -561,7 +561,7 @@ def update_google_sheet_daily():
 
 	for e in list_of_export:
 		doc = frappe.get_doc("Google Sheet Data Export",e.name)
-		time.sleep(20)
+		time.sleep(120)
 
 		frappe.enqueue(export_data, 
 			doctype= doc.reference_doctype,
