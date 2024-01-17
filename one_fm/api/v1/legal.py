cb603cf4daf1320c71218bbc72b99c5a2afcf5ce
NoBoneZ <adekunleabraham09@gmail.com>
2024-01-17 20:27:57 +0100

    made updates

--- one_fm/api/v1/legal.py
+++ one_fm/api/v1/legal.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 import frappe, base64, json, grpc
-from frappe.utils import cint
+from frappe.utils import cint, now
 from one_fm.legal.doctype.penalty_issuance.penalty_issuance import get_filtered_employees
 from one_fm.legal.doctype.penalty.penalty import send_email_to_legal
 from frappe import _
@@ -44,16 +46,18 @@ def get_all_shifts():
     try:
         result = frappe.db.sql("""SELECT osh.name, osh.site, osh.project, ost.site_location 
 			FROM `tabOperations Shift` osh, `tabOperations Site` ost  
-			WHERE osh.site=ost.name
+			WHERE osh.site=ost.name 
+			AND osh.status="Active"	
 			ORDER BY name ASC """, as_dict=1)
 
         return response("Success", 200, result)
     except Exception as error:
+        print(error)
         return response("Internal Server Error", 500, None, error)
 
 
 @frappe.whitelist()
-def issue_penalty(penalty_category, issuing_time, issuing_location, penalty_location, penalty_occurence_time,company_damage, customer_property_damage, asset_damage, other_damages, shift=None, site=None, project=None, site_location=None, penalty_employees=[], penalty_details=[]):
+def issue_penalty(penalty_category, issuing_location, issuing_time, penalty_location, penalty_occurence_time,company_damage, customer_property_damage, asset_damage, other_damages, shift=None, site=None, project=None, site_location=None, penalty_employees=[], penalty_details=[]):
 	try:
 		employee, employee_name, designation = frappe.get_value("Employee", {"user_id": frappe.session.user}, ["name","employee_name", "designation"])
 
@@ -112,7 +116,7 @@ def issue_penalty(penalty_category, issuing_time, issuing_location, penalty_loca
 
 	except Exception as error:
 		frappe.log_error(error, 'Penalty Issuance Error')
-		return response("Internal Server Error", 500, None, error)@frappe.whitelist()
+		return response("Internal Server Error", 500, None, error)
 
 
 @frappe.whitelist()
@@ -341,7 +345,7 @@ def get_subordinates(employee: str = None):
 		if not isinstance(employee, str):
 			return response("Bad Request", 400, None, "employee must be of type str.")
 
-		the_subordinates = frappe.db.get_list("Employee", {"reports_to": employee}, ["employee_id", "employee_name", "name"])
+		the_subordinates = frappe.db.get_list("Employee", {"reports_to": employee}, ["employee_name", "name"])
 		return response("Success", 200, the_subordinates)
 	except Exception as e:
 		frappe.log_error(str(e), "Error while getting subordinates (API)")
@@ -351,4 +355,68 @@ def get_subordinates(employee: str = None):
 
         
     
+@frappe.whitelist()
+def new_issue_penalty(penalty_category, issuing_location, penalty_location, penalty_occurence_time,company_damage, customer_property_damage, 
+                      asset_damage, other_damages, shift=None, site=None, project=None, site_location=None, penalty_employees=[], 
+                      penalty_files=[], issuing_time=datetime.strptime(now(), '%Y-%m-%d %H:%M:%S.%f'), penalty_issuance_details=[]):
+	try:
+		employee, employee_name, designation = frappe.get_value("Employee", {"user_id": frappe.session.user}, ["name","employee_name", "designation"])
+
+		penalty_issuance = frappe.new_doc("Penalty Issuance")
+		penalty_issuance.penalty_category = penalty_category
+		
+		penalty_issuance.issuing_time = issuing_time
+		penalty_issuance.location = issuing_location
+		penalty_issuance.penalty_location = penalty_location
+		penalty_issuance.penalty_occurence_time = penalty_occurence_time
+
+		penalty_issuance.issuing_employee = employee
+		penalty_issuance.employee_name = employee_name
+		penalty_issuance.designation = designation
+		
+		penalty_issuance.customer_property_damage = cint(customer_property_damage)
+		penalty_issuance.company_damage = cint(company_damage)
+		penalty_issuance.other_damages = cint(other_damages)
+		penalty_issuance.asset_damage = cint(asset_damage)
+  
+		employees = json.loads(penalty_employees)
+		for employee in employees:
+			penalty_issuance.append('employees', {'employee_id':employee})
+
+		penalty_files = json.loads(penalty_files)
+	
+		path_name = frappe.utils.cstr(frappe.local.site)+"/public/files/Legal"
+		if not check_path(path_name):
+			create_path(path_name)
+
+		for detail in penalty_files:
+			if detail.get("attachments") and detail.get("attachment_name"):
+				filename = detail["attachment_name"]
+				attach = detail["attachments"]
+				content = base64.b64decode(attach)
+
+				OUTPUT_IMAGE_PATH = path_name+"/"+filename
+				fh = open(OUTPUT_IMAGE_PATH, "wb")
+				fh.write(content)
+				fh.close()
+				Attachment_file="/files/Legal/"+filename
+				detail.update({'attachments': Attachment_file})
     
+
+		for obj in penalty_issuance_details:
+			penalty_issuance.append('penalty_issuance_details', {"penalty_type": obj})
+
+		if penalty_category == "Performance":
+			penalty_issuance.shift = shift
+			penalty_issuance.site = site
+			penalty_issuance.project = project
+			penalty_issuance.site_location = site_location
+
+
+		penalty_issuance.insert()
+		penalty_issuance.submit()
+		return response("Success", 201, penalty_issuance)
+
+	except Exception as error:
+		frappe.log_error(error, 'Penalty Issuance Error')
+		return response("Internal Server Error", 500, None, error)
