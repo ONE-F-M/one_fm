609b0371627425f5e6dad104048a4c1bcc0c261a
Abraham Adekunle <adekunleabraham09@gmail.com>
2024-02-13 14:30:10 +0100

    Revert "FEAT: ADDED NOTE THAT DEPICTS THE APPROVAL FOR THE PURCHASE ORDER BASED ON THE CURRENT WORKFLOW"

--- one_fm/purchase/doctype/approvers/test_approvers.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, omar jaber and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestApprovers(FrappeTestCase):
-	pass
