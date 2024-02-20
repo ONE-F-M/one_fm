387ed7545ae48548a6a46ce0494e65c5746a65fe
SalShai <slwshkh@gmail.com>
2024-02-20 14:36:38 +0300

    Button to Create SHift Assignment

--- one_fm/one_fm/doctype/shift_assignment_checker/shift_assignment_checker.js
+++ one_fm/one_fm/doctype/shift_assignment_checker/shift_assignment_checker.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, omar jaber and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shift Assignment Checker", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Shift Assignment Checker", {
+	refresh(frm) {
+        if (frm.doc.attendance_manager == frappe.session.user && frm.doc.shift_assignment_created == 0){
+            frm.add_custom_button(__("Create Shift Assignment"), () =>
+				frappe.call({
+					method:
+						"one_fm.one_fm.doctype.shift_assignment_checker.shift_assignment_checker.create_shift_assignment",
+					args: {doc: frm.doc},
+					callback: function (r) {
+						if (!r.exc) {
+							frappe.msgprint("Shift Assignment Created")
+						}
+					},
+				})
+		    );
+        }
+	},
+});
--- one_fm/one_fm/doctype/shift_assignment_checker/shift_assignment_checker.py
+++ one_fm/one_fm/doctype/shift_assignment_checker/shift_assignment_checker.py
@@ -8,12 +8,21 @@ from frappe.utils import (
 )
 from datetime import datetime
 from one_fm.utils import ( production_domain, get_today_leaves)
-import re 
+import json
+from frappe.desk.form.assign_to import add as add_assignment
+
 
 class ShiftAssignmentChecker(Document):
-	pass
+	def on_submit(self):
+		add_assignment({
+				'doctype': self.doctype,
+				'name': self.name,
+				'assign_to': [self.attendance_manager],
+				'description': (f"{self.roster_type} Shift Assignment for the {self.employee_name} was not created for {self.start_date}. Kindly, take action. ")
+			})
 
-frappe.whitelist()
+
+@frappe.whitelist()
 def run_shift_assignment_checker():
 	date = cstr(getdate())
 	now = datetime.strptime("20-02-2024 06:00:00", "%d-%m-%Y %H:%M:%S")  #now_datetime()
@@ -77,31 +86,63 @@ def run_shift_assignment_checker():
 				roster_emp.append(r)
 
 	if len(roster)>0:
-		frappe.enqueue(create_shift_assignmen_checker, roster = roster, is_async=True, queue='long')
+		create_shift_assignmen_checker(roster = roster, date = date)
 
-def create_shift_assignmen_checker(roster):
-	try:
-		for schedule in roster:
-			shift_assignment_checker = frappe.new_doc("Shift Assignment Checker")
-			shift_assignment_checker.start_date = date
-			shift_assignment_checker.employee = schedule.employee
-			shift_assignment_checker.employee_name = schedule.employee_name
-			shift_assignment_checker.department = schedule.department
-			shift_assignment_checker.operations_role = schedule.operations_role
-			shift_assignment_checker.shift = schedule.shift if schedule.get('doctype') !="Shift Request" else schedule.get('operations_shift')
-			shift_assignment_checker.site = schedule.site
-			shift_assignment_checker.project = schedule.project
-			shift_assignment_checker.shift_type = schedule.shift_type
-			shift_assignment_checker.operations_role = schedule.operations_role
-			shift_assignment_checker.post_abbrv = schedule.post_abbrv
-			shift_assignment_checker.roster_type = schedule.roster_type
-			shift_assignment_checker.site_location = schedule.checkin_location
-			if schedule.doctype == 'Shift Request':
-				shift_assignment_checker.shift_request = schedule.name
-				if schedule.check_in_site and schedule.check_out_site:
-					shift_assignment_checker.check_in_site = shift_assignment_checker.check_in_site
-					shift_assignment_checker.check_out_site = shift_assignment_checker.check_out_site
-			shift_assignment_checker.submit()
-	except Exception:
-		# Log all the errors except the OverlappingShiftError(ValidationError)
-		frappe.log_error(frappe.get_traceback(), "Create Shift Assignment Checker")
+def create_shift_assignmen_checker(roster, date):
+	# try:
+	attendance_manager = frappe.db.get_value("Employee", {"name":frappe.get_doc("ONEFM General Setting").get("attendance_manager")},['user_id'])
+	for schedule in roster:
+		print(schedule)
+		shift_assignment_checker = frappe.new_doc("Shift Assignment Checker")
+		shift_assignment_checker.start_date = date
+		shift_assignment_checker.employee = schedule.employee
+		shift_assignment_checker.employee_name = schedule.employee_name
+		shift_assignment_checker.department = schedule.department
+		shift_assignment_checker.operations_role = schedule.operations_role
+		shift_assignment_checker.shift = schedule.shift if schedule.get('doctype') !="Shift Request" else schedule.get('operations_shift')
+		shift_assignment_checker.site = schedule.site
+		shift_assignment_checker.project = schedule.project
+		shift_assignment_checker.shift_type = schedule.shift_type
+		shift_assignment_checker.operations_role = schedule.operations_role
+		shift_assignment_checker.post_abbrv = schedule.post_abbrv
+		shift_assignment_checker.roster_type = schedule.roster_type
+		shift_assignment_checker.site_location =  frappe.db.get_value("Operations Site",{'name':schedule.site},["site_location"])
+		if attendance_manager:
+			print(attendance_manager)
+			shift_assignment_checker.attendance_manager = attendance_manager
+		if schedule.doctype == 'Shift Request':
+			shift_assignment_checker.shift_request = schedule.name
+			if schedule.check_in_site and schedule.check_out_site:
+				shift_assignment_checker.check_in_site = shift_assignment_checker.check_in_site
+				shift_assignment_checker.check_out_site = shift_assignment_checker.check_out_site
+		shift_assignment_checker.submit()
+	# except Exception:
+	# 	# Log all the errors except the OverlappingShiftError(ValidationError)
+	# 	frappe.log_error(frappe.get_traceback(), "Create Shift Assignment Checker")
+
+@frappe.whitelist()
+def create_shift_assignment(doc):
+	doc = json.loads(doc)
+	print(doc.get('employee'))
+	shift_assignment = frappe.new_doc("Shift Assignment")
+	shift_assignment.employee = doc.get('employee')
+	shift_assignment.employee_name = doc.get('employee_name')
+	shift_assignment.department = doc.get('department')
+	shift_assignment.start_date =  doc.get('start_date')
+	shift_assignment.operations_role = doc.get('operations_role')
+	shift_assignment.shift = doc.get('operations_shift')
+	shift_assignment.site = doc.get('site')
+	shift_assignment.project = doc.get('project')
+	shift_assignment.shift_type = doc.get('shift_type')
+	shift_assignment.operations_role = doc.get('operations_role')
+	shift_assignment.post_abbrv = doc.get('post_abbrv')
+	shift_assignment.roster_type = doc.get('roster_type')
+	shift_assignment.site_location = doc.get('site_location')
+	if doc.get('shift_request'):
+		if doc.get('check_in_site') and doc.get('check_out_site'):
+			shift_assignment.check_in_site = doc.get('check_in_site')
+			shift_assignment.check_out_site = doc.get('doc.check_out_site')
+	shift_assignment.submit()
+
+	frappe.set_value("Shift Assignment Checker", doc.get('name'), 'shift_assignment_created', 1)
+	frappe.db.commit()
