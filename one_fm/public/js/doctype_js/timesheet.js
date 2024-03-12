14a52c4459f4cd70e3acb4fbcf0eecb477b20c90
Jamsheer PP <kuttypjamsheer@gmail.com>
2024-03-12 12:49:36 +0530

    feat: Timesheet update

--- one_fm/public/js/doctype_js/timesheet.js
+++ one_fm/public/js/doctype_js/timesheet.js
@@ -8,17 +8,18 @@ frappe.ui.form.on('Timesheet', {
 })
 
 function set_approver(frm){
-    if(frm.doc.employee && frm.doc.attendance_by_timesheet){
-        frappe.call({
-            method: 'one_fm.overrides.timesheet.fetch_approver',
-            args:{
-                'employee':frm.doc.employee
-            },
-            callback: function(r) {
-                if(r.message){
-                    frm.set_value("approver",r.message)
-                }
-            }
-        });
-    }
+  frm.set_value("approver", "");
+  if(frm.doc.employee && frm.doc.attendance_by_timesheet){
+    frappe.call({
+      method: 'one_fm.overrides.timesheet.fetch_approver',
+      args:{
+        'employee':frm.doc.employee
+      },
+      callback: function(r) {
+        if(r.message){
+          frm.set_value("approver",r.message)
+        }
+      }
+    });
+  }
 }
