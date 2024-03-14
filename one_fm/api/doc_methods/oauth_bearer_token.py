b8d0eba8bffead942848a0ed73a60ce6b3edb342
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2024-03-14 13:59:12 +0500

    fix: updated oauth token revocation method and filters

--- one_fm/api/doc_methods/oauth_bearer_token.py
+++ one_fm/api/doc_methods/oauth_bearer_token.py
@@ -1,14 +1,30 @@
 import frappe
-from frappe.integrations.oauth2 import revoke_token
+import requests
+from frappe.utils import get_url
+
+
+def revoke_token(access_token):
+
+    url = f"{get_url()}/api/method/frappe.integrations.oauth2.revoke_token"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    data = {"token": access_token}
+
+    try:
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return True
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error revoking token: {e}")
+        return False
+
 
 def revoke_and_delete_existing_tokens(doc, event):
     """
         Revokes and deletes all the existing tokens for the target user.
     """
-    user_existing_tokens = frappe.get_all('OAuth Bearer Token', filters={'user': doc.user}, fields=['name'])
+    user_existing_tokens = frappe.get_all('OAuth Bearer Token', filters={
+                                          'client': doc.client, 'user': doc.user, 'status': 'Active', 'name': ['!=', doc.name]}, fields=['name'])
 
     for token in user_existing_tokens:
-        # Check if its not the recently generated token
-        if(token.name != doc.name):
-            revoke_token(token=token.name)
-            frappe.delete_doc('OAuth Bearer Token', token.name,ignore_permissions=True)
+        revoke_token(token.name)
