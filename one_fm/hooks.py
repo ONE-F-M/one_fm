d4afa03ff21030422f54a4fb284cc3eee1b6d3ad
Jamsheer PP <kuttypjamsheer@gmail.com>
2024-03-22 16:43:35 +0530

    feat: Attendance Request - assignment rule

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -775,10 +775,11 @@ fixtures = [
 		"dt": "Assignment Rule",
 		"filters": [["name", "in",
 			[
-				"RFM Approver", "Shift Permission Approver", "Attendance Check Reports To",
+				"RFM Approver", "Shift Permission Approver", "Attendance Check Reports To", "Shift Permission Approver",
 				"Attendance Check Site Supervisor", "Attendance Check Shift Supervisor", "Subcontract Staff Request",
 				"Purchase Order Approver Action", "Purchase Order Finance Manager Action", "Purchase Order Purchase Manager Action",
-				"Timesheet Return to Draft", "Timesheet Approval Assignment"
+				"Timesheet Return to Draft", "Timesheet Approval Assignment",
+				"Attendance Request Return to Draft", "Attendance Request Approval"
 			]
 		]]
 	},
