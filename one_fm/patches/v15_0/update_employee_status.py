a0cf034c78196fb82e88aafdb54f79371acba06b
Ebuka Akeru <38866184+Talleyrand333@users.noreply.github.com>
2024-04-07 08:10:10 +0100

    Update update_employee_status.py

--- one_fm/patches/v15_0/update_employee_status.py
+++ one_fm/patches/v15_0/update_employee_status.py
@@ -6,7 +6,7 @@ def execute():
     if active_staff:
         active_staff = [i.name for i in active_staff]
         today = frappe.utils.today()
-        affected_leaves = frappe.get_all("Leave Application",{'from_date':['<=',today],'to_date':['>',today],'employee':['IN',active_staff],'status':"Approved",'docstatus':1},['employee'])
+        affected_leaves = frappe.get_all("Leave Application",{'from_date':['<=',today],'to_date':['>=',today],'employee':['IN',active_staff],'status':"Approved",'docstatus':1},['employee'])
         if affected_leaves:
             for one in affected_leaves:
                 frappe.db.set_value("Employee",one.employee,'status','Vacation')
