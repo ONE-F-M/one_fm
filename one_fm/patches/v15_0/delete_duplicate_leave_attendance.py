c21563e03a298eccff6817f9b54e1a4e31325c03
SalShai <slwshkh@gmail.com>
2024-04-18 09:19:27 +0300

    Create Patch to Delete Duplicate

---
+++ one_fm/patches/v15_0/delete_duplicate_leave_attendance.py
@@ -0,0 +1,28 @@
+import frappe
+import datetime
+from frappe.utils import getdate
+
+def execute() :
+    delete_list = []
+    date = getdate('2023-12-20')
+
+    dup_attendance = frappe.db.sql(f"""SELECT name, employee, attendance_date, roster_type, COUNT(*) as count
+                        FROM `tabAttendance` WHERE status = "On Leave"
+                        GROUP BY employee, attendance_date, roster_type
+                        HAVING COUNT(*)> 1""", as_dict=1)
+    for att in dup_attendance:
+        atts = frappe.db.sql(f"""SELECT name, creation, status, owner
+                        FROM `tabAttendance`
+                        WHERE employee='{att.employee}'
+                        AND attendance_date='{att.attendance_date}'
+                        AND roster_type= '{att.roster_type}'
+                        ORDER BY creation desc""", as_dict=1)
+        if att.count== 2:
+            if atts[0].owner == "Administrator":
+                delete_list.append (atts[0].name)
+            else:
+                delete_list.append (atts[1].name)
+
+    delete_list = tuple(delete_list)
+    frappe.db.sql(f"""DELETE FROM `tabAttendance Check` where attendance IN {delete_list}""")
+    frappe.db.sql(f"""DELETE FROM `tabAttendance` where name IN {delete_list}""")
