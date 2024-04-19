468902b8ea9f92210d509c5ea7b5e6a6c395e3c2
mymi14s <mymi14s@hotmail.com>
2024-04-19 22:17:28 +0100

    update: password security token

--- one_fm/one_fm/doctype/password_reset_token/password_reset_token.py
+++ one_fm/one_fm/doctype/password_reset_token/password_reset_token.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2024, omar jaber and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import (
+	now_datetime, datetime
+)
 
 
 class PasswordResetToken(Document):
-	pass
+	
+	def before_insert(self):
+		self.expiration_time = now_datetime() + datetime.timedelta(minutes=7)
+
+	def validate(self):
+		current_time = now_datetime()
+		if not self.expiration_time:
+			self.expiration_time = current_time + datetime.timedelta(minutes=7)
+
+		if self.expiration_time < current_time:
+			self.status = "Revoked"
+
+
