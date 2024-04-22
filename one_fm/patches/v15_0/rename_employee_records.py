817e00c44aef3619af9ddaef3ce3d55f66d317b2
Kartik Sharma <kartiksharma9319@gmail.com>
2024-04-22 08:48:54 +0000

    fix: Remove debugging statements

--- one_fm/patches/v15_0/rename_employee_records.py
+++ one_fm/patches/v15_0/rename_employee_records.py
@@ -1,16 +1,12 @@
 import frappe
-import time
 
 def execute():
     data = frappe.get_all("Employee", filters={"name": ["like", "HR-EMP%"], "employee_id": ["is", "set"]}, fields=["name", "employee_number"])
 
     # Get total no of employees and convert into batches of 5
     for employee_batches in batches(data, 5):
-        start = time.time()
         # Rename a batch of 5 employees as background job
         frappe.enqueue(patch_employee_naming_series, employee_batches=employee_batches)
-        end = time.time()
-        print("TIME", end-start)
 
 def patch_employee_naming_series(employee_batches):
     for employee in employee_batches:
