f1f7c76ec3d42cb755a9ee19c315f1330011a8f3
Jamsheer PP <kuttypjamsheer@gmail.com>
2024-04-23 09:50:17 +0530

    feat: checkin validation api

--- one_fm/api/v1/face_recognition.py
+++ one_fm/api/v1/face_recognition.py
@@ -6,8 +6,8 @@ from one_fm.api.v1.utils import response
 from frappe.utils import cstr, getdate,now_datetime
 from one_fm.proto import facial_recognition_pb2, facial_recognition_pb2_grpc, enroll_pb2, enroll_pb2_grpc
 from one_fm.api.doc_events import haversine
-
-
+from one_fm.overrides.employee import has_day_off, is_employee_on_leave
+from erpnext.setup.doctype.employee.employee import is_holiday
 
 # setup channel for face recognition
 face_recognition_service_url = frappe.local.conf.face_recognition_service_url
@@ -179,10 +179,10 @@ def verify_checkin_checkout(employee_id: str = None, video : str = None, log_typ
         #     frappe.enqueue('one_fm.operations.doctype.face_recognition_log.face_recognition_log.create_face_recognition_log',
         #     **{'data':{'employee':employee, 'log_type':log_type, 'verification':res.verification,
         #         'message':res.message, 'data':res.data, 'source': 'Checkin'}})
-                
+
         #     doc = create_checkin_log(employee, log_type, skip_attendance, latitude, longitude, "Mobile App")
         #     return response("Success", 201, doc, None)
-        
+
         # if res.verification == "FAILED":
         #     msg = res.message
         #     if not res.verification == "OK":
@@ -213,20 +213,8 @@ def check_employee_non_shift(employee):
         return True
     return False
 
-def has_day_off(employee,date):
-    """
-        Confirm if the employee schedule for that day and employee is set to day off
-    """
-    is_day_off = False
-    existing_schedule = frappe.get_value("Employee Schedule",{'employee':employee,'date':date},['employee_availability'])
-    if existing_schedule:
-        if existing_schedule == 'Day Off':
-            is_day_off = True
-    return is_day_off
-
 @frappe.whitelist()
 def get_site_location(employee_id: str = None, latitude: float = None, longitude: float = None) -> dict:
-
     try:
         if not employee_id:
             return response("Bad Request", 400, None, "employee_id required.")
@@ -239,92 +227,190 @@ def get_site_location(employee_id: str = None, latitude: float = None, longitude
 
         if not isinstance(employee_id, str):
             return response("Bad Request", 400, None, "employee must be of type str.")
-        
+
         employee = frappe.db.get_value("Employee", {"employee_id": employee_id})
         if not employee:
             return response("Resource Not Found", 404, None, "No employee found with {employee_id}".format(employee_id=employee_id))
 
-        shift_exists = get_current_shift(employee)
-        if shift_exists:
-            if shift_exists['type'] == "Early":
+        shift = False
+        shift_details = get_current_shift(employee)
+        if shift_details:
+            if shift_details['type'] == "Early":
                 # check if user can checkin with the correct time
-                return response("Resource Not Found", 404, None, f"You are checking in too early, checkin is allowed in {shift_exists['data']} minutes ")
-            elif shift_exists['type'] == "Late":
-                return response("Resource Not Found", 404, None, f"You are checking out too late, checkout was allowed {shift_exists['data']} minutes ago ")
-            else:
-                shift = shift_exists['data']
-        else:
-            shift = None
+                return response("Resource Not Found", 404, None, f"You are checking in too early, checkin is allowed in {shift_details['data']} minutes ")
+            elif shift_details['type'] == "Late":
+                return response("Resource Not Found", 404, None, f"You are checking out too late, checkout was allowed {shift_details['data']} minutes ago ")
+            elif shift_details['type'] == "On Time":
+                shift = shift_details['data'] # Return the object of Shift Assignment
+
         date = cstr(getdate())
 
-        site, location = None, None
         if shift:
-            log_type = shift.check_existing_checking()
+            if shift.is_replaced == 1:
+                return response("Resource Not Found", 404, None, f"You have been replaced with another Employee")
+
+            if is_attendance_request_exists(employee, date):
+                return response("Resource Not Found", 404, None, f"You have an attendance request for today. Your attendance will be marked.")
+
+            log_type = shift.get_next_checkin_log_type()
             if log_type=='IN':
                 if shift.after_4hrs():
                     # check if hrs has passed since shift start. Here we can also allow those who checked out tp checkin by checkin if OUT exist for same shift
                     return response("Resource Not Found", 404, None, "You are 4 or more hours late, you cannot checkin at this time.")
 
-            if frappe.db.exists("Shift Request", {
-                "employee":employee, 'from_date':['<=',date],
-                'to_date':['>=',date], "status": "Approved"}
-                ):
-                check_in_site, check_out_site = frappe.get_value("Shift Request", {
-                    "employee":employee, 'from_date':['<=',date],'to_date':['>=',date], 
-                    "status": "Approved"},["check_in_site","check_out_site"]
-                )
-                if log_type == "IN":
-                    site = check_in_site
-                    location = frappe.get_list("Location", {'name':check_in_site}, ["latitude","longitude", "geofence_radius"])
-                else:
-                    site = check_out_site
-                    location = frappe.get_list("Location", {'name':check_out_site}, ["latitude","longitude", "geofence_radius"])
+            location = get_shift_site_location(shift, date, log_type)
+            site = frappe.get_value("Operations Shift", shift.shift, "site")
 
-            else:
-                if shift.site_location:
-                    site = shift.site_location
-                    location = frappe.get_list("Location", {'name':shift.site_location}, ["latitude","longitude", "geofence_radius"])
-                elif shift.shift:
-                    site = frappe.get_value("Operations Shift", shift.shift, "site")
-                    location= frappe.db.sql("""
-                        SELECT loc.latitude, loc.longitude, loc.geofence_radius
-                        FROM `tabLocation` as loc
-                        WHERE
-                        loc.name IN (SELECT site_location FROM `tabOperations Site` where name="{site}")
-                        """.format(site=site), as_dict=1)
+            if location:
+                result=location[0]
+                result['user_within_geofence_radius'] = True
 
+                distance = float(haversine(result.latitude, result.longitude, latitude, longitude))
+                if distance > float(result.geofence_radius):
+                    result['user_within_geofence_radius'] = False
 
-        if not site:
-            if has_day_off(employee,date):
+                result['site_name'] = site
+                if shift:
+                    result['shift'] = shift
+
+                # log to checkin radius log
+                data = result.copy()
+                data = {
+                    **data,
+                    **{'employee':employee_id, 'user_latitude':latitude, 'user_longitude':longitude, 'user_distance':distance, 'diff':distance-result.geofence_radius}
+                }
+                if not result['user_within_geofence_radius']:
+                    frappe.enqueue('one_fm.operations.doctype.checkin_radius_log.checkin_radius_log.create_checkin_radius_log', **{'data':data})
+                result['log_type'] = log_type
+                return response("Success", 200, result)
+
+            elif site:
+                return response("Resource Not Found", 404, None, "No site location set for {site}".format(site=site))
+
+        else:
+            if has_day_off(employee, date):
                 employee_name = frappe.get_value("Employee",employee,'employee_name')
                 return response("Resource Not Found", 404, None, f"Dear {employee_name}, Today is your day off.  Happy Recharging!.")
+
+            if is_employee_on_leave(employee, date):
+                return response("Resource Not Found", 404, None, "You are currently on leave, see you soon!")
+
+            if is_holiday(employee, date):
+                return response("Resource Not Found", 404, None, "Today is your holiday, have fun")
+
             return response("Resource Not Found", 404, None, "User not assigned to a shift.")
-
-        if not location and site:
-            return response("Resource Not Found", 404, None, "No site location set for {site}".format(site=site))
-
-        result=location[0]
-        result['user_within_geofence_radius'] = True
-
-        distance = float(haversine(result.latitude, result.longitude, latitude, longitude))
-        if distance > float(result.geofence_radius):
-            result['user_within_geofence_radius'] = False
-
-        result['site_name'] = site
-        if shift:
-            result['shift'] = shift
-
-        # log to checkin radius log
-        data = result.copy()
-        data = {
-            **data,
-            **{'employee':employee_id, 'user_latitude':latitude, 'user_longitude':longitude, 'user_distance':distance, 'diff':distance-result.geofence_radius}
-        }
-        if not result['user_within_geofence_radius']:
-            frappe.enqueue('one_fm.operations.doctype.checkin_radius_log.checkin_radius_log.create_checkin_radius_log', **{'data':data})
-        result['log_type'] = log_type
-        return response("Success", 200, result)
 
     except Exception as error:
         frappe.log_error(title="API Site location", message=frappe.get_traceback())
         return response("Internal Server Error", 500, None, error)
+
+def is_attendance_request_exists(employee, date):
+    return frappe.db.exists(
+        "Attendance Request",
+        {
+            "employee": employee,
+            "from_date": ["<=", date],
+            "to_date": [">=", date],
+            "docstatus": 1
+        }
+    )
+
+def get_shift_site_location(shift, date, log_type):
+    """
+        Method to retrieves the site location details (latitude, longitude, and optionally geofence radius)
+        for a given shift on a specific date, considering both shift and shift request information.
+
+        Args:
+            shift (object): A object of Shift Assignment
+            date (str): The date (YYYY-MM-DD format) for which to retrieve the location.
+            log_type (str): "IN" or "OUT".
+
+        Return:
+            dict (or None): If a valid location is found, a dictionary containing the following keys is returned:
+                latitude (float): The latitude of the site location.
+                longitude (float): The longitude of the site location.
+                geofence_radius (float, optional): The geofence radius of the site location.
+            None: If no valid location information is found.
+    """
+    location = get_shift_request_site_location(shift.employee, date, log_type)
+    if not location:
+        if shift.site_location:
+            return frappe.get_value(
+                "Location",
+                {"name": shift.site_location},
+                ["latitude","longitude", "geofence_radius"],
+                as_dict = True
+            )
+        elif shift.shift:
+            # Fetch the site from Operations Shift to get the location of the Site
+            site = frappe.get_value("Operations Shift", shift.shift, "site")
+            return frappe.db.sql("""
+                SELECT
+                    loc.latitude, loc.longitude, loc.geofence_radius
+                FROM
+                    `tabLocation` as loc
+                WHERE
+                    loc.name IN (
+                        SELECT site_location FROM `tabOperations Site` where name="{site}"
+                    )
+            """.format(site=site), as_dict=1)
+    return location
+
+def get_shift_request_site_location(employee, date, log_type):
+    """
+        This function retrieves the site location details for an employee's approved shift request on a specific date.
+        It checks for an "Approved" shift request that overlaps with the provided date and returns the corresponding check-in
+        or check-out site location (depending on the log_type) along with its latitude, longitude, and geofence radius.
+
+        Args:
+            employee (str): The employee ID for whom to fetch the shift request details.
+            date (str): The date (YYYY-MM-DD format) for which to check the shift request.
+            log_type (str): "IN" or "OUT". This specifies whether to retrieve the check-in
+                or check-out site location from the shift request.
+
+        Return:
+            dict (or None): If an approved shift request is found overlapping the provided date and log_type,
+                a dictionary containing the following keys is returned:
+                latitude (float): The latitude of the site location.
+                longitude (float): The longitude of the site location.
+                geofence_radius (float): The geofence radius of the site location (optional, depending on your data model).
+            None: If no approved shift request is found or there's an error retrieving the location details.
+    """
+
+    location = False
+    shift_request_exists = frappe.db.exists(
+        "Shift Request",
+        {
+            "employee":employee,
+            "from_date":["<=",date],
+            "to_date":[">=",date],
+            "status": "Approved"
+        }
+    )
+    if shift_request_exists:
+        shift_request_details = frappe.get_value(
+            "Shift Request",
+            {
+                "employee":employee,
+                "from_date":["<=",date],
+                "to_date":[">=",date],
+                "status": "Approved"
+            },
+            ["check_in_site", "check_out_site"],
+            as_dict = True
+        )
+        if log_type == "IN":
+            # Fetch check in site location from shift request
+            location = shift_request_details.check_in_site
+        else:
+            # Fetch check out site location from shift request
+            location = shift_request_details.check_out_site
+        if location:
+            # Return the location details
+            return frappe.get_value(
+                "Location",
+                {"name": location},
+                ["latitude","longitude", "geofence_radius"],
+                as_dict = True
+            )
+    return None
