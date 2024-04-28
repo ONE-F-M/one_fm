6e8318205722d8088f00f41d956bcfc8f6a2375f
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2024-04-28 16:14:06 +0500

    chore: linked improve button to helpdesk ticket

--- one_fm/public/js/desk.js
+++ one_fm/public/js/desk.js
@@ -42,43 +42,8 @@ let improve_my_erp = () => {
 	let improveBTN = document.createElement('a');
 	improveBTN.classList = "btn btn-default btn-xs improve-my-erp";
 	improveBTN.textContent = "Improve";
+  improveBTN.href = '/helpdesk/tickets/new'
 	document.querySelector(".form-inline.fill-width.justify-content-end").prepend(improveBTN);
-	$('.improve-my-erp').on('click', function() {
-		var dialog = new frappe.ui.Dialog({
-			title: __("Tell us, How can we improve our ONEERP?"),
-			fields: [
-				{
-					"fieldtype": "Small Text",
-					"fieldname": "suggestions",
-					"label": "Suggestions",
-					"reqd": true
-				}
-			],
-			primary_action_label: 'Submit',
-			primary_action(values) {
-				frappe.confirm('Are you sure you want to proceed?',
-				() => {
-					// action to perform if Yes is selected
-					frappe.call({
-						method: 'one_fm.utils.mark_suggestions_to_issue',
-						args: values,
-						callback: function(r) {
-							if(!r.exc){
-								frappe.show_alert({
-			            message:__('Thanks for your suggestions and are added to the feedback!'),
-			            indicator:'green'
-			          }, 20);
-							}
-						}
-					});
-					dialog.hide();
-				}, () => {
-					// action to perform if No is selected
-				})
-			}
-		});
-		dialog.show();
-	});
 }
 
 // KNOWLEDGE BASE
