6b29f395154be854c827ad6e48c3a4ce58e0c735
mymi14s <mymi14s@hotmail.com>
2024-04-29 10:16:16 +0100

    update: skip face recognition

--- one_fm/api/v1/face_recognition.py
+++ one_fm/api/v1/face_recognition.py
@@ -159,33 +159,33 @@ def verify_checkin_checkout(employee_id: str = None, video : str = None, log_typ
         # setup stub
         # stub = facial_recognition_pb2_grpc.FaceRecognitionServiceStub(channel)
         # request body
-        req = facial_recognition_pb2.FaceRecognitionRequest(
-            username = frappe.session.user,
-            media_type = "video",
-            media_content = video
-        )
+        # req = facial_recognition_pb2.FaceRecognitionRequest(
+        #     username = frappe.session.user,
+        #     media_type = "video",
+        #     media_content = video
+        # )
         # Call service stub and get response
-        res = random.choice(stubs).FaceRecognition(req)
-        data = {'employee':employee, 'log_type':log_type, 'verification':res.verification,
-            'message':res.message, 'data':res.data, 'source': 'Checkin'}
-        if res.verification == "FAILED" and 'Invalid media content' in res.data:
-            frappe.enqueue('one_fm.operations.doctype.face_recognition_log.face_recognition_log.create_face_recognition_log',
-            **{'data':{'employee':employee, 'log_type':log_type, 'verification':res.verification,
-                'message':res.message, 'data':res.data, 'source': 'Checkin'}})
+        # res = random.choice(stubs).FaceRecognition(req)
+        # data = {'employee':employee, 'log_type':log_type, 'verification':res.verification,
+        #     'message':res.message, 'data':res.data, 'source': 'Checkin'}
+        # if res.verification == "FAILED" and 'Invalid media content' in res.data:
+        #     frappe.enqueue('one_fm.operations.doctype.face_recognition_log.face_recognition_log.create_face_recognition_log',
+        #     **{'data':{'employee':employee, 'log_type':log_type, 'verification':res.verification,
+        #         'message':res.message, 'data':res.data, 'source': 'Checkin'}})
                 
-            doc = create_checkin_log(employee, log_type, skip_attendance, latitude, longitude, "Mobile App")
-            return response("Success", 201, doc, None)
+        doc = create_checkin_log(employee, log_type, skip_attendance, latitude, longitude, "Mobile App")
+        return response("Success", 201, doc, None)
         
-        if res.verification == "FAILED":
-            msg = res.message
-            if not res.verification == "OK":
-                frappe.enqueue('one_fm.operations.doctype.face_recognition_log.face_recognition_log.create_face_recognition_log',**{'data':data})
-            return response(msg, 400, None, data)
-        elif res.verification == "OK":
-            doc = create_checkin_log(employee, log_type, skip_attendance, latitude, longitude, "Mobile App")
-            return response("Success", 201, doc, None)
-        else:
-            return response("Success", 400, None, "No response from face recognition server")
+        # if res.verification == "FAILED":
+        #     msg = res.message
+        #     if not res.verification == "OK":
+        #         frappe.enqueue('one_fm.operations.doctype.face_recognition_log.face_recognition_log.create_face_recognition_log',**{'data':data})
+        #     return response(msg, 400, None, data)
+        # elif res.verification == "OK":
+        #     doc = create_checkin_log(employee, log_type, skip_attendance, latitude, longitude, "Mobile App")
+        #     return response("Success", 201, doc, None)
+        # else:
+        #     return response("Success", 400, None, "No response from face recognition server")
     except Exception as error:
         frappe.log_error(frappe.get_traceback(), 'Verify Checkin')
         return response("Internal Server Error", 500, None, error)
