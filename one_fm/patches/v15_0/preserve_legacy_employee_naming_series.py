ccbc63b5cd311c744f27f2de672866791c394c28
Kartik Sharma <kartiksharma9319@gmail.com>
2024-05-02 06:40:31 +0000

    fix: Sync fixtures before executing patch

--- one_fm/patches/v15_0/preserve_legacy_employee_naming_series.py
+++ one_fm/patches/v15_0/preserve_legacy_employee_naming_series.py
@@ -1,6 +1,7 @@
 import frappe
-
+from frappe.utils.fixtures import sync_fixtures
 def execute():
+    sync_fixtures("one_fm")
     data = frappe.get_all("Employee", fields=["name", "employee_name"])
     for employee in data:
             frappe.db.set_value("Employee", employee.name, "custom_legacy_name", employee.name)
