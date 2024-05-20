3a265c93e42508e8b043151b38e1b3e77ee49ba4
SalShai <slwshkh@gmail.com>
2024-05-20 14:26:38 +0300

    Fix: create_new_leave API

--- one_fm/api/v1/leave_application.py
+++ one_fm/api/v1/leave_application.py
@@ -277,7 +277,7 @@ def create_new_leave_application(employee_id: str = None, from_date: str = None,
         if proof_document_required_for_leave_type(leave_type):
             if not proof_document:
                 return response("Missing", 400, None, "Proof document is required for {leave_type}".format(leave_type=leave_type))
-            proof_doc_json = json.loads(proof_document)
+            proof_doc_json = json.loads(proof_document)[0]
             attachment = proof_doc_json.get('attachment')
             attachment_name = proof_doc_json.get('attachment_name')
             if not attachment or not attachment_name:
