f09f7a3c067755fbc8ece1e77d03b5a862b658f6
NoBoneZ <adekunleabraham09@gmail.com>
2024-05-26 09:58:38 +0100

    updated api

--- one_fm/api/v1/face_recognition.py
+++ one_fm/api/v1/face_recognition.py
@@ -1,4 +1,4 @@
-import frappe, ast, base64, time, grpc, json, random, requests
+import frappe, ast, base64, time, grpc, json, random
 from frappe import _
 from one_fm.one_fm.page.face_recognition.face_recognition import update_onboarding_employee, check_existing
 from one_fm.utils import get_current_shift
