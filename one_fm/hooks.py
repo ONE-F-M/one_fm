d4666d442d07d536325530e094db05ad1c4f1117
NoBoneZ <adekunleabraham09@gmail.com>
2024-05-27 21:41:55 +0100

    added email for change in priority

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -241,7 +241,7 @@ doc_events = {
 	"HD Ticket": {
 		"validate": "one_fm.overrides.hd_ticket.validate_hd_ticket",
 		"after_insert":[
-      					# "one_fm.overrides.hd_ticket.send_google_chat_notification",
+      					"one_fm.overrides.hd_ticket.send_google_chat_notification",
                   		"one_fm.overrides.hd_ticket.notify_ticket_raiser_of_receipt"
                     	], 
 		"on_change": "one_fm.overrides.hd_ticket.notify_issue_raiser_about_priority"
