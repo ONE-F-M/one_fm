10335ad6fec5b250f355e53b75437c5f7d2c5aab
Anthony C. Emmanuel <mymi14s@hotmail.com>
2024-06-05 08:11:14 +0100

    Update face_recognition.py

--- one_fm/api/v1/face_recognition.py
+++ one_fm/api/v1/face_recognition.py
@@ -27,14 +27,13 @@ stubs = list()
 video_path = frappe.utils.get_site_path()+"/public/files/video.mp4"
 
 def base64_to_mp4(base64_string):
-    # Decode the Base64 string to bytes
-    video_data = base64.b64decode(base64_string)
-    os.remove(video_path)
-    
-    # Write the bytes to an MP4 file
-    with open(, 'wb') as mp4_file:
-        mp4_file.write(video_data)
-    #print(f"MP4 file has been saved as {output_file_path}")
+	# Decode the Base64 string to bytes
+	video_data = base64.b64decode(base64_string)
+	os.remove(video_path)
+	
+	# Write the bytes to an MP4 file
+	with open(, 'wb') as mp4_file:mp4_file.write(video_data)
+	#print(f"MP4 file has been saved as {output_file_path}")
 
 
 @frappe.whitelist()
