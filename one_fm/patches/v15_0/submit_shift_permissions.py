a1c4b7abb6c378fdddcad1c680e77012aaeceebd
Ebuka <ebukaakeru@gmail.com>
2024-06-19 15:28:00 +0100

    tweak query

--- one_fm/patches/v15_0/submit_shift_permissions.py
+++ one_fm/patches/v15_0/submit_shift_permissions.py
@@ -2,7 +2,7 @@ import frappe
 
 def execute():
     """Submit all shift permissions for 20th,21st and 22nd of May 2024"""
-    all_shift_permissions = frappe.get_all("Shift Permission",{"docstatus":0,'date':['in',['2024-05-20','2024-05-21','2024-05-22']]})
+    all_shift_permissions = frappe.get_all("Shift Permission",{"workflow_state":['in',['Approved','Pending']],"docstatus":0,'date':['in',['2024-05-20','2024-05-21','2024-05-22']]})
     if all_shift_permissions:
         for each in all_shift_permissions:
             frappe.db.set_value("Shift Permission",each.name,'workflow_state','Approved')
