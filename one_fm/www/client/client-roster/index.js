918c84421453c60a27e0537fd69e0e6ab3acd9d9
abraham_adekunle <gadekunle@afexnigeria.com>
2024-06-24 10:49:32 +0100

    made progress with roster

--- one_fm/www/client/client-roster/index.js
+++ one_fm/www/client/client-roster/index.js
@@ -1,41 +1,4 @@
-// import DataTable from "frappe-datatable";
-
 frappe.ready(function () {
-    fetch_data()
-
-
-
 
 });
-
-
-
-const fetch_data = () => {
-    frappe.call({
-		method: 'one_fm.www.client.client-roster.index.get_client_roster',
-        args:{
-            route_hash: new URLSearchParams(window.location.search).get("id")
-        },
-		callback: function(r){
-			if(r && r.status_code == 200){
-                const options = {
-                    columns: r.data.columns,
-                    data: r.data.data
-                }
-                console.log(options)
-                // const options = {
-                //     columns: ['Name', 'Position', 'Salary'],
-                //     data: [
-                //         ['John Doe', 'DevOps Engineer', '$12300'],
-                //         ['Mary Jane', 'UX Design', '$14000'],
-                //     ]
-                // }
-                
-                const datatable = new DataTable('#datatable', options);
-                datatable.getColumns();
-
-            }
-		}
-	});
-}
 
