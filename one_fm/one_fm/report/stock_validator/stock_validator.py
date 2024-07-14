35402a894828b46fc195c722cc9a3a1cd91c2aba
Ebuka <ebukaakeru@gmail.com>
2024-07-14 13:15:30 +0100

    Stock Validator Report

--- one_fm/one_fm/report/stock_validator/stock_validator.py
+++ one_fm/one_fm/report/stock_validator/stock_validator.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import getdate
+from frappe.utils import getdate,get_datetime
 from frappe.utils.nestedset import get_descendants_of
 
 SLE_FIELDS = (
@@ -11,6 +11,7 @@ SLE_FIELDS = (
 	"warehouse",
 	"posting_date",
 	"posting_time",
+	"posting_datetime",
 	"creation",
 	"voucher_type",
 	"voucher_no",
@@ -33,7 +34,8 @@ def generate_columns(filters):
 	columns = [{
 			"label": "Item Code",
 			"fieldname": 'item_code',
-			"fieldtype": "Data",
+			"fieldtype": "Link",
+			"options": "Item",
 			"width": 200,
 		},{
 			"label": "Item Name ",
@@ -58,11 +60,17 @@ def generate_columns(filters):
 			"fieldtype": "Float", 
 			"width": 200,
 		},
-			   {
+			{
 			"label": "Stock Balance Valuation Rate",
 			"fieldname": 'balance_valuation',
 			"fieldtype": "Float",
 			"width": 200,
+		},
+   		{
+			"label": "Is Quantity Aligned",
+			"fieldname": 'is_qty_aligned',
+			"fieldtype": "Data",
+			"width": 100,
 		}]
 	return columns
  
@@ -110,35 +118,81 @@ def get_stock_ledger_entries(filters):
 			cond+=f" and item_code = '{all_items[0]}'"
 		else:
 			cond+= f" and item_code in {tuple(all_items)}"
-		
-
-	# if filters.from_date:
-	# 	sle_filters["posting_date"] = (">=", filters.from_date)
-	# 	cond+=f" and posting_date >= '{filters.from_date}'"
 	if filters.to_date:
 		sle_filters["posting_date"] = ("<=", filters.to_date)
 		cond+=f" and posting_date <= '{filters.to_date}'"
-	final_query  = query+cond + " ORDER BY posting_date DESC LIMIT 1"
-	result_set = frappe.db.sql(final_query,as_dict=1)
-	result_set2 = frappe.get_all(
+	
+	
+	result_set = frappe.get_all(
 		"Stock Ledger Entry",
 		fields=SLE_FIELDS,
 		filters=sle_filters,
 		order_by="timestamp(posting_date, posting_time), creation",
 	)
-	return {'result_set':result_set,'result_set_2':result_set2}
+	return result_set
 
+def get_last_ledger_entry(ledger_data,item,date_value):
+	"""
+	Return the most recent stock data available for an item
+
+	Args:
+		ledger_data (list): Stock Ledger Data
+		item (string): Item
+	"""
+	date_dict = {}
+	date_str = frappe.utils.get_date_str(date_value)
+	date = get_datetime(date_str+' 23:59:59')
+	for each in ledger_data:
+		if each.item_code == item:
+			row_date = get_datetime(each.posting_datetime)
+			if date > row_date:
+				diff_in_seconds = abs((date-row_date).total_seconds())
+				date_dict[diff_in_seconds] = ledger_data.index(each)
+			
+    
+	lowest_diff = min(date_dict.keys())
+	lowest_index = date_dict[lowest_diff]
+	return ledger_data[lowest_index]
 	
 
 def execute(filters=None):
 	columns, data = [], []
 	update_filters(filters)
+	stock_balance_dict = {}
 	ledger_items = []
 	columns= generate_columns(filters)
 	ledger_data = get_stock_ledger_entries(filters)
-	stock_balance_data = get_stock_balance(filters)
-	# if ledger_data:
-	# 	data = list(set([{'item_code':i.item_code} for i in  ledger_data])) #Remove Duplicates
-		
-
+	stock_balance_data = get_stock_balance(filters)[1]
+	for each in stock_balance_data:
+		stock_balance_dict[each.item_code] = each
+	if ledger_data:
+		unique_items = list(set([i.item_code for i in ledger_data]))
+		unique_names = frappe.get_all("Item",{"name":['in',unique_items]},['item_name','name'])
+		item_dict = [{}]
+		for each in unique_names:
+			last_ledger_balance = get_last_ledger_entry(ledger_data,each.name,filters.to_date)
+			if stock_balance_dict.get(each.name):
+				aligned_qty = "Yes" if float(last_ledger_balance.qty_after_transaction) == float(stock_balance_dict[each.name].bal_qty) else "No"
+				aligned_val =  "Yes" if float(last_ledger_balance.valuation_rate) == float(stock_balance_dict[each.name].bal_val) else "No"
+				data.append({
+        			'item_code':each.name,
+           			'item_name':stock_balance_dict[each.name].get("item_name"),
+					'ledger_quantity':last_ledger_balance.qty_after_transaction,
+					'ledger_valuation':last_ledger_balance.valuation_rate,
+					'balance_quantity':stock_balance_dict[each.name].bal_qty,
+					'balance_valuation':stock_balance_dict[each.name].bal_val,
+					'is_qty_aligned':aligned_qty,
+					
+           		})
+			else:
+				data.append({
+        			'item_code':each.name,
+           			'item_name':each.item_name,
+					'ledger_quantity':0,
+					'ledger_valuation':0,
+					'balance_quantity':0,
+					'balance_valuation':0,
+					'is_qty_aligned':'Yes',
+     				
+           			})
 	return columns, data
