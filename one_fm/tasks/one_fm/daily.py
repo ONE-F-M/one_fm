9730a2088612ae1d2bf7d10217b45006b13f4326
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2024-07-15 15:29:06 +0500

    scheduled daily task for employee docs expiry reminder

--- one_fm/tasks/one_fm/daily.py
+++ one_fm/tasks/one_fm/daily.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-import frappe, json
-from frappe.utils import getdate, nowdate, get_last_day
+import frappe
+import json
+from frappe.utils import getdate, get_last_day, add_days, today
+from frappe.utils.data import get_url_to_form
 from frappe.desk.query_report import *
+from one_fm.processor import sendemail
 
 
 def generate_contracts_invoice():
@@ -11,7 +14,7 @@ def generate_contracts_invoice():
     try:
         dt = datetime
         contracts = frappe.get_list('Contracts', filters={
-            'workflow_state':'Active',
+            'workflow_state': 'Active',
             # 'due_date':str(dt.today().date().day)
         })
         # generate
@@ -28,6 +31,7 @@ def generate_contracts_invoice():
     except Exception as e:
         frappe.log(str(e), "Contracts Invoice")
 
+
 def roster_projection_view_task():
     """
         Generate ROSTER projection
@@ -36,6 +40,99 @@ def roster_projection_view_task():
     dt = datetime
     report = frappe.get_doc("Report", 'Roster Projection View')
     background_enqueue_run(report.name,
-        filters=json.dumps(
-            {'month':dt.today().month, 'year':dt.today().year}), user='Administrator'
-    )
+                           filters=json.dumps(
+                               {'month': dt.today().month, 'year': dt.today().year}), user='Administrator'
+                           )
+
+
+def notify_for_employee_docs_exiry():
+    """
+        Notify Onboarding officers about employee docs expiration
+
+        - Work Permit
+        - Residency
+        - Civil ID
+        - Passport
+    """
+    supervisor = frappe.db.get_single_value(
+        'GRD Settings', 'default_grd_supervisor')
+    operator = frappe.db.get_single_value(
+        'GRD Settings', 'default_grd_operator')
+    operator_pifss = frappe.db.get_single_value(
+        'GRD Settings', 'default_grd_operator_pifss')
+
+    recipients = list(set([supervisor, operator, operator_pifss]))
+
+    target_expiry_date = add_days(today(), 30)
+
+    work_permit_employees = frappe.get_all("Employee",
+                                           filters=[
+                                               ["work_permit_expiry_date",
+                                                   "=", target_expiry_date],
+                                           ],
+                                           fields=["name", "employee_name",
+                                                   "work_permit_expiry_date"]
+                                           )
+    residency_employees = frappe.get_all("Employee",
+                                         filters=[
+                                             ["residency_expiry_date",
+                                                 "=", target_expiry_date],
+                                         ],
+                                         fields=["name", "employee_name",
+                                                 "residency_expiry_date"]
+                                         )
+    civil_id_employees = frappe.get_all("Employee",
+                                        filters=[
+                                            ["civil_id_expiry_date",
+                                                "=", target_expiry_date],
+                                        ],
+                                        fields=["name", "employee_name",
+                                                "civil_id_expiry_date"]
+                                        )
+    passport_employees = frappe.get_all("Employee",
+                                        filters=[
+                                            ["valid_upto", "=", target_expiry_date],
+                                        ],
+                                        fields=[
+                                            "name", "employee_name", "valid_upto"]
+                                        )
+
+    def send_notification(document_type, employees):
+        for employee in employees:
+            subject = f"Document Expiry - {employee['employee_name']}"
+            header = f"{employee['employee_name']}'s {document_type} is about to expire."
+            message = f"Dear Onboarding Officer,<br><br>The following employee's {document_type} is expiring on {target_expiry_date}:<br><br>Employee Name: {employee['employee_name']}<br>Employee ID: {employee['name']}<br><br>Kindly take the necessary action."
+
+            for recipient in recipients:
+                # Create a notification log entry
+                notification_doc = frappe.get_doc({
+                    "doctype": "Notification Log",
+                    "subject": subject,
+                    "email_content": message,
+                    "document_type": "Employee",
+                    "document_name": employee['name'],
+                    "for_user": recipient,
+                })
+                notification_doc.insert(ignore_permissions=True)
+                frappe.db.commit()
+
+                doc_link = get_url_to_form(notification_doc.document_type, notification_doc.document_name)
+
+                context = {
+                    "body_content": notification_doc.subject,
+                    "description": notification_doc.email_content,
+                    "document_type": notification_doc.document_type,
+                    "document_name": notification_doc.document_name,
+                    "doc_link": doc_link,
+                    "header": header
+                }
+
+                msg = frappe.render_template(
+                    'one_fm/templates/emails/notification_log.html', context=context)
+                
+                sendemail(recipients=[recipient], subject=subject, content=msg, is_external_mail=True,is_scheduler_email=True)
+
+    send_notification("Work Permit", work_permit_employees)
+    send_notification("Residency", residency_employees)
+    send_notification("Civil ID", civil_id_employees)
+    send_notification("Passport", passport_employees)
