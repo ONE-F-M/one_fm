4f280151347374f49c214ab600bc7f32c5022571
abraham_adekunle <gadekunle@afexnigeria.com>
2024-09-16 07:47:53 +0100

    removed patch

--- one_fm/patches/v15_0/add_task_assignments_to_form_field.py
+++ one_fm/patches/v15_0/add_task_assignments_to_form_field.py
@@ -4,19 +4,19 @@ from frappe.desk.form.assign_to import get as get_assignments
 def execute():
     tasks = frappe.get_all("Task")
     
-    for task in tasks:
-        doc = frappe.get_doc('Task', task.name)        
-        user_doc_assignments = [assignment.owner for assignment in get_assignments({'doctype': 'Task', 'name': task.name})]
+    # for task in tasks:
+    #     doc = frappe.get_doc('Task', task.name)        
+    #     user_doc_assignments = [assignment.owner for assignment in get_assignments({'doctype': 'Task', 'name': task.name})]
         
-        # If there are no assignments then skip next step
-        if len(user_doc_assignments) == 0:
-            continue
+    #     # If there are no assignments then skip next step
+    #     if len(user_doc_assignments) == 0:
+    #         continue
         
-        # Append the existing doc assignments to the 'custom_assigned_to' field
-        for user in user_doc_assignments:
-            doc.append('custom_assigned_to', {"user": user})
+    #     # Append the existing doc assignments to the 'custom_assigned_to' field
+    #     for user in user_doc_assignments:
+    #         doc.append('custom_assigned_to', {"user": user})
         
-        doc.save(ignore_permissions=True)
+    #     doc.save(ignore_permissions=True)
 
-    frappe.db.commit()
+    # frappe.db.commit()
 
