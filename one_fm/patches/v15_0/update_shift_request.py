f3193230f99ac0279120fcb9d57ef44635c22a71
abraham_adekunle <gadekunle@afexnigeria.com>
2024-09-18 09:17:00 +0100

    updated patch

--- one_fm/patches/v15_0/update_shift_request.py
+++ one_fm/patches/v15_0/update_shift_request.py
@@ -6,7 +6,6 @@ from one_fm.utils import get_approver
 from one_fm.api.notification import get_employee_user_id
 
 def execute():
-    pass
     sync_fixtures("one_fm")
     # Pending Approval and Draft 
     open_shift_requests = frappe.get_all("Shift Request", 
