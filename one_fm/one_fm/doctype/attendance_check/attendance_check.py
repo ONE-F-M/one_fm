b92922ac447a8ae504d8bd7356e5c6bbcb25a518
yusuffAFEX <yusufoyedele43@gmail.com>
2024-10-09 20:55:06 +0100

    attendance-check-created-after-approved-shift-permission

--- one_fm/one_fm/doctype/attendance_check/attendance_check.py
+++ one_fm/one_fm/doctype/attendance_check/attendance_check.py
@@ -301,10 +301,16 @@ def create_attendance_check(attendance_date=None):
         if not attendance_date:
             attendance_date = add_days(today(), -1)
 
+        employee_with_shift_permission = [i.employee for i in frappe.db.get_list('Shift Permission', {
+            'date': attendance_date
+        }
+        )]
+
         absentees = frappe.get_all("Attendance", filters={
             'docstatus':1,
             'status':'Absent',
-            'attendance_date':attendance_date},
+            'attendance_date':attendance_date,
+            'employee':["not in", employee_with_shift_permission]},
             fields="*"
         )
 
