1707cb2779c9fb28ecc0defa6e35d0f2312f8a29
yusuffAFEX <yusufoyedele43@gmail.com>
2024-10-20 16:35:09 +0100

    email leave application email

--- one_fm/overrides/leave_application.py
+++ one_fm/overrides/leave_application.py
@@ -159,12 +159,14 @@ class LeaveApplicationOverride(LeaveApplication):
             parent_doc = frappe.get_doc("Leave Application", self.name)
             employee = frappe.db.get_value("Employee", self.employee, "employee_name_in_arabic", as_dict=True) or {}
             leave_approver = frappe.db.get_value("Employee", {"company_email": self.leave_approver}, ["employee_name_in_arabic", "employee_name"], as_dict=True) or {}
+            leave_type_in_arabic = frappe.db.get_value('Leave Type', self.leave_type, 'custom_leave_type_name_in_arabic')
             args = parent_doc.as_dict()
             args["employee_name_in_arabic"] = employee.get("employee_name_in_arabic")
             args["leave_approver_in_arabic"] = leave_approver.get("employee_name_in_arabic")
             args["leave_approver_in_english"] = leave_approver.get("employee_name")
             args["status"] = "Pending" if args.get("status") == "Open" else "Approved"
             args["status_in_arabic"] = get_status_in_arabic(args.get("status"))
+            args["leave_type_in_arabic"] = leave_type_in_arabic if leave_type_in_arabic else self.leave_type
             email_template = frappe.get_doc("Email Template", template)
             message = frappe.render_template(email_template.response_html, args)
             if is_app_user(self.employee):
