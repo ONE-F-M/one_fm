dcb20cfe7fb657b08fa29a3677539bde3e1b5aff
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-05-04 20:16:55 +0500

    added child table doctypes for department weekly review

---
+++ one_fm/one_fm/doctype/department_blocker/department_blocker.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, omar jaber and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Department Blocker", {
+// 	refresh(frm) {
+
+// 	},
+// });
