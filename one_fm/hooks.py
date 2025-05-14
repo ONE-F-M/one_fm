e63607a150ff41401f33298436ee2f7adb0092aa
zeinaremah <zeinaremah@gmail.com>
2025-05-14 14:35:43 +0300

    refactor: undo setup for checkbox added to email template doctype

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -164,7 +164,6 @@ website_generators = ["Client"]
 
 before_install = "one_fm.install.before_install.execute"
 # after_install = "one_fm.install.after_install"
-after_install = ["one_fm.setup.setup.after_install"]
 #revert staging
 
 # Desk Notifications
