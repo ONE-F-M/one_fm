2e72c5a8f619a61d807f85be182a315898845ab3
yusuffAFEX <yusufoyedele43@gmail.com>
2025-05-15 21:56:07 +0100

    ROS-8

--- one_fm/operations/doctype/default_shift_checker/default_shift_checker.js
+++ one_fm/operations/doctype/default_shift_checker/default_shift_checker.js
@@ -4,6 +4,15 @@
 frappe.ui.form.on("Default Shift Checker", {
     action_type: function(frm){
         reset_new_operations_shift_and_role_allocation(frm)
+    },
+    new_shift_allocation: function(frm){
+        frm.set_query("new_operations_role_allocation", function() {
+            return {
+                filters: {
+                    shift: frm.doc.new_shift_allocation
+                }
+            };
+        });
     }
 });
 
