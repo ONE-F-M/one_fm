9fe200abf378ce8b76bf6b44dc2de04b71993868
yusuffAFEX <yusufoyedele43@gmail.com>
2025-06-29 18:37:43 +0100

    ON-165

--- one_fm/setup/setup.py
+++ one_fm/setup/setup.py
@@ -2,3 +2,4 @@ import frappe
 
 def after_migrate():
     frappe.get_attr("one_fm.setup.workflow.leave_acknowledgement_form.execute")()
+    frappe.get_attr("one_fm.setup.assignment_rule.subcontract_staff_shortlist.execute")()
