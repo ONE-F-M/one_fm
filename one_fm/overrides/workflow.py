b466d8703640d45d443c978445f21c87c83cf109
zeinaremah <zeinaremah@gmail.com>
2025-07-29 11:56:45 +0300

    Revert "refactor(workflow): remove redundant if-else block in get_next_possible_transitions for clarity and maintainability"
    This reverts commit 439453d6760978b1c0cb415cf18cb7ce4fc38f4e.

--- one_fm/overrides/workflow.py
+++ one_fm/overrides/workflow.py
@@ -255,8 +255,14 @@ def get_next_possible_transitions(workflow_name, state,doc=None):
 	transitions = frappe.get_all('Workflow Transition', fields='*', filters=filters)
 	valid_transitions = []
 	for transition in transitions:
-		if is_transition_condition_satisfied(transition, doc):
+		if isinstance(doc, dict):
+			doc_obj = frappe.get_doc(doc)
+		else:
+			doc_obj = doc
+
+		if is_transition_condition_satisfied(transition, doc_obj):
 			valid_transitions.append(transition)
+
 	return valid_transitions
 			
 def get_doc_history(doc, transition=None):
