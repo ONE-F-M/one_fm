4d4ce6f9cd8e77ba9c7c8d6c51c488da2c41f178
jam <jam@Mac.lan>
2025-10-14 12:44:43 +0530

    fix: assignment rule task

--- one_fm/patches/v15_0/add_task_assignment_rule.py
+++ one_fm/patches/v15_0/add_task_assignment_rule.py
@@ -2,4 +2,4 @@ import frappe
 from one_fm.custom.assignment_rule.assignment_rule import get_assignment_rule_json_file, create_assignment_rule
 
 def execute():
-    create_assignment_rule(get_assignment_rule_json_file("task.json"))
+    create_assignment_rule(get_assignment_rule_json_file("confirm_erpnext_task.json"))
