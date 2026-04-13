bc433fa30c825ad83dd4db37fbcfa22e14a2bab5
Jamsheer PP <kuttypjamsheer@gmail.com>
2026-04-13 21:13:41 +0530

    fix: patch item code change (#5918)
    Co-authored-by: jam <jam@jams-MacBook-Air.local>

--- one_fm/patches/v15_0/rename_item_int_civ_000033.py
+++ one_fm/patches/v15_0/rename_item_int_civ_000033.py
@@ -1,7 +1,9 @@
 import frappe
 
 def execute():
-    if frappe.db.exists("Item", "INT-CIV-000033") and not frappe.db.exists("Item", "INT-CIV-000002"):
-        frappe.rename_doc("Item", "INT-CIV-000033", "INT-CIV-000002", ignore_permissions=True)
-        frappe.db.set_value("Item", "INT-CIV-000002", "item_code", "INT-CIV-000002", update_modified=False)
-        frappe.db.set_value("Item", "INT-CIV-000002", "item_id", "000002", update_modified=False)
+    existing_item_name = "INT-CIV-000033"
+    new_item_name = "INT-CIV-000002"
+    if frappe.db.exists("Item", existing_item_name) and not frappe.db.exists("Item", new_item_name):
+        frappe.rename_doc("Item", existing_item_name, new_item_name)
+        frappe.db.set_value("Item", new_item_name, "item_code", new_item_name, update_modified=False)
+        frappe.db.set_value("Item", new_item_name, "item_id", "000002", update_modified=False)
