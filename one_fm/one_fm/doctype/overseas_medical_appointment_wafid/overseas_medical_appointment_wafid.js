fadf2750e86df07a69f66426a621901894775b36
Kevin Makora <develop@one-fm.com>
2026-05-11 13:48:29 +0300

    feat(medical): redirect to create Remedical doc when Medical failed status is saved

--- one_fm/one_fm/doctype/overseas_medical_appointment_wafid/overseas_medical_appointment_wafid.js
+++ one_fm/one_fm/doctype/overseas_medical_appointment_wafid/overseas_medical_appointment_wafid.js
@@ -10,6 +10,29 @@ frappe.ui.form.on("Overseas Medical Appointment WAFID", {
 	},
 	appointment_date: function(frm) {
 		update_actual_medical_status(frm);
+	},
+	status: function(frm) {
+		if (frm.doc.status === "Medical failed and Proceeded to Remedical") {
+			frappe.show_alert({
+				message: __("Save this document to automatically create the Remedical record."),
+				indicator: "orange"
+			});
+		}
+	},
+	after_save: function(frm) {
+		if (frm.doc.status === "Medical failed and Proceeded to Remedical") {
+			frappe.db.get_value("Overseas Remedical", {"original_medical_ref": frm.doc.name}, "name", function(r) {
+				if (!r || !r.message || !r.message.name) {
+					frappe.confirm('Do you want to create the Overseas Remedical record now?', function() {
+						frappe.route_options = {
+							candidate_country_process: frm.doc.candidate_country_process,
+							original_medical_ref: frm.doc.name
+						};
+						frappe.new_doc("Overseas Remedical");
+					});
+				}
+			});
+		}
 	}
 });
 
