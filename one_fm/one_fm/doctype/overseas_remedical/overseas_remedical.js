4d77c4f0b6f4f60dd8aa0f543b799382dd430da6
Kevin Makora <develop@one-fm.com>
2026-05-11 09:31:52 +0300

    feat(medical): restructure Remedical form into Appointment and Actual Remedical sections with auto-sync script

--- one_fm/one_fm/doctype/overseas_remedical/overseas_remedical.js
+++ one_fm/one_fm/doctype/overseas_remedical/overseas_remedical.js
@@ -1,8 +1,17 @@
-// Copyright (c) 2026, ONE FM and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Overseas Remedical", {
+	appointment_status: function(frm) {
+		update_actual_remedical_status(frm);
+	},
+	appointment_date: function(frm) {
+		update_actual_remedical_status(frm);
+	}
+});
 
-// frappe.ui.form.on("Overseas Remedical", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function update_actual_remedical_status(frm) {
+	if (frm.doc.appointment_status === "Booked" && frm.doc.appointment_date) {
+		// Only update if it's currently "Yet to apply"
+		if (frm.doc.status === "Yet to apply") {
+			frm.set_value("status", "In process");
+		}
+	}
+}
