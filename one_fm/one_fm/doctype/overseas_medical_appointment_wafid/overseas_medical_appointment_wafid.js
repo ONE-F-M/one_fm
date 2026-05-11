e2b6a17b03cb78d0398d0c059123c92bf3176c4d
Kevin Makora <develop@one-fm.com>
2026-05-11 09:07:04 +0300

    feat(medical): restructure WAFID form into Appointment and Actual Medical sections with auto-sync script

--- one_fm/one_fm/doctype/overseas_medical_appointment_wafid/overseas_medical_appointment_wafid.js
+++ one_fm/one_fm/doctype/overseas_medical_appointment_wafid/overseas_medical_appointment_wafid.js
@@ -1,8 +1,17 @@
-// Copyright (c) 2026, ONE FM and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Overseas Medical Appointment WAFID", {
+	appointment_status: function(frm) {
+		update_actual_medical_status(frm);
+	},
+	appointment_date: function(frm) {
+		update_actual_medical_status(frm);
+	}
+});
 
-// frappe.ui.form.on("Overseas Medical Appointment WAFID", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function update_actual_medical_status(frm) {
+	if (frm.doc.appointment_status === "Booked" && frm.doc.appointment_date) {
+		// Only update if it's currently "Yet to apply"
+		if (frm.doc.status === "Yet to apply") {
+			frm.set_value("status", "In process");
+		}
+	}
+}
