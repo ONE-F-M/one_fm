ccef9f92242c29a3223ffced550eaa55d4bac79c
Kevin Makora <develop@one-fm.com>
2026-05-11 15:05:50 +0300

    feat(dashboard): add custom dashboard method to accurately count sibling tracking documents

---
+++ one_fm/one_fm/doctype/pcc_clearance/pcc_clearance_dashboard.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+    return {
+        'fieldname': 'candidate_country_process',
+        'method': 'one_fm.utils.get_sibling_counts',
+        'transactions': [
+            {'items': ['Candidate Country Process', 'Overseas Medical Appointment WAFID']},
+            {'items': ['Overseas Remedical', 'Visa Stamping']},
+            {'items': ['Arrival and Deployment']}
+        ],
+    }
