196d935b5c4bbccf0595c17664376273db75cd37
Kevin Makora <develop@one-fm.com>
2026-05-11 15:17:03 +0300

    fix(dashboard): replace dashboard.open_document_list directly instead of relying on async DOM click events to ensure correct routing

--- one_fm/one_fm/doctype/arrival_and_deployment/arrival_and_deployment.js
+++ one_fm/one_fm/doctype/arrival_and_deployment/arrival_and_deployment.js
@@ -5,25 +5,21 @@
 // 	refresh(frm) {
 
 		// Override dashboard link routing for sibling tracking documents
-		setTimeout(() => {
-			if (frm.dashboard && frm.dashboard.wrapper) {
-				frm.dashboard.wrapper.find('.document-link').off('click').on('click', function(e) {
-					e.preventDefault();
-					e.stopPropagation();
-					let doctype = $(this).attr('data-doctype');
-					if (doctype && doctype !== "Candidate Country Process") {
-						frappe.route_options = {
-							"candidate_country_process": frm.doc.candidate_country_process
-						};
-					} else {
-						frappe.route_options = {
-							"name": frm.doc.candidate_country_process
-						};
-					}
-					frappe.set_route("List", doctype);
-				});
-			}
-		}, 1000);
+		if (frm.dashboard) {
+			frm.dashboard.open_document_list = function($link, show_open) {
+				let doctype = $link.attr("data-doctype");
+				if (doctype && doctype !== "Candidate Country Process") {
+					frappe.route_options = {
+						"candidate_country_process": frm.doc.candidate_country_process
+					};
+				} else {
+					frappe.route_options = {
+						"name": frm.doc.candidate_country_process
+					};
+				}
+				frappe.set_route("List", doctype);
+			};
+		}
 
 
 // 	},
