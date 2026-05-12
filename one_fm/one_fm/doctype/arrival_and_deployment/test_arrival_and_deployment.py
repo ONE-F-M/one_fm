e0b7a7bfe68f02d705cc5b0876a35336b71bdcdb
Kevin Makora <develop@one-fm.com>
2026-05-12 13:51:17 +0300

    test: add unit tests for arrival and deployment

--- one_fm/one_fm/doctype/arrival_and_deployment/test_arrival_and_deployment.py
+++ one_fm/one_fm/doctype/arrival_and_deployment/test_arrival_and_deployment.py
@@ -6,4 +6,35 @@ from frappe.tests.utils import FrappeTestCase
 
 
 class TestArrivalandDeployment(FrappeTestCase):
-	pass
+	def setUp(self):
+		frappe.db.sql("delete from `tabArrival and Deployment`")
+
+	def test_arrival_date_mandatory_for_onboarding(self):
+		doc = frappe.new_doc("Arrival and Deployment")
+		doc.candidate_name = "Test Candidate"
+		doc.onboarding_officer = "Administrator"
+		doc.general_services = "Administrator"
+		doc.warehouse = "Administrator"
+		doc.workflow_state = "Pending Onboarding"
+		
+		# Should throw exception because arrival_date is missing
+		self.assertRaises(frappe.ValidationError, doc.validate)
+		
+		# Once arrival_date is set, it should pass (for local hire since no CCP)
+		doc.arrival_date = frappe.utils.today()
+		doc.validate() # Should not raise exception
+		
+	def test_overseas_flight_details_mandatory(self):
+		doc = frappe.new_doc("Arrival and Deployment")
+		doc.candidate_name = "Overseas Candidate"
+		doc.candidate_country_process = "CCP-TEST-001"
+		doc.onboarding_officer = "Administrator"
+		doc.transportation_manager = "Administrator"
+		doc.finance = "Administrator"
+		doc.general_services = "Administrator"
+		doc.warehouse = "Administrator"
+		doc.workflow_state = "Pending Onboarding"
+		doc.arrival_date = frappe.utils.today()
+		
+		# Should throw exception because flight details are missing for overseas
+		self.assertRaises(frappe.ValidationError, doc.validate)
