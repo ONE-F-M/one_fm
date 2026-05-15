58a990c23a67f50b22390b0b6798a602fb6a485e
Kevin Makora <develop@one-fm.com>
2026-05-15 22:17:58 +0300

    fix: export 3 remaining resignation custom fields for employee to prevent fatal mysql errors

--- one_fm/custom/custom_field/employee.py
+++ one_fm/custom/custom_field/employee.py
@@ -1013,6 +1013,32 @@ def get_employee_custom_fields():
 				"insert_after": "feedback",
 				"is_system_generated": 1,
 				"label": "Resignation Workflow Status",
+			},
+			{
+				"dt": 'Employee',
+			"label": 'Current Withdrawal',
+			"fieldname": 'current_withdrawal',
+			"insert_after": 'current_resignation',
+			"fieldtype": 'Link',
+			"options": 'Employee Resignation Withdrawal',
+			"is_system_generated": 1
+			},
+			{
+				"dt": 'Employee',
+			"label": 'Resignation Status & Documents',
+			"fieldname": 'resignation_section',
+			"insert_after": 'feedback',
+			"fieldtype": 'Section Break',
+			"is_system_generated": 1
+			},
+			{
+				"dt": 'Employee',
+			"label": 'Resignation Date',
+			"fieldname": 'resignation_date',
+			"insert_after": 'date_of_joining',
+			"fieldtype": 'Date',
+			"read_only": 1,
+			"is_system_generated": 1
 			}
 		]
 	}
