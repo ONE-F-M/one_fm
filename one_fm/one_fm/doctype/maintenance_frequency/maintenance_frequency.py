652b68afd3f7228fce2b79fb1f21e5214a5fd5de
yusuffAFEX <yusufoyedele43@gmail.com>
2026-05-18 21:25:36 +0100

    WI-000691

--- one_fm/one_fm/doctype/maintenance_frequency/maintenance_frequency.py
+++ one_fm/one_fm/doctype/maintenance_frequency/maintenance_frequency.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2026, ONE FM and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import cint
 
 
 class MaintenanceFrequency(Document):
-	pass
+	def validate(self):
+		self.validate_frequency_days()
+
+	def validate_frequency_days(self):
+		"""Ensure frequency_days is a positive integer to avoid infinite loops in the scheduler."""
+		if cint(self.frequency_days) <= 0:
+			frappe.throw(
+				_("Frequency (Days) must be a positive integer. Got: {0}").format(self.frequency_days)
+			)
--- one_fm/one_fm/doctype/space/space.py
+++ one_fm/one_fm/doctype/space/space.py
@@ -1,9 +1,38 @@
 # Copyright (c) 2026, ONE FM and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Space(Document):
-	pass
+	def validate(self):
+		self.validate_location_chain()
+
+	def validate_location_chain(self):
+		"""Enforce the entire location chain: floor → building → site → project.
+
+		Auto-corrects fetched fields to match the selected maintenance_floor,
+		preventing API-level tampering of read-only fields.
+		"""
+		if not self.maintenance_floor:
+			return
+
+		floor = frappe.db.get_value(
+			"Maintenance Floor",
+			self.maintenance_floor,
+			["building_name", "operations_site", "project"],
+			as_dict=True,
+		)
+		if not floor:
+			return
+
+		if self.building_name != floor.building_name:
+			self.building_name = floor.building_name
+
+		if self.operations_site != floor.operations_site:
+			self.operations_site = floor.operations_site
+
+		if self.project != floor.project:
+			self.project = floor.project
