e196389dcef2d33bc48d8ca0f99d07374ec67544
Talleyrand333 <ebukaakeru@gmail.com>
2026-05-21 12:51:15 +0100

    Revert "Merge pull request #6075 from ONE-F-M/WI-000513"
    This reverts commit a1dbdc94dd3073494dff6026b214aab428dd4fb9, reversing
    changes made to 21130b2c3dcd3dc55dc98bc76fb913c7a4c0cc0d.

--- one_fm/patches/v15_0/update_bank_account_workflow.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from one_fm.custom.workflow.workflow import create_workflow, get_workflow_json_file
-
-def execute():
-	"""Create or update Bank Account workflow with Inactive Account state."""
-	workflow_data = get_workflow_json_file("bank_account.json")
-	if workflow_data:
-		create_workflow(workflow_data)
-		frappe.db.commit()
