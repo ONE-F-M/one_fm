eecd96f4a5016ebb63917056cd11968869a755ad
yusuffAFEX <yusufoyedele43@gmail.com>
2026-05-24 12:29:28 +0100

    WI-000863

---
+++ one_fm/one_fm/doctype/maintenance_holiday_list/test_maintenance_holiday_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ONE FM and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase
+
+
+class TestMaintenanceHolidayList(IntegrationTestCase):
+	pass
