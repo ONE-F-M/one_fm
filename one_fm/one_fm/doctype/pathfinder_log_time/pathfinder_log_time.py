c5931f96133e59f56cf91622d9a4d7efb8bc83e8
NoBoneZ <adekunleabraham09@gmail.com>
2026-05-30 19:44:21 +0100

    moved bpmn related doctypes to one bpmn app

--- one_fm/one_fm/doctype/pathfinder_log_time/pathfinder_log_time.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, ONE FM and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class PathfinderLogTime(Document):
-	pass
