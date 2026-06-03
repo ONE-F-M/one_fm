166618b1e8dac7ba703b6925ddebb7cde3deb76e
jam <jam@jams-MacBook-Air.local>
2026-06-03 20:43:54 +0530

    fix: tod reference url for hd ticket

--- one_fm/overrides/todo.py
+++ one_fm/overrides/todo.py
@@ -187,7 +187,7 @@ def create_description_for_google_todo(doc):
     if doc.reference_type and doc.reference_name:
         todo_reference = get_url_to_form("Todo", doc.name) if doc.name else ""
         todo_doc_type = doc.reference_type or ""
-        todo_reference_link = get_url_to_form(todo_doc_type, doc.reference_name) if doc.reference_name else ""
+        todo_reference_link = get_url_to_todo_reference_doc(todo_doc_type, doc.reference_name) if doc.reference_name else ""
         warning_msg = f"""
         Hey you can't update this task on Google Task, Close the task in ERPNext
 
@@ -426,7 +426,7 @@ def send_email_on_todo_created(doc, method):
     if doc.reference_type and doc.reference_name:
         todo_reference = get_url_to_form("Todo", doc.name)
         todo_doc_type = doc.reference_type
-        todo_reference_link = get_url_to_form(todo_doc_type, doc.reference_name)
+        todo_reference_link = get_url_to_todo_reference_doc(todo_doc_type, doc.reference_name)
     args = frappe._dict({
                     "task_id": doc.custom_google_task_id,
                     "source": doc.custom_source,
@@ -444,3 +444,17 @@ def send_email_on_todo_created(doc, method):
     message = frappe.render_template("one_fm/templates/emails/email_notification_on_task_creation.html", args)
     subject = f"""A Task has been Created via {doc.custom_source} by {user_id}"""
     sendemail(recipients= recipients, message=message, subject=subject)
+
+def get_url_to_todo_reference_doc(document_type, name):
+    url = ""
+    if not document_type or not name:
+        return ""
+    try:
+        if document_type == "HD Ticket":
+            url = frappe.utils.get_url(f"/helpdesk/tickets/{name}")
+        else:
+            url = get_url_to_form(document_type, name)
+        return url
+    except Exception as e:
+        frappe.log_error(message=str(e), title=f"Failed to get URL for {document_type} {name}")
+        return ""
