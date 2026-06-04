59a4e6b3c34695627dbd62b00196e03a50c2728e
Talleyrand333 <ebukaakeru@gmail.com>
2026-06-04 12:21:21 +0100

    Revert "Merge pull request #6140 from ONE-F-M/WI-000850"
    This reverts commit 05f1b95ada8f33ae9e624f1fc769d741a1ab0ea3, reversing
    changes made to 8ddd9bba9d74bb508fdc9852126359e0e2e85d18.

--- one_fm/patches/v15_0/add_subcontractor_exit_workflow.py
+++
@@ -1,6 +0,0 @@
-from one_fm.custom.workflow.workflow import get_workflow_json_file, create_workflow
-
-def execute():
-	"""Add Subcontractor Exit workflow."""
-	workflow_data = get_workflow_json_file("subcontractor_exit.json")
-	create_workflow(workflow_data)
