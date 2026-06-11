907ddbc55e41140888f0e44d49aa93b387ea7a97
jam <jam@jams-MacBook-Air.local>
2026-06-11 07:08:22 +0530

    fix: Ticket creation from error log

--- one_fm/events/error_log.py
+++ one_fm/events/error_log.py
@@ -5,10 +5,14 @@ import frappe, json
 def create_issue_log(error_log):
     error_log = frappe._dict(json.loads(error_log))
 
+    is_doctype_related = "Yes" if error_log.reference_doctype else "No"
+
     issue_log = frappe.get_doc({
         'doctype':'HD Ticket',
         'reference_doctype':error_log.doctype,
         'reference_name':error_log.name,
+        'custom_is_doctype_related': is_doctype_related,
+        'custom_reference_doctype': error_log.reference_doctype if is_doctype_related == "Yes" else None,
         'subject':error_log.method,
         'status':'Open',
         'description':error_log.error,
