429193dbe2c9cb8d1d24ecc88596a44ce4979f30
jam <jam@jams-MacBook-Air.local>
2026-06-17 15:54:05 +0530

    fix: leave application not permit issue

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -864,6 +864,7 @@ override_whitelisted_methods = {
     "hrms.hr.doctype.leave_application.leave_application.get_number_of_leave_days": "one_fm.api.doc_methods.leave_application_calculation.custom_get_number_of_leave_days",
 	"hrms.hr.doctype.leave_application.leave_application.get_leave_approver" : "one_fm.overrides.leave_application.get_leave_approver",
 	"hrms.hr.doctype.leave_application.leave_application.get_leave_details" : "one_fm.overrides.leave_application.get_leave_details",
+	"hrms.hr.doctype.leave_application.leave_application.get_leave_balance_on" : "one_fm.overrides.leave_application.get_leave_balance_on",
     "frappe.desk.form.load.getdoc": "one_fm.permissions.getdoc",
     "frappe.desk.form.load.get_docinfo": "one_fm.permissions.get_docinfo",
 	"hrms.hr.doctype.goal.goal.get_children":"one_fm.overrides.goal.get_childrens",
--- one_fm/overrides/leave_application.py
+++ one_fm/overrides/leave_application.py
@@ -679,6 +679,52 @@ def get_leave_details(employee, date):
     }
 
 @frappe.whitelist()
+def get_leave_balance_on(
+	employee: str,
+	leave_type: str,
+	date: datetime.date,
+	to_date: datetime.date | None = None,
+	consider_all_leaves_in_the_allocation_period: bool = False,
+	for_consumption: bool = False,
+):
+	"""
+	Returns leave balance till date
+	:param employee: employee name
+	:param leave_type: leave type
+	:param date: date to check balance on
+	:param to_date: future date to check for allocation expiry
+	:param consider_all_leaves_in_the_allocation_period: consider all leaves taken till the allocation end date
+	:param for_consumption: flag to check if leave balance is required for consumption or display
+	        eg: employee has leave balance = 10 but allocation is expiring in 1 day so employee can only consume 1 leave
+	        in this case leave_balance = 10 but leave_balance_for_consumption = 1
+	        if True, returns a dict eg: {'leave_balance': 10, 'leave_balance_for_consumption': 1}
+	        else, returns leave_balance (in this case 10)
+	"""
+	if not to_date:
+		to_date = nowdate()
+
+	allocation_records = get_leave_allocation_records(employee, date, leave_type)
+	allocation = allocation_records.get(leave_type, frappe._dict())
+	end_date = (
+		allocation.to_date if (allocation and cint(consider_all_leaves_in_the_allocation_period)) else date
+	)
+	cf_expiry = get_allocation_expiry_for_cf_leaves(employee, leave_type, to_date, allocation.from_date)
+
+	leaves_taken = get_leaves_for_period(employee, leave_type, allocation.from_date, end_date)
+	manually_expired_leaves = get_manually_expired_leaves(
+		employee, leave_type, allocation.from_date, end_date
+	)
+
+	remaining_leaves = get_remaining_leaves(
+		allocation, leaves_taken, date, cf_expiry, manually_expired_leaves
+	)
+
+	if for_consumption:
+		return remaining_leaves
+	else:
+		return remaining_leaves.get("leave_balance")
+
+@frappe.whitelist()
 def get_leave_approver(employee):
     approver = get_approver_user(employee)
     return approver
