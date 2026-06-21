21d79609ac9240ffb9c908288fca9fd9846b7da8
samdanikouser <samdani.shareef6@gmail.com>
2026-06-21 15:58:53 +0300

    sync google task button from bpmn

--- one_fm/public/js/doctype_list_js/todo_list.js
+++ one_fm/public/js/doctype_list_js/todo_list.js
@@ -7,10 +7,13 @@ frappe.listview_settings['ToDo'] = {
 const sync_my_google_tasks = function(listview) {
 	listview.page.add_button(__('Sync My Google Tasks'), function() {
         frappe.call({
-			method: 'one_fm.overrides.todo.sync_my_google_tasks_with_todos',
+			method: 'one_bpmn.api.instance_api.start_process',
+			args: {
+				model_name: 'Sync Google Task to ERPNext ToDo'
+			},
 			callback: function (r) {
 				if (!r.exc) {
-					frappe.msgprint(__(r.message || "Tasks Synched!"));
+					frappe.msgprint(__('Google Task sync process started'));
 					listview.refresh();
 				}
 			},
