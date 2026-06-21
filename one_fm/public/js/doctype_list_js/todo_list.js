397859f8e92551f545967eadfd7d2ae1f7dbf55b
samdanikouser <samdani.shareef6@gmail.com>
2026-06-21 16:37:44 +0300

    button control

--- one_fm/public/js/doctype_list_js/todo_list.js
+++ one_fm/public/js/doctype_list_js/todo_list.js
@@ -7,18 +7,18 @@ frappe.listview_settings['ToDo'] = {
 const sync_my_google_tasks = function(listview) {
 	listview.page.add_button(__('Sync My Google Tasks'), function() {
         frappe.call({
-			method: 'one_bpmn.api.instance_api.start_process',
+			method: 'one_bpmn.api.instance_api.start_process_async',
 			args: {
 				model_name: 'Sync Google Task to ERPNext ToDo'
 			},
 			callback: function (r) {
 				if (!r.exc) {
-					frappe.msgprint(__('Google Task sync process started'));
-					listview.refresh();
+					frappe.show_alert({
+						message: __('Google Task sync started in background'),
+						indicator: 'green'
+					}, 5);
 				}
-			},
-			freeze: true,
-			freeze_message: __('Syncing My Google Tasks')
+			}
 		});
     }, 'primary');
 };
