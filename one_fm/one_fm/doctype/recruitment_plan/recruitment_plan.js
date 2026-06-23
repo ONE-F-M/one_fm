182cf1feab5db2d8cbe266bb15cb4dacd3569d2d
Kevin_Crypto <kevinmakora1@gmail.com>
2026-06-23 15:37:57 +0300

    style: resolve minor JS string quoting and naming conventions

--- one_fm/one_fm/doctype/recruitment_plan/recruitment_plan.js
+++ one_fm/one_fm/doctype/recruitment_plan/recruitment_plan.js
@@ -1,53 +1,53 @@
 // Copyright (c) 2026, ONE FM and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Recruitment Plan', {
+frappe.ui.form.on("Recruitment Plan", {
 	setup: function(frm) {
-		frm.set_query('erf', function() {
+		frm.set_query("erf", function() {
 			if (!frm.doc.designation) {
 				return {
 					filters: {
-						name: ['in', []] // Show nothing if no designation is selected
+						name: ["in", []] // Show nothing if no designation is selected
 					}
 				};
 			}
 			return {
 				filters: {
 					docstatus: 1,
-					status: 'Accepted',
+					status: "Accepted",
 					designation: frm.doc.designation
 				}
 			};
 		});
 	},
 	refresh: function(frm) {
-		load_recruitment_plan_autocomplete_options(frm);
-		toggle_country_label(frm);
+		loadRecruitmentPlanAutocompleteOptions(frm);
+		toggleCountryLabel(frm);
 	},
 	designation: function(frm) {
 		// Reset ERF if it does not match the new designation
 		if (frm.doc.erf) {
-			frappe.db.get_value('ERF', frm.doc.erf, 'designation', (r) => {
+			frappe.db.get_value("ERF", frm.doc.erf, "designation", (r) => {
 				if (r && r.designation !== frm.doc.designation) {
-					frm.set_value('erf', '');
+					frm.set_value("erf", "");
 				}
 			});
 		}
 	},
 	recruitment_plan_type: function(frm) {
-		toggle_country_label(frm);
+		toggleCountryLabel(frm);
 	}
 });
 
-function toggle_country_label(frm) {
-	if (frm.doc.recruitment_plan_type === 'Recruitment Trip') {
-		frm.set_df_property('trip_country', 'label', 'Trip Country');
+function toggleCountryLabel(frm) {
+	if (frm.doc.recruitment_plan_type === "Recruitment Trip") {
+		frm.set_df_property("trip_country", "label", "Trip Country");
 	} else {
-		frm.set_df_property('trip_country', 'label', 'Country');
+		frm.set_df_property("trip_country", "label", "Country");
 	}
 }
 
-function _populate_autocomplete(frm, fieldname, options) {
+function populateAutocomplete(frm, fieldname, options) {
 	let field = frm.fields_dict[fieldname];
 	if (!field) return;
 	if (field.get_query) delete field.get_query;
@@ -60,13 +60,13 @@ function _populate_autocomplete(frm, fieldname, options) {
 	}, 0);
 }
 
-function load_recruitment_plan_autocomplete_options(frm) {
+function loadRecruitmentPlanAutocompleteOptions(frm) {
 	const COUNTRY_KEY = "__recruitment_plan_country_options";
 	const NATIONALITY_KEY = "__recruitment_plan_nationality_options";
 
 	if (frappe[COUNTRY_KEY] && frappe[NATIONALITY_KEY]) {
-		_populate_autocomplete(frm, "trip_country", frappe[COUNTRY_KEY]);
-		_populate_autocomplete(frm, "nationality_in_pr_request", frappe[NATIONALITY_KEY]);
+		populateAutocomplete(frm, "trip_country", frappe[COUNTRY_KEY]);
+		populateAutocomplete(frm, "nationality_in_pr_request", frappe[NATIONALITY_KEY]);
 		return;
 	}
 
@@ -81,7 +81,7 @@ function load_recruitment_plan_autocomplete_options(frm) {
 					}
 				});
 				frappe[COUNTRY_KEY] = countries;
-				_populate_autocomplete(frm, "trip_country", countries);
+				populateAutocomplete(frm, "trip_country", countries);
 
 				let nationalities = ["Any", "African", "Asian"];
 				(r.message.nationalities || []).forEach(n => {
@@ -90,7 +90,7 @@ function load_recruitment_plan_autocomplete_options(frm) {
 					}
 				});
 				frappe[NATIONALITY_KEY] = nationalities;
-				_populate_autocomplete(frm, "nationality_in_pr_request", nationalities);
+				populateAutocomplete(frm, "nationality_in_pr_request", nationalities);
 			}
 		}
 	});
