97f2b6074b1fe3404c4082982bfb759b054ac1c3
ONE-FM Dev <dev@one-fm.com>
2026-07-05 18:36:42 +0200

    Revert "Merge pull request #6327 from ONE-F-M/BA_CHANGES_ON_TODO_PROCESS"
    This reverts commit 59947254e57e2ab756089b6425b7d2e63c3f03d4, reversing
    changes made to 130ccedac066dc181d7e99ee6fc6e53ba92beece.

--- one_fm/custom/custom_field/todo.py
+++ one_fm/custom/custom_field/todo.py
@@ -3,6 +3,13 @@ def get_todo_custom_fields():
     return {
         "ToDo": [
             {
+                "fieldname": "notify_allocated_to_via_email",
+                "fieldtype": "Check",
+                "label": "Notify Allocated To Via Email",
+                "insert_after": "allocated_to",
+                "allow_in_quick_entry": 1
+            },
+            {
                 "fieldname": "custom_column_break_x8z6v",
                 "fieldtype": "Column Break",
                 "label": "",
@@ -12,7 +19,7 @@ def get_todo_custom_fields():
                 "fieldname": "custom_google_task",
                 "fieldtype": "Section Break",
                 "label": "Google Task",
-                "insert_after": "allocated_to"
+                "insert_after": "notify_allocated_to_via_email"
             },
             {
                 "fieldname": "custom_google_task_id",
--- one_fm/public/js/doctype_list_js/todo_list.js
+++ one_fm/public/js/doctype_list_js/todo_list.js
@@ -6,22 +6,17 @@ frappe.listview_settings['ToDo'] = {
 
 const sync_my_google_tasks = function(listview) {
 	listview.page.add_button(__('Sync My Google Tasks'), function() {
-        frappe.show_alert({
-            message: __('Syncing Google Tasks...'),
-            indicator: 'blue'
-        }, 3);
         frappe.call({
-			method: 'one_bpmn.api.instance_api.start_process',
+			method: 'one_bpmn.api.instance_api.start_process_async',
 			args: {
 				model_name: 'Sync Google Task to ERPNext ToDo'
 			},
 			callback: function (r) {
 				if (!r.exc) {
 					frappe.show_alert({
-						message: __('Google Tasks synced successfully'),
+						message: __('Google Task sync started in background'),
 						indicator: 'green'
 					}, 5);
-					listview.refresh();
 				}
 			}
 		});
