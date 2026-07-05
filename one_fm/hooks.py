316e7e02a35d65cfd91e0f5f6dac732297e8bd57
ONE-FM Dev <dev@one-fm.com>
2026-07-05 10:59:13 +0200

    revert: sprint 24th-30th — revert non-Done Operations-011 PRs + BA_CHANGES (sprint cleanup)

--- one_fm/hooks.py
+++ one_fm/hooks.py
@@ -454,15 +454,15 @@ doc_events = {
 		"after_insert": "one_fm.one_fm.task_assignment_from_email.assign_task_to_user_from_communication_content"
 	},
 	# COMMENTED OUT: ToDo hooks now handled by SpiffWorkflow/BPMN server scripts
-	# "ToDo": {
-	# 	"validate": "one_fm.overrides.todo.validate_todo",
-	# 	"before_save":"one_fm.overrides.todo.before_save",
-	# 	"after_insert":[
-	# 		"one_fm.overrides.todo.create_google_task_on_todo_creation",
-	# 		"one_fm.overrides.todo.send_email_on_todo_created"
-	# 	],
-	# 	"on_update": "one_fm.overrides.todo.update_google_task_on_todo_status_change"
-	# },
+	"ToDo": {
+		"validate": "one_fm.overrides.todo.validate_todo",
+		"before_save":"one_fm.overrides.todo.before_save",
+		"after_insert":[
+			"one_fm.overrides.todo.create_google_task_on_todo_creation",
+			"one_fm.overrides.todo.send_email_on_todo_created"
+		],
+		"on_update": "one_fm.overrides.todo.update_google_task_on_todo_status_change"
+	},
 	"OAuth Bearer Token": {
 		"after_insert": "one_fm.api.doc_methods.oauth_bearer_token.revoke_and_delete_existing_tokens",
 	}
@@ -673,7 +673,6 @@ scheduler_events = {
 			"one_fm.api.tasks.overtime_shift_assignment",
 			#"one_fm.api.tasks.automatic_checkout",
 			"one_fm.one_fm.doctype.password_reset_token.password_reset_token.revoke_password_tokens",
-			"one_fm.api.tasks.rambo_shift_assignment",
 		],
 		"0/15 * * * *": [
 			"one_fm.api.tasks.update_shift_type"
